717f78ab54883b561d1a54797c3b3933baa00d22
Igor <igor@s3stores.com>
2016-02-12 16:52:11 +0300

    fix gtin to float conversion

--- include/func/func.froogle.php
+++ include/func/func.froogle.php
@@ -1661,7 +1661,10 @@ function SubmitBingProductsBatch($bproducts, $MerchantID, $CatalogID, $username,
 			$postBody["entries"][$k_counter]["product"]["brand"] = $product_info["product"]["google_brand"];
 			$postBody["entries"][$k_counter]["product"]["condition"] = "new";
             if (!empty($product_info["product"]["upc"])) {
-                $postBody["entries"][$k_counter]["product"]["gtin"] = $product_info["product"]["upc"];
+                $postBody["entries"][$k_counter]["product"]["gtin"] = "$product_info[product][upc]";
+                if ($debug_requests == 'Y') {
+                    print("gtin debug ".$postBody["entries"][$k_counter]["product"]["gtin"]."\r\n");
+                }
             }
 			$postBody["entries"][$k_counter]["product"]["mpn"] = $product_info["product"]["mpn"];
 			$postBody["entries"][$k_counter]["product"]["price"]["value"] = $product_info["product"]["price"];
@@ -1909,7 +1912,7 @@ function SubmitGoogleProductsBatch($gproducts, $service, $MerchantID){
                 $postBody["entries"][$k_counter]["product"]["brand"] = $product_info["product"]["google_brand"];
                 $postBody["entries"][$k_counter]["product"]["condition"] = "new";
                 if (!empty($product_info["product"]["upc"])) {
-                    $postBody["entries"][$k_counter]["product"]["gtin"] = $product_info["product"]["upc"];
+                    $postBody["entries"][$k_counter]["product"]["gtin"] = "$product_info[product][upc]";
                 }
                 $postBody["entries"][$k_counter]["product"]["mpn"] = $product_info["product"]["mpn"];
 
