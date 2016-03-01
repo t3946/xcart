8e9d3c356a5ea4f787a7104df35c75177629f9f9
xcartmaster <xcartmaster@gmail.com>
2016-03-01 06:56:25 +0000

    new changes

--- skin1_kolin/ajax_add_to_cart.js
+++ skin1_kolin/ajax_add_to_cart.js
@@ -5,7 +5,7 @@ $().ready(function(){
   $.ajaxSetup({
     error:function(x,e){
       if (x.status == 0) {
-        alert('You are offline!!\n Please Check Your Network.');
+//        alert('You are offline!!\n Please Check Your Network.');
       } else if (x.status == 404) {
         alert('Requested URL not found.');
       } else if (x.status == 500) {
