79e407fe9fda55f69ff4c265f6d9d4dd8c443273
Roman Novokshonov <romann@s3stores.com>
2016-04-29 10:21:06 +0300

    fix ETS date convert

--- include/product_modify.php
+++ include/product_modify.php
@@ -801,10 +801,9 @@ if (($REQUEST_METHOD == "POST") && ($mode == "product_modify")) {
                 }
 */
 
-
+		$eta_date_mm_dd_yyyy = func_convert_date_mm_dd_yyyy($eta_date_mm_dd_yyyy, 'seconds');
 
 		if ($manufacturer_feed_fields["eta_date_mm_dd_yyyy"]["disable"] == "Y") {
-			$eta_date_mm_dd_yyyy = func_convert_date_mm_dd_yyyy($eta_date_mm_dd_yyyy, 'seconds');
 
 			$todaysDate = strtotime(date("Y-m-d"));
 			$maxETADate = strtotime('+2 weeks', $todaysDate);
