604d8f28d75afbe3faacb9892405da481177f091
Roman Novokshonov <romann@s3stores.com>
2016-05-25 12:20:51 +0300

    fix

--- skin1_kolin/ajax_notify_by_email.js
+++ skin1_kolin/ajax_notify_by_email.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     $(document).on('submit', 'form[name^=notifyform_]', function (event )
     {
         event.preventDefault();
-        $(this)
+        $(this).p
     })
 });
