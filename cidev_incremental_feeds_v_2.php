8b3d576cec320e45247caa4d18e6fe17a7917ec0
Roman Novokshonov <romann@s3stores.com>
2016-06-03 11:47:02 +0300

    off debug mode

--- cidev_incremental_feeds_v_2.php
+++ cidev_incremental_feeds_v_2.php
@@ -47,7 +47,7 @@ define("FROOGLE_TAIL_LEN", strlen(constant("FROOGLE_TAIL")));
 define('FROOGLE_MAX_DESCRIPTION_LENGTH', 10 * 1024); //The content in an attribute in an item exceeds 10 KB.
 
 define('EXCLUDE_CATEGORYID_BRANCH', 5099);
-define('DEBUG_MODE', 'Y');
+define('DEBUG_MODE', 'N');
 
 ini_set('memory_limit', '512M');
 set_time_limit(0);
