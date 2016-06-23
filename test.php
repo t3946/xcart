3a564c818266b57fd4480fbcd44a9a6e460dcde7
Roman Novokshonov <romann@s3stores.com>
2016-06-23 13:12:50 +0300

    Paypal sandbox

--- test.php
+++ test.php
@@ -10,15 +10,7 @@ global $xcart_dir;
 require_once $xcart_dir."/include/class/classElasticSearch.php";
 
 
-$classElastic = new classElasticSearch($config["ElasticSearch_options"],'www.artistsupplysource.com');
-//$classElastic->setSource("*._id");
-$classElastic->setType("product");
-$classElastic->setMinScore(0.5);
-//$classElastic->setProductId("376");
-
-
-$classElastic->setSearchQuery($a);
-$result = $classElastic->query();
+var_dump($config['Paypal_API']['debug_mode']);
 
 
 
