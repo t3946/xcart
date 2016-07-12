9d049c2660d80c8f289ab3b90856f4e5087e8482
Roman Novokshonov <romann@s3stores.com>
2016-07-12 10:39:11 +0300

    https://s3stores.teamwork.com/tasks/7390890

--- include/class/classProducts.php
+++ include/class/classProducts.php
@@ -3,6 +3,7 @@ global $xcart_dir;
 require_once $xcart_dir."/include/class/classCloneData.php";
 require_once $xcart_dir."/include/class/classManufacturers.php";
 require_once $xcart_dir."/include/class/classCategories.php";
+require_once $xcart_dir."/include/class/classProduct.php";
 
 class classProducts extends classCloneData
 {
