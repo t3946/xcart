0db012c67e272405e6254ddabe411ef5f7c40318
Roman Novokshonov <romann@s3stores.com>
2016-07-25 13:20:40 +0300

    fix

--- include/func/func.froogle.php
+++ include/func/func.froogle.php
@@ -1584,13 +1584,14 @@ function SubmitAmazonInventoryBatch($ainventory, $a_config, $marketplaceIdArray)
 	        print('Amazon inventory empty\r');
 		return false;
 	}
+	$sMerchantId = $ainventory[''];
 ######################### Avail start #########################
 	$feed = <<<EOD
 <?xml version="1.0" encoding="utf-8" ?>
 <AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd">
 <Header>
    <DocumentVersion>1.01</DocumentVersion>
-   <MerchantIdentifier>M_SELLER_354577</MerchantIdentifier>
+   <MerchantIdentifier>$sMerchantId</MerchantIdentifier>
    </Header>
    <MessageType>Inventory</MessageType>
 EOD;
@@ -1730,7 +1731,7 @@ EOD;
 <AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd">
 <Header>
    <DocumentVersion>1.01</DocumentVersion>
-   <MerchantIdentifier>M_SELLER_354577</MerchantIdentifier>
+   <MerchantIdentifier>$sMerchantId</MerchantIdentifier>
    </Header>
    <MessageType>Price</MessageType>
 EOD;
