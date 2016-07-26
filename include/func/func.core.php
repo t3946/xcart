a209d1aaf1600a07e0a491cd9e7f602a1c936c48
Roman Novokshonov <romann@s3stores.com>
2016-07-26 13:39:55 +0300

    fix supplier feed https images

--- include/func/func.core.php
+++ include/func/func.core.php
@@ -4052,4 +4052,15 @@ function func_check_comma_in_field($orderid, $value, $sFieldName)
 	}
 	return false;
 }
+
+function file_get_contents_curl($url){
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	$data = curl_exec($curl);
+	curl_close($curl);
+	return $data;
+}
 ?>
