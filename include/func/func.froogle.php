8b6bc4a9546f6c47930c2f03195d819425a0d814
Roman Novokshonov <romann@s3stores.com>
2016-09-05 14:23:35 +0300

    https://s3stores.teamwork.com/#tasks/8297276

--- include/func/func.froogle.php
+++ include/func/func.froogle.php
@@ -668,9 +668,9 @@ if ($sExtraLog=='Y')
 	$product['mpn'] = $mpn;
 	$product['gpc'] = $gpc;
 	$product['cats_path'] = $cats_path;
-	$product['google_descr'] = iconv("UTF-8", "ISO-8859-1//TRANSLIT",func_froogle_convert($product['descr'], 5000));
-	$product['google_brand'] = iconv("UTF-8", "ISO-8859-1//TRANSLIT",func_froogle_convert($product['brand'], 256));
-	$product['google_product'] = iconv("UTF-8", "ISO-8859-1//TRANSLIT",func_froogle_convert($product['product'], 80));
+	$product['google_descr'] = iconv("UTF-8", "ISO-8859-1//TRANSLIT",func_froogle_convert(trim($product['descr']), 5000));
+	$product['google_brand'] = iconv("UTF-8", "ISO-8859-1//TRANSLIT",func_froogle_convert(trim($product['brand']), 256));
+	$product['google_product'] = iconv("UTF-8", "ISO-8859-1//TRANSLIT",func_froogle_convert(trim($product['product']), 80));
 
 
 	if ($product['shipping_weight']) {
