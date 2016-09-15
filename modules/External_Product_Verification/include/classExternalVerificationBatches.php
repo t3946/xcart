441c3263cb6560e89689b1c62076f5706369efe6
Roman Novokshonov <romann@s3stores.com>
2016-09-15 11:54:22 +0300

    fixes

--- modules/External_Product_Verification/include/classExternalVerificationBatches.php
+++ modules/External_Product_Verification/include/classExternalVerificationBatches.php
@@ -167,12 +167,14 @@ class classExternalVerificationBatch extends classData
         global $login;
         $oProduct = null;
         $sStatuses = "q.status = 'In progress' AND q.cross_verify_count <= 1";
+        $slogin = "";
         if ($isTest) {
             $sStatuses = "q.status IN ('etalon_match','etalon_not_match')";
+            $slogin = "AND login = '.$login.'";
         }
         $aNextProducts = $this->oSQL->init()->addSelect('p.productid')->addSelect('cross_verify_count', 'batch_processed')->addFromTable('products', 'p')->
         addInnerJoin('external_verification_products_queue', 'q', "q.productid = p.productid AND $sStatuses")->
-        addCondition("p.forsale = 'Y'")->addCondition('NOT EXISTS (SELECT 1 FROM ' . self::$sql_tbl['external_verification_products'] . ' vp WHERE vp.productid = p.productid  AND login = \''.$login.'\' AND action IN (\'open\'))')->addGroupBy('p.productid')->addOrderBy('cross_verify_count DESC')->setLimit('1')->Execute()->getQueryResult();
+        addCondition("p.forsale = 'Y'")->addCondition('NOT EXISTS (SELECT 1 FROM ' . self::$sql_tbl['external_verification_products'] . ' vp WHERE vp.productid = p.productid' . $slogin . ' AND action IN (\'open\'))')->addGroupBy('p.productid')->addOrderBy('cross_verify_count DESC')->setLimit('1')->Execute()->getQueryResult();
         if (!empty($aNextProducts)) {
             foreach ($aNextProducts as $aNextProduct) {
                 $oProduct = new classProduct(['productid' => $aNextProduct['productid']]);
