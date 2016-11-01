c651f63e3546177108db2cd3a97227276c818414
Roman Novokshonov <romann@s3stores.com>
2016-11-01 09:50:38 +0300

    delete old classes (reverted from commit c1bd796e19f71f1c74f40c6f5cb7420cf8382cce)

---
+++ include/class/classHTMLShot.php
@@ -0,0 +1,68 @@
+<?php
+global $xcart_dir;
+require_once $xcart_dir . "/include/class/classData.php";
+
+class classHTMLShot extends classData
+{
+    const PATH_TO_HTMLS_SHOT_IMAGES = "/images/HTML/%d_%d/";
+
+    public function __construct($aParams = [])
+    {
+        $this->aPrimaryKeys = ['id'];
+        $this->sPrimaryTable = 'product_htmlshot';
+        parent::__construct($aParams);
+    }
+
+    public function createHTMLShot(classProduct $oProduct, $orderid)
+    {
+        global $xcart_dir;
+        $aImagesD = $oProduct->getImages('D');
+        $aImagesP = $oProduct->getImages('P');
+        /** @var classProductImage[] $aImages */
+        $aImages = array_merge($aImagesD, $aImagesP);
+        $oStoreFront = $oProduct->getStoreFront()->getStoreFrontByProductId($oProduct->getProductId());
+
+        foreach ($aImages as $oImage) {
+                if ($oStoreFront->isCDNEnable()) {
+                    $oImage->useCDN(true, $oStoreFront->getCDNURL());
+                } else $oImage->useCDN(false);
+
+
+                if ($oImage->saveImageTo($xcart_dir . sprintf(self::PATH_TO_HTMLS_SHOT_IMAGES, $orderid, $oProduct->getProductId()))) {
+                    $oImage->setField('image_path', sprintf(self::PATH_TO_HTMLS_SHOT_IMAGES, $orderid, $oProduct->getProductId()) . $oImage->getFileName());
+                }
+
+            }
+            $oProduct->getPricing();
+            $oStoreFront->setCDNDisable();
+
+            $this->setField('htmlshot', addslashes(serialize($oProduct)))->
+            setField('product_id', $oProduct->getProductId())->
+            setField('order_id', $orderid)->_insert();
+    }
+
+    public function getId()
+    {
+        return $this->getField('id');
+    }
+
+    public function getOrderId()
+    {
+        return $this->getField('order_id');
+    }
+
+    public function getProductId()
+    {
+        return $this->getField('product_id');
+    }
+
+    public function getHTMLShot()
+    {
+        return unserialize($this->getField('htmlshot'));
+    }
+
+    public function deleteHTMLShot()
+    {
+        $this->_delete();
+    }
+}
