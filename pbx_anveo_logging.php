65927ad65113f6486afc909c7bd6dc5acf27853e
Maksim Korobitsyn <dog.mixer@gmail.com>
2016-11-21 20:36:44 +0300

    save to table calls

--- pbx_anveo_logging.php
+++ pbx_anveo_logging.php
@@ -2,8 +2,63 @@
 require_once "top.inc.php";
 require_once "init.php";
 
-//if (!empty($_REQUEST))
-//{
+$now = date('Y-m-d H:i:s');
+
+if (!empty($_GET['incoming_flow_start']) || !empty($_GET['start_out']))
+{
+    $direction = (!empty($_GET['incoming_flow_start'])) ? 0 : 1; // 0 - in | 1 - out
+    $session = getDecodedString($_GET['session']);
+    db_query("INSERT INTO anveo_calls (`session`, `start_at`) VALUES ('{$session}', '{$now}')");
+}
+
+if (!empty($_GET['incoming_flow_end']) || !empty($_GET['end_out']))
+{
+    $session = getDecodedString($_GET['session']);
+    db_query("UPDATE anveo_calls SET `end_at` = '{$now}' WHERE `session` = '{$session}'");
+}
+
+if (!empty($_POST['incoming_call_saved']) || !empty($_POST['record_out']))
+{
+    $session = getDecodedString($_POST['session']);
+
+    if (!empty($_POST['incoming_call_saved']))
+    {
+        $file = getDecodedString($_POST['incoming_call_saved']);
+    }
+    elseif (!empty($_POST['record_filename'])) {
+        $file = getDecodedString($_POST['record_filename']);
+    }
+
+    db_query("UPDATE anveo_calls SET `file` = '{$file}' WHERE `session` = '{$session}'");
+}
+
+saveToLog();
+
+function getClearValue($value)
+{
+    if (is_array($value)) {
+        $t = [];
+
+        foreach ($value as $k=>$v) {
+            $t[$k] = getDecodedString($v);
+        }
+
+        $value = json_encode($t);
+    }
+    else {
+        $value = getDecodedString($value);
+    }
+
+    return $value;
+}
+
+function getDecodedString($str)
+{
+    return htmlspecialchars_decode(stripslashes($str), ENT_QUOTES);
+}
+
+function saveToLog()
+{
     $getArray = [];
 
     foreach ($_GET as $key => $value)
@@ -46,27 +101,4 @@ require_once "init.php";
     if (!empty($getArray)) {
         func_backprocess_log('anveo_logging', implode("\n---\n", $getArray));
     }
-//}
-
-function getClearValue($value)
-{
-    if (is_array($value)) {
-        $t = [];
-
-        foreach ($value as $k=>$v) {
-            $t[$k] = getDecodedString($v);
-        }
-
-        $value = json_encode($t);
-    }
-    else {
-        $value = getDecodedString($value);
-    }
-
-    return $value;
-}
-
-function getDecodedString($str)
-{
-    return htmlspecialchars_decode(stripslashes($str), ENT_QUOTES);
 }
