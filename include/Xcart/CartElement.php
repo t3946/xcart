bb0b3994c00ab1a84a4a5d59eba4861a1a046223
Roman Novokshonov <romann@s3stores.com>
2017-01-11 10:21:03 +0300

    cart element class

---
+++ include/Xcart/CartElement.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Xcart;
+
+
+class CartElement
+{
+    private $oProduct = null;
+    private $iQty = 0;
+
+    public function __construct($oProduct, $iQty)
+    {
+        $this->oProduct = $oProduct;
+        $this->iQty = $iQty;
+    }
+
+    public function getProduct()
+    {
+        return $this->oProduct;
+    }
+
+    public function getQuantity()
+    {
+        return $this->iQty;
+    }
+
+    public function setQuantity($iQty)
+    {
+        $this->iQty = $iQty;
+    }
+}
