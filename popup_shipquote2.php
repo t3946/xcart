c4828abaffbf1589d84932fca5c8defc43492cf3
Roman Novokshonov <romann@s3stores.com>
2017-01-26 10:47:41 +0300

    add showing shipping extra margin value in popup_shipquote

--- popup_shipquote2.php
+++ popup_shipquote2.php
@@ -199,7 +199,9 @@ if ($REQUEST_METHOD == "POST" || $shipping_error == "Y") {
                             $shipping[$oShippingRate->getShippingId()] = $oShippingRate->getShippingEntity()->getFields();
                             $shipping[$oShippingRate->getShippingId()]['rate'] = $oShippingRate->getShippingCharge();
                             $shipping[$oShippingRate->getShippingId()]['allowed'] = 1;
-                            $shipping[$oShippingRate->getShippingId()]['shipping_extra_margin_value'] = $oShippingRate->getCart()->getExtraMarginValue();
+                            if ($oShippingRate->getCart()->getExtraMarginValue() > 0) {
+                                $shipping[$oShippingRate->getShippingId()]['shipping_extra_margin_value'] = $oShippingRate->getShippingChargeBeforeMap() - $oShippingRate->getShippingCharge();
+                            }
                         }
                     }
                 }
