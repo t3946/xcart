c232bf334da3dac07e021a0b1da0901476607186
Roman Novokshonov <romann@s3stores.com>
2017-02-07 15:44:22 +0300

    fix log processing time

--- cron/cron_paypal_transactions_watchdog.php
+++ cron/cron_paypal_transactions_watchdog.php
@@ -26,7 +26,7 @@ if ($config[LOG_CATEGORY] == "Y") {
     setFrom('team@s3stores.com')->
     setBody(LOG_CATEGORY . ' already launched')->
     setSubject(sprintf('Attention! Xcart cron %s Already launched', LOG_CATEGORY))->sendEmail();
-    //die("Already launched"); // ################################
+    die("Already launched"); // ################################
 }
 db_query("REPLACE $sql_tbl[config] SET value='Y', name='" . LOG_CATEGORY . "'");
 $start_time = new DateTime('now');
@@ -37,14 +37,17 @@ func_backprocess_log(LOG_CATEGORY, $log_text);
 $aOrderGroups = OrderGroup::model()->findAll(
     SQLBuilder::getInstance()
         ->addCondition("cb_status ='P'")
-        ->addCondition("cb_update_datetime > DATE_SUB(NOW(), INTERVAL 1 MONTH)")
+        ->addCondition("cb_update_datetime > DATE_SUB(NOW(), INTERVAL 2 MONTH)")
+        ->addInnerJoin('order_transactions', 'ot', 'main.orderid = ot.orderid')
+        ->addCondition("ot.paymentid IN (5, 17, 21, 100)")
         ->addGroupBy('orderid')
         ->addOrderBy('cb_update_datetime DESC')
-        ->setLimit('50'));
+        ->setLimit('2'));
 
 if (!empty($aOrderGroups)) {
+    $countOrders = count($aOrderGroups);
+    func_backprocess_log(LOG_CATEGORY, "Processing {$countOrders} orders.");
     foreach ($aOrderGroups as $oOrderGroup) {
-        if (!(in_array($oOrderGroup->getOrderInstance()->getPaymentMethodId(), [5, 17, 21, 100]))){ continue;}
         $fOrderGroupTotalAmount = 0;
         $aTransactions = OrderTransactions::getOrderTransactionsByOrderIdAndStatus($oOrderGroup->getOrderId(), ['completed']);
         if (!empty($aTransactions)) {
@@ -59,18 +62,20 @@ if (!empty($aOrderGroups)) {
                 }
             }
         }
-        echo "OrderId: ".$oOrderGroup->getOrderId(). ". " . $fOrderGroupTotalAmount . " - ". $oOrderGroup->getTotalGross()."<br>";
+
         if ($fOrderGroupTotalAmount != $oOrderGroup->getTotalGross()){
             $oAttentionTag = new AttentionTag(['status_id' => 44]);
             $aInsertArray = ['orderid' => $oOrderGroup->getOrderId(), 'status_id' => $oAttentionTag->getStatusId()];
             func_array2insert('orders_additional_tags', $aInsertArray, true);
             $sLog = "Attention tag added: " . $oAttentionTag->getStatus() . "\n";
             Logs::_log('orders', $oOrderGroup->getOrderId(), 'X', $sLog);
+            $sLog = "Difference in OrderId: ".$oOrderGroup->getOrderId(). ". TransactionsTotal(" . $fOrderGroupTotalAmount . ") - OrderTotal(". $oOrderGroup->getTotalGross().")";
+            func_backprocess_log(LOG_CATEGORY, $sLog);
         }
     }
 }
 Config::model(['name' => LOG_CATEGORY])->setValue('N')->_update();
-$str_time = (new DateTime('now'))->diff($start_time)->format('%H:%i:%s');
+$str_time = (new DateTime('now'))->diff($start_time)->format('%H:%I:%S');
 $log_text = "Cron completed. Processing time: {$str_time}";
 func_backprocess_log(LOG_CATEGORY, $log_text);
 
