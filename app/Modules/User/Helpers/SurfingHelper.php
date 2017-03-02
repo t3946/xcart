85cea41861ddaf3bb59f4a2691134ebcbb904445
Roman Novokshonov <romann@s3stores.com>
2017-03-02 13:57:22 +0300

    fix null UserAgent

--- app/Modules/User/Helpers/SurfingHelper.php
+++ app/Modules/User/Helpers/SurfingHelper.php
@@ -92,13 +92,14 @@ class SurfingHelper
                 $oReferer->save();
 
                 if ($oSurfMeta->id) {
+                    $userAgent = Xcart::app()->request->getUserAgent();
                     (new SurfPathModel([
                             'meta_id'         => $oSurfMeta->id,
                             'resource_id'     => $oReferer->referer_id,
                             'resource_type'   => $model::GOAL_TYPE_REFERER,
                             'timestamp'       => time(),
                             'position'        => $oSurfMeta->points_visited,
-                            'additional_data' => Xcart::app()->request->getUserAgent(),
+                            'additional_data' => is_null($userAgent) ? '' : $userAgent,
                         ]
                     ))->save();
                 }
