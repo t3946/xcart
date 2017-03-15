5b9eddbf44465e6367077a86b0b66b56844e2733
Roman Novokshonov <romann@s3stores.com>
2017-03-15 15:36:59 +0300

    fixes

--- include/func/func.froogle.php
+++ include/func/func.froogle.php
@@ -1,4 +1,6 @@
 <?php
+use Xcart\StoreFront;
+
 if ( !defined('XCART_START') ) { header("Location: ../"); die("Access denied"); }
 
 #
@@ -67,14 +69,18 @@ $start_time = round(microtime(true) * 1000);
 if ($sExtraLog=='Y')
 	echo 'Storefrontid: '.$use_storefrontid;
 
-	if (empty($productid)){
+	if (empty($productid)) {
 //		$row = "title\tdescription\tlink\tadwords_redirect\tadwords_grouping\tadwords_labels\timage link\tadditional image link\tid\tprice\tpayment accepted\tpayment notes\tquantity\tweight\texpiration date\tbrand\tcondition\tproduct type\tmpn\tmodel number\tgtin\tcompatible with\tonline only\tshipping\tavailability\tmultipack\tgoogle product category\n";
 		$row = "title\tdescription\tlink\tadwords_redirect\timage link\tadditional image link\tid\tprice\tshipping weight\texpiration date\tbrand\tcondition\tproduct type\tmpn\tgtin\tshipping\tavailability\tmultipack\tgoogle product category\n";
 		return $row;
 	}
+	$classProduct = Xcart\Product::model(['productid' => $productid]);
 
-        $froogle_location = $config['Froogle']['froogle_used_https_links'] == 'Y' ? $https_location : $http_location;
-        $froogle_scheme = $config['Froogle']['froogle_used_https_links'] == 'Y' ? 'https://' : 'http://';
+	$froogle_location = $config['Froogle']['froogle_used_https_links'] == 'Y' ? $https_location : $http_location;
+	$froogle_scheme = $config['Froogle']['froogle_used_https_links'] == 'Y' ? 'https://' : 'http://';
+	if ($classProduct->getProductId()) {
+		$froogle_scheme = ($classProduct->getStoreFront()->getConfigValue('https_enabled') == 'Y') ? 'https://' : 'http://';
+	}
 
 	$where = "";
 	$fields = "";
@@ -305,7 +311,7 @@ if ($sExtraLog=='Y')
 		$product['adwords_labels'] .= ", offlist";
 	}
 
-	$classProduct = Xcart\Product::model(['productid'=>$product['productid']]);
+
 	$mpn = $classProduct->getMPN();
 	$product['custom_label_3'] = $classProduct->getManfacturerClass()->getField("manufacturer");
 
