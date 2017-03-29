7963c9382bed62b216758541c24de34fb328c998
Maksim Korobitsyn <dog.mixer@gmail.com>
2017-03-29 12:17:46 +0300

    up: user in cli mode

--- include/Xcart/App/Application/Application.php
+++ include/Xcart/App/Application/Application.php
@@ -226,7 +226,7 @@ class Application
     public function getUser()
     {
         /** @var AuthInterface $auth */
-        if (self::getIsWebMode() && $auth = $this->getComponent('auth')) {
+        if ($auth = $this->getComponent('auth')) {
             return $auth->getUser();
         }
         return null;
