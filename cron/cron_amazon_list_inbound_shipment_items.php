b7abf5b960467c851fae18c10ad00b582c1b1818
Roman Novokshonov <romann@s3stores.com>
2017-04-19 12:04:22 +0300

    fix

--- cron/cron_amazon_list_inbound_shipment_items.php
+++ cron/cron_amazon_list_inbound_shipment_items.php
@@ -19,7 +19,7 @@ if ($config[LOG_CATEGORY] == "Y") {
     $oMail->subject = sprintf('Attention! Xcart cron %s Already launched', LOG_CATEGORY);
     $oMail->body = LOG_CATEGORY . ' already launched';
     $oMail->sendEmail();
-    //die("Already launched");
+    die("Already launched");
 }
 db_query_param(/** @lang MySQL */
     "REPLACE xcart_config SET value='Y', name=:name",['name' => LOG_CATEGORY]);
