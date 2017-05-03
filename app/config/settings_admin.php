bdd81b89a700103418d4ca974a1904fb270c9481
Maksim K <dog.mixer@gmail.com>
2017-05-03 15:05:27 +0300

    up: disable new errHandler

--- app/config/settings_admin.php
+++ app/config/settings_admin.php
@@ -9,21 +9,14 @@ return array_replace_recursive((is_file($base_config)) ? include $base_config : 
             'debug' => true,
             'errHandler' => false
         ],
-        'request' => [
-            'class' => '\\Xcart\\App\\Request\\RequestManager',
-            'httpRequest' => [
-                'class' => '\\Xcart\\App\\Request\\HttpRequest',
-                //'from_get' => 'path',
-                'session' => [
-                    'class' => '\\Xcart\\App\\Request\\XcartSession'
-                ]
-            ],
-        ],
         'router' => [
             'class' => '\\Xcart\\App\\Router\\Router',
             'pathRoutes' => 'base.config.routes_admin',
             //'basePath' => '/admin/controllers.php?',
             //'mode' => 'get',
         ],
+    ],
+    'autoloadComponents' => [
+        'errorHandler'
     ]
 ]);
