06ea63a9f44626a5af2d6b0ea7c308e07e51c2fa
Maksim K <dog.mixer@gmail.com>
2017-05-24 21:09:34 +0300

    up: add presets for es2016^ es2017

--- static/config/gulp.frontend.js
+++ static/config/gulp.frontend.js
@@ -55,8 +55,7 @@ module.exports = {
                         use: {
                             loader: 'babel-loader',
                             options: {
-                                presets: ['es2015'],
-                                // presets: ['env']
+                                presets: [{ "es2015": { "modules": false }}],
                             }
                         }
                     }
