7ab8734ed6b9a97be4c2de18b1087d1e813a3e44
Maksim K <dog.mixer@gmail.com>
2017-05-26 17:04:38 +0300

    up: images for bots

--- app/Modules/User/UserModule.php
+++ app/Modules/User/UserModule.php
@@ -1,9 +1,21 @@
 <?php
 namespace Modules\User;
 
+use Fenom;
+use Modules\User\Helpers\BotsHelper;
+use Xcart\App\Main\Xcart;
 use Xcart\App\Module\Module;
 
 class UserModule extends Module
 {
     public $sessionTime = 15552000;
+
+
+    public static function onApplicationRun()
+    {
+        $template = Xcart::app()->template->getRenderer();
+
+        $template->addAccessorSmart("isBot", "isBot", Fenom::ACCESSOR_PROPERTY);
+        $template->isBot = BotsHelper::IsBot();
+    }
 }
