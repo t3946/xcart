5a64bc2d818c00ab21a8d252bc7ca5425a201d5f
Roman Novokshonov <kshon@mail.ru>
2017-06-05 10:49:50 +0300

    fast fix category model

--- cron/cron_supplier_feeds.php
+++ cron/cron_supplier_feeds.php
@@ -52,7 +52,7 @@ if ($config[$log_category] == "Y") {
     $oMail->subject = sprintf('Attention! Xcart cron %s Already launched', $log_category);
     $oMail->body = $log_category . ' already launched';
     $oMail->sendEmail();
-    //die("Already launched"); // ################################
+    die("Already launched"); // ################################
 }
 db_query_param('REPLACE xcart_config SET value=:value, name=:name', ['value' => 'Y', 'name' => $log_category]);
 
