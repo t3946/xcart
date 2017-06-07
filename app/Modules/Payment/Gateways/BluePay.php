24aeb96b9e4bff7c9b49ea63e5ccadb53d9a4b77
Roman Novokshonov <kshon@mail.ru>
2017-06-07 16:13:36 +0300

    fix bluepay auth

--- app/Modules/Payment/Gateways/BluePay.php
+++ app/Modules/Payment/Gateways/BluePay.php
@@ -76,7 +76,7 @@ class BluePay extends Gateway
             ->authorize($params)
             ->send();
         $data = $this->result->getData();
-        if ($this->result->isSuccessful() && ($data['STATUS '] == 1)) {
+        if ($this->result->isSuccessful() && ($data['STATUS'] == 1)) {
             return true;
         } else {
             return false;
