bc9ddac71343f057ce12f9ad0045fe031d936dc0
Maksim K <dog.mixer@gmail.com>
2017-06-07 17:51:56 +0300

    add: new felpers

---
+++ app/config/logger.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maksim
+ * Date: 07.06.17
+ * Time: 14:48
+ */
--- include/Xcart/App/Main/Xcart.php
+++ include/Xcart/App/Main/Xcart.php
@@ -1,21 +1,13 @@
 <?php
-/**
- *
- *
- * All rights reserved.
- *
- * @author Okulov Anton
- * @email qantus@mail.ru
- * @version 1.0
- * @company HashStudio
- * @site http://hashstudio.ru
- * @date 10/04/16 10:20
- */
 
 namespace Xcart\App\Main;
 
 use Xcart\App\Application\Application;
 use Xcart\App\Helpers\Creator;
+
+if (!function_exists('d')) {
+    require_once __DIR__ . '/dump.php';
+}
 
 class Xcart
 {
