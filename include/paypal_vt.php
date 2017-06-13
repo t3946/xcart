634079f8f720535b38f540a4693ee213df7eeeb0
Roman Novokshonov <kshon@mail.ru>
2017-06-13 12:43:17 +0300

    up

--- include/paypal_vt.php
+++ include/paypal_vt.php
@@ -12,11 +12,14 @@ use Modules\Payment\Models\PaymentProcessorModel;
 use Omnipay\Common\CreditCard;
 use Xcart\Paypal;
 
-if (!defined('XCART_SESSION_START')) { header("Location: ../");  die("Access denied"); }
+if (!defined('XCART_SESSION_START')) {
+    header("Location: ../");
+    die("Access denied");
+}
 
 global $REQUEST_METHOD, $mode, $top_message, $order_transaction_id, $paypal_vt, $transaction_status, $AJAX_SUBMIT, $login;
 
-$gw = $log = $orderTransaction = $result = $countTr =null;
+$gw = $log = $orderTransaction = $result = $countTr = null;
 $isAllowed = true;
 $params = [];
 
@@ -60,106 +63,109 @@ if ($REQUEST_METHOD == "POST" && !empty($orderid) && in_array($mode, array_keys(
         case 'look_up_payment' :
             break;
     }
-    if ($isAllowed) {
-        if ($gw && isset(Gateway::$gatewayMethods[$mode])) {
-            $method = Gateway::$gatewayMethods[$mode]['method'];
-            $status = Gateway::$gatewayMethods[$mode]['status'];
-            $log .= Gateway::$gatewayMethods[$mode]['log'];
-            if ($res = $gw->$method($params)) {
-                if ($mode == 'authorize') {
-                    $orderTransaction = new OrderTransactionModel(['orderid' => $orderModel->orderid]);
-                }
-                $orderTransaction->setAttributes(
-                    [
-                        'transaction_id' => $gw->result->getTransactionReference(),
-                        'transaction_status' => $status,
-                        'transaction_currency' => $params["currency"],
-                        'transaction_amount' => $params["amount"],
-                        'transaction_response' => $gw->result->getData(),
-                        'login' => $login,
-                        'paymentid' => $pmModel->paymentid
-                    ]
-                );
-                $orderTransaction->save();
-                $log .= "<br />Transaction:" . $orderTransaction->transaction_id;
-                if (!$countTr && $mode == 'authorize') {
-                    list ($o_log, $send_notification) = OrderHelper::changeOrderCBStatus($orderModel, OrderStatusModel::ORDER_STATUS_AUTHORIZED);
-                    $log .= $o_log;
-                    if ($send_notification) {
-                        func_send_order_status_notification($orderModel->orderid, OrderStatusModel::ORDER_STATUS_AUTHORIZED);
+    try {
+        if ($isAllowed) {
+            if ($gw && isset(Gateway::$gatewayMethods[$mode])) {
+                $method = Gateway::$gatewayMethods[$mode]['method'];
+                $status = Gateway::$gatewayMethods[$mode]['status'];
+                $log .= Gateway::$gatewayMethods[$mode]['log'];
+                if ($res = $gw->$method($params)) {
+                    if ($mode == 'authorize') {
+                        $orderTransaction = new OrderTransactionModel(['orderid' => $orderModel->orderid]);
                     }
-                }
-            } else {
-                $result = $gw->result->getData();
-                if ($result['name'] == 'AUTHORIZATION_EXPIRED') {
-                    $transaction_status = 'Expired';
-                    $orderTransaction->transaction_status = $transaction_status;
-                    $orderTransaction->transaction_response = $result;
-                    $orderTransaction->save();
-                }
-                $log .= "<br />{$result['name']}";
-                $log .= "<br />{$result['message']}";
-            }
-        } else {
-            if ($mode == 'add_manual_transaction'){
-                $transaction_id = trim($transaction_id);
-                $transaction_amount = trim($transaction_amount);
-
-                if ($transaction_status == "authorized") {
-                    $transaction_type = "authorization";
-                    $set_cb_status_for_first_transaction = "AP";
-                } else {
-                    $transaction_type = "capture";
-                    $set_cb_status_for_first_transaction = "P";
-                }
-                $count_transactions = OrderTransactionModel::objects()
-                    ->filter(['orderid' => $orderid])
-                    ->exclude(['transaction_status' => '', 'transaction_id' => ''])
-                    ->count();
-                $allowed_statuses_flag = func_check_for_the_allowed_statuses_for_create_payment($order);
-                if (!$allowed_statuses_flag && empty($count_transactions) && (empty($AJAX_SUBMIT) || $AJAX_SUBMIT != "Y")) {
-                    $top_message = array(
-                        'type' => 'E',
-                        'content' => func_get_langvar_by_name("lbl_first_transaction_in_order_exception")
+                    $orderTransaction->setAttributes(
+                        [
+                            'transaction_id' => $gw->result->getTransactionReference(),
+                            'transaction_status' => $status,
+                            'transaction_currency' => $params["currency"],
+                            'transaction_amount' => $params["amount"],
+                            'transaction_response' => $gw->result->getData(),
+                            'login' => $login,
+                            'paymentid' => $pmModel->paymentid
+                        ]
                     );
-                    $section_name_top_message = $top_message;
-                    x_session_save("section_name_top_message");
-                    func_header_location("order.php?orderid=" . $orderid . "&tab=y#main_order_tabs-VT");
-                }
-                if ($allowed_statuses_flag && empty($count_transactions) && (empty($AJAX_SUBMIT) || $AJAX_SUBMIT != "Y")) {
-                    $new_cb_status_flag = false;
-                    $new_cb_status_value = func_query_first_cell("SELECT name FROM $sql_tbl[order_statuses] WHERE code='$set_cb_status_for_first_transaction'");
-                    foreach ($order["shipping_groups"] as $ko => $vo) {
-                        if (in_array($vo["cb_status"], array('Q', 'N', 'I'))) {
-                            db_query("UPDATE $sql_tbl[order_groups] SET cb_status='$set_cb_status_for_first_transaction' WHERE orderid='$orderid' AND manufacturerid='$ko'");
-                            $current_cb_status_value = func_query_first_cell("SELECT name FROM $sql_tbl[order_statuses] WHERE code='" . $vo["cb_status"] . "'");
-                            $log .= "<br /><B>" . $vo["all_distributor_info"]["code"] . ":</B> cb_status: " . $current_cb_status_value . " -> " . $new_cb_status_value . "<br />";
-                            $new_cb_status_flag = true;
+                    $orderTransaction->save();
+                    $log .= "<br />Transaction:" . $orderTransaction->transaction_id;
+                    if (!$countTr && $mode == 'authorize') {
+                        list ($o_log, $send_notification) = OrderHelper::changeOrderCBStatus($orderModel, OrderStatusModel::ORDER_STATUS_AUTHORIZED);
+                        $log .= $o_log;
+                        if ($send_notification) {
+                            func_send_order_status_notification($orderModel->orderid, OrderStatusModel::ORDER_STATUS_AUTHORIZED);
                         }
                     }
-                    if ($new_cb_status_flag) {
-                        db_query("UPDATE $sql_tbl[orders] SET cb_status='$set_cb_status_for_first_transaction' WHERE orderid='$orderid'");
-                        func_send_order_status_notification($orderid, $set_cb_status_for_first_transaction);
+                } else {
+                    $result = $gw->result->getData();
+                    if ($result['name'] == 'AUTHORIZATION_EXPIRED') {
+                        $transaction_status = 'Expired';
+                        $orderTransaction->transaction_status = $transaction_status;
+                        $orderTransaction->transaction_response = $result;
+                        $orderTransaction->save();
                     }
+                    $log .= "<br />{$result['name']}";
+                    $log .= "<br />{$result['message']}";
                 }
-                $result = func_paypal_look_up_payment($Access_Token, $transaction_id, $transaction_type);
-                $transaction_total = $transaction_amount;
-                $result["FIELD_manual_transaction"] = "Y";
-                $result["FIELD_avs_code"] = $avs_code;
-                $log .= "'Add transaction' at 'Add manual transaction' section";
+            } else {
+                if ($mode == 'add_manual_transaction') {
+                    $transaction_id = trim($transaction_id);
+                    $transaction_amount = trim($transaction_amount);
+
+                    if ($transaction_status == "authorized") {
+                        $transaction_type = "authorization";
+                        $set_cb_status_for_first_transaction = "AP";
+                    } else {
+                        $transaction_type = "capture";
+                        $set_cb_status_for_first_transaction = "P";
+                    }
+                    $count_transactions = OrderTransactionModel::objects()
+                        ->filter(['orderid' => $orderid])
+                        ->exclude(['transaction_status' => '', 'transaction_id' => ''])
+                        ->count();
+                    $allowed_statuses_flag = func_check_for_the_allowed_statuses_for_create_payment($order);
+                    if (!$allowed_statuses_flag && empty($count_transactions) && (empty($AJAX_SUBMIT) || $AJAX_SUBMIT != "Y")) {
+                        $top_message = array(
+                            'type' => 'E',
+                            'content' => func_get_langvar_by_name("lbl_first_transaction_in_order_exception")
+                        );
+                        $section_name_top_message = $top_message;
+                        x_session_save("section_name_top_message");
+                        func_header_location("order.php?orderid=" . $orderid . "&tab=y#main_order_tabs-VT");
+                    }
+                    if ($allowed_statuses_flag && empty($count_transactions) && (empty($AJAX_SUBMIT) || $AJAX_SUBMIT != "Y")) {
+                        $new_cb_status_flag = false;
+                        $new_cb_status_value = func_query_first_cell("SELECT name FROM $sql_tbl[order_statuses] WHERE code='$set_cb_status_for_first_transaction'");
+                        foreach ($order["shipping_groups"] as $ko => $vo) {
+                            if (in_array($vo["cb_status"], array('Q', 'N', 'I'))) {
+                                db_query("UPDATE $sql_tbl[order_groups] SET cb_status='$set_cb_status_for_first_transaction' WHERE orderid='$orderid' AND manufacturerid='$ko'");
+                                $current_cb_status_value = func_query_first_cell("SELECT name FROM $sql_tbl[order_statuses] WHERE code='" . $vo["cb_status"] . "'");
+                                $log .= "<br /><B>" . $vo["all_distributor_info"]["code"] . ":</B> cb_status: " . $current_cb_status_value . " -> " . $new_cb_status_value . "<br />";
+                                $new_cb_status_flag = true;
+                            }
+                        }
+                        if ($new_cb_status_flag) {
+                            db_query("UPDATE $sql_tbl[orders] SET cb_status='$set_cb_status_for_first_transaction' WHERE orderid='$orderid'");
+                            func_send_order_status_notification($orderid, $set_cb_status_for_first_transaction);
+                        }
+                    }
+                    $result = func_paypal_look_up_payment($Access_Token, $transaction_id, $transaction_type);
+                    $transaction_total = $transaction_amount;
+                    $result["FIELD_manual_transaction"] = "Y";
+                    $result["FIELD_avs_code"] = $avs_code;
+                    $log .= "'Add transaction' at 'Add manual transaction' section";
+                }
+            }
+        } else {
+            if (!$countTr && (empty($AJAX_SUBMIT) || $AJAX_SUBMIT != "Y")) {
+                $section_name_top_message = [
+                    'type' => 'E',
+                    'content' => func_get_langvar_by_name("lbl_first_transaction_in_order_exception")
+                ];
+                x_session_save("section_name_top_message");
+                func_header_location("order.php?orderid=" . $orderid . "&tab=y#main_order_tabs-VT");
             }
         }
-    } else {
-        if (!$countTr && (empty($AJAX_SUBMIT) || $AJAX_SUBMIT != "Y")) {
-            $section_name_top_message = [
-                'type' => 'E',
-                'content' => func_get_langvar_by_name("lbl_first_transaction_in_order_exception")
-            ];
-            x_session_save("section_name_top_message");
-            func_header_location("order.php?orderid=" . $orderid . "&tab=y#main_order_tabs-VT");
-        }
-    }
+    } catch (\Exctetption $e) {
 
+    }
 
 
     $result["xcart_log"] = $log;
