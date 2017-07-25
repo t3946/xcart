f92cd925d77952fce62671732ceb7c51544dbf61
Roman Novokshonov <kshon@mail.ru>
2017-07-25 10:30:01 +0300

    GMC shipping label

--- include/func/func.froogle.php
+++ include/func/func.froogle.php
@@ -1364,6 +1364,8 @@ function SubmitGoogleProductsBatch($gproducts, $service, $MerchantID, $debug_mod
                 $postBody["entries"][$k_counter]["product"]["targetCountry"] = "US";
                 $postBody["entries"][$k_counter]["product"]["channel"] = "online";
 
+                $postBody["entries"][$k_counter]["product"]["shippinglabel"] = $product_info["product"]["shipping_label"];
+
 
 				$product_availability = func_product_availability(false,$product_info["product"]);
 
