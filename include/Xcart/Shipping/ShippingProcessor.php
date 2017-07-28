34f5e6f1124c704a2fe6818a162064db5b1bfe80
Roman Novokshonov <kshon@mail.ru>
2017-07-28 13:12:58 +0300

    up

--- include/Xcart/Shipping/ShippingProcessor.php
+++ include/Xcart/Shipping/ShippingProcessor.php
@@ -84,7 +84,7 @@ abstract class ShippingProcessor
                     foreach ($this->aShippingRates as $oShippingRate) {
                         $oShippingRate->setCart($this->getCart());
                     }
-                    usort($this->aShippingRates, [get_called_class(), 'sortShippingRatesByCostAsc']);
+                    usort($this->aShippingRates, ['\Xcart\Shipping\ShippingProcessor', 'sortShippingRatesByCostAsc']);
                     $this->removeFromCart();
                 }
             }
