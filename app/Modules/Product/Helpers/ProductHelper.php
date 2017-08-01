876c2ac60b87cd919993c731a338ab4282368b71
Roman Novokshonov <kshon@mail.ru>
2017-08-01 11:14:15 +0300

    html entities decode

--- app/Modules/Product/Helpers/ProductHelper.php
+++ app/Modules/Product/Helpers/ProductHelper.php
@@ -32,7 +32,7 @@ class ProductHelper
     public static function getFileNameFromDownloadLink($imgLink, $allowExtensions = [], $defaultExtension)
     {
         $result = null;
-        $path = parse_url(strtolower($imgLink));
+        $path = parse_url(strtolower(html_entity_decode($imgLink)));
         if (!empty($path)) {
             $fileName = basename($path['path']);
             $filename = pathinfo($fileName);
