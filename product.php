d23c7c0b12a914833a7894db2a2c3d462db9df62
Roman Novokshonov <kshon@mail.ru>
2017-08-07 10:42:55 +0300

    fix calculate shipping non amazon products

--- product.php
+++ product.php
@@ -1,10 +1,6 @@
 <?php
 
 
-use Modules\Core\Helpers\GeoipHelper;
-use Modules\Distributor\Models\DistributorModel;
-use Modules\Shipping\Models\ZoneElementModel;
-
 define('OFFERS_DONT_SHOW_NEW',1);
 require "./auth.php";
 
@@ -81,7 +77,7 @@ if (empty($product_info)) {
 	func_header_location("search.php?substring=".urlencode($sku)."&by_sku=1&mode=search&from=fast_search");
 }
 
-$oProduct = Xcart\Product::model(['productid'=>$product_info['productid']]);
+$oProduct = Modules\Product\Models\ProductModel::objects()->get(['productid'=>$product_info['productid']]);
 
 $reverse_sku = func_query_first_cell("SELECT reverse_sku FROM $sql_tbl[manufacturers] WHERE manufacturerid='$product_info[manufacturerid]'");
 $remove_dashes = func_query_first_cell("SELECT remove_dashes FROM $sql_tbl[manufacturers] WHERE manufacturerid='$product_info[manufacturerid]'");
