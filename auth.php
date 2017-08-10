7baff03e1722815291eadb2a5e6fa5ec88757aee
Roman Novokshonov <kshon@mail.ru>
2017-08-10 09:58:26 +0300

    add checking close shop method

--- auth.php
+++ auth.php
@@ -72,13 +72,12 @@ if (!empty($_GET['shopkey'])) {
 }
 
 if ($config["General"]["shop_closed"] == "Y" && !$always_allow_shop){
-	#
-	# Close store front
-	# Thanks to rubyaryat for the Shop Closed mod
-	#
-	if (!func_readfile($xcart_dir.DIRECTORY_SEPARATOR.$shop_closed_file, true))
-		echo func_get_langvar_by_name("txt_shop_temporarily_unaccessible",false,false,true);
-	exit();
+	if (!$config["General"]["shop_closed_method"]) {
+        if (!func_readfile($xcart_dir.DIRECTORY_SEPARATOR.$shop_closed_file, true)) {
+            echo func_get_langvar_by_name("txt_shop_temporarily_unaccessible",false,false,true);
+            exit();
+        }
+    }
 }
 
 ###
