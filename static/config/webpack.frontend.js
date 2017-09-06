4ea64d97ac898126464b725fbc06e8cb18f4ca44
Maksim K <dog.mixer@gmail.com>
2017-09-06 16:06:33 +0300

    up

--- static/config/webpack.frontend.js
+++ static/config/webpack.frontend.js
@@ -46,7 +46,7 @@ config = {
         rules: [
             {
                 test: /\.(js|jsx)?$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
--- static/frontend/jsx/ext/foundation_init.js
+++ static/frontend/jsx/ext/foundation_init.js
@@ -1,15 +1,5 @@
-import $ from 'jquery';
-
 import { Foundation } from 'bower_components/foundation-sites/js/foundation.core';
-Foundation.addToJquery($);
-
-// Add Foundation Utils to Foundation global namespace for backwards
-// compatibility.
-
 import { rtl, GetYoDigits, transitionend } from 'bower_components/foundation-sites/js/foundation.util.core';
-Foundation.rtl = rtl;
-Foundation.GetYoDigits = GetYoDigits;
-Foundation.transitionend = transitionend;
 
 import { Box } from 'bower_components/foundation-sites/js/foundation.util.box'
 import { onImagesLoaded } from 'bower_components/foundation-sites/js/foundation.util.imageLoader';
@@ -18,84 +8,77 @@ import { MediaQuery } from 'bower_components/foundation-sites/js/foundation.util
 // import { Motion, Move } from 'bower_components/foundation-sites/js/foundation.util.motion';
 // import { Nest } from 'bower_components/foundation-sites/js/foundation.util.nest';
 // import { Timer } from 'bower_components/foundation-sites/js/foundation.util.timer';
-
-Foundation.Box = Box;
-Foundation.onImagesLoaded = onImagesLoaded;
-Foundation.Keyboard = Keyboard;
-Foundation.MediaQuery = MediaQuery;
-// Foundation.Motion = Motion;
-// Foundation.Move = Move;
-// Foundation.Nest = Nest;
-// Foundation.Timer = Timer;
-
-// Touch and Triggers previously were almost purely sede effect driven,
-// so no // need to add it to Foundation, just init them.
-
 // import { Touch } from 'bower_components/foundation-sites/js/foundation.util.touch';
-// Touch.init($);
-
 import { Triggers } from 'bower_components/foundation-sites/js/foundation.util.triggers';
-Triggers.init($, Foundation);
 
 // import { Abide } from 'bower_components/foundation-sites/js/foundation.abide';
-// Foundation.plugin(Abide, 'Abide');
-
 import { Accordion } from 'bower_components/foundation-sites/js/foundation.accordion';
-Foundation.plugin(Accordion, 'Accordion');
-
 // import { AccordionMenu } from 'bower_components/foundation-sites/js/foundation.accordionMenu';
-// Foundation.plugin(AccordionMenu, 'AccordionMenu');
-
 // import { Drilldown } from 'bower_components/foundation-sites/js/foundation.drilldown';
-// Foundation.plugin(Drilldown, 'Drilldown');
-
 // import { Dropdown } from 'bower_components/foundation-sites/js/foundation.dropdown';
-// Foundation.plugin(Dropdown, 'Dropdown');
-
 // import { DropdownMenu } from 'bower_components/foundation-sites/js/foundation.dropdownMenu';
-// Foundation.plugin(DropdownMenu, 'DropdownMenu');
-
 // import { Equalizer } from 'bower_components/foundation-sites/js/foundation.equalizer';
-// Foundation.plugin(Equalizer, 'Equalizer');
-
 // import { Interchange } from 'bower_components/foundation-sites/js/foundation.interchange';
-// Foundation.plugin(Interchange, 'Interchange');
-
 // import { Magellan } from 'bower_components/foundation-sites/js/foundation.magellan';
-// Foundation.plugin(Magellan, 'Magellan');
-
 import { OffCanvas } from 'bower_components/foundation-sites/js/foundation.offcanvas';
-Foundation.plugin(OffCanvas, 'OffCanvas');
-
 // import { Orbit } from 'bower_components/foundation-sites/js/foundation.orbit';
-// Foundation.plugin(Orbit, 'Orbit');
-
 // import { ResponsiveMenu } from 'bower_components/foundation-sites/js/foundation.responsiveMenu';
-// Foundation.plugin(ResponsiveMenu, 'ResponsiveMenu');
-
 // import { ResponsiveToggle } from 'bower_components/foundation-sites/js/foundation.responsiveToggle';
-// Foundation.plugin(ResponsiveToggle, 'ResponsiveToggle');
-
 // import { Reveal } from 'bower_components/foundation-sites/js/foundation.reveal';
-// Foundation.plugin(Reveal, 'Reveal');
-
 // import { Slider } from 'bower_components/foundation-sites/js/foundation.slider';
-// Foundation.plugin(Slider, 'Slider');
-
 // import { SmoothScroll } from 'bower_components/foundation-sites/js/foundation.smoothScroll';
-// Foundation.plugin(SmoothScroll, 'SmoothScroll');
-
 import { Sticky } from 'bower_components/foundation-sites/js/foundation.sticky';
-Foundation.plugin(Sticky, 'Sticky');
-
 import { Tabs } from 'bower_components/foundation-sites/js/foundation.tabs';
-Foundation.plugin(Tabs, 'Tabs');
-
 // import { Toggler } from 'bower_components/foundation-sites/js/foundation.toggler';
-// Foundation.plugin(Toggler, 'Toggler');
-
 // import { Tooltip } from 'bower_components/foundation-sites/js/foundation.tooltip';
-// Foundation.plugin(Tooltip, 'Tooltip');
-
 // import { ResponsiveAccordionTabs } from 'bower_components/foundation-sites/js/foundation.responsiveAccordionTabs';
-// Foundation.plugin(ResponsiveAccordionTabs, 'ResponsiveAccordionTabs');
+
+(()=>{
+    Foundation.addToJquery($);
+
+    // Add Foundation Utils to Foundation global namespace for backwards
+    // compatibility.
+
+    Foundation.rtl = rtl;
+    Foundation.GetYoDigits = GetYoDigits;
+    Foundation.transitionend = transitionend;
+
+    Foundation.Box = Box;
+    Foundation.onImagesLoaded = onImagesLoaded;
+    Foundation.Keyboard = Keyboard;
+    Foundation.MediaQuery = MediaQuery;
+    // Foundation.Motion = Motion;
+    // Foundation.Move = Move;
+    // Foundation.Nest = Nest;
+    // Foundation.Timer = Timer;
+
+    // Touch and Triggers previously were almost purely sede effect driven,
+    // so no // need to add it to Foundation, just init them.
+
+    // Touch.init($);
+    Triggers.init($, Foundation);
+
+    // Foundation.plugin(Abide, 'Abide');
+    Foundation.plugin(Accordion, 'Accordion');
+    // Foundation.plugin(AccordionMenu, 'AccordionMenu');
+    // Foundation.plugin(Drilldown, 'Drilldown');
+    // Foundation.plugin(Dropdown, 'Dropdown');
+    // Foundation.plugin(DropdownMenu, 'DropdownMenu');
+    // Foundation.plugin(Equalizer, 'Equalizer');
+    // Foundation.plugin(Interchange, 'Interchange');
+    // Foundation.plugin(Magellan, 'Magellan');
+    Foundation.plugin(OffCanvas, 'OffCanvas');
+    // Foundation.plugin(Orbit, 'Orbit');
+    // Foundation.plugin(ResponsiveMenu, 'ResponsiveMenu');
+    // Foundation.plugin(ResponsiveToggle, 'ResponsiveToggle');
+    // Foundation.plugin(Reveal, 'Reveal');
+    // Foundation.plugin(Slider, 'Slider');
+    // Foundation.plugin(SmoothScroll, 'SmoothScroll');
+    Foundation.plugin(Sticky, 'Sticky');
+    Foundation.plugin(Tabs, 'Tabs');
+    // Foundation.plugin(Toggler, 'Toggler');
+    // Foundation.plugin(Tooltip, 'Tooltip');
+    // Foundation.plugin(ResponsiveAccordionTabs, 'ResponsiveAccordionTabs');
+
+    window.Foundation = Foundation;
+})($);
