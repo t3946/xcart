e49090317b3040e747b53902403ccca68a9e1d90
Nikolay Meleshev <nikolay@s3stores.com>
2017-10-30 10:21:10 +0300

    fix line 43

--- cron/cron_new_approximation_shipping_rates.php
+++ cron/cron_new_approximation_shipping_rates.php
@@ -40,7 +40,7 @@ if (!empty($tmp_manufacturers)) {
 //		$date_shipping_rates_last_update_date = date("j", $shipping_rates_last_update_date);
 
         $m_val = ($manufacturerid - 1) / ($max_manufacturerid - 1);
-        $IDNorm = 30 * $m_val + 1;
+        $IDNorm = 27 * $m_val + 1;
         $IDNorm = intval($IDNorm);
 
 //func_print_r($IDNorm, $date_shipping_rates_last_update_date, $manufacturerid);
