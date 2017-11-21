7d9afc5bde231c88248f34fe882eada2daa58c15
Maksim K <dog.mixer@gmail.com>
2017-11-21 15:11:01 +0300

    pbx up to AutoMetaTrait

--- app/Modules/Order/Models/OrdersCallsModel.php
+++ app/Modules/Order/Models/OrdersCallsModel.php
@@ -3,13 +3,16 @@
 namespace Modules\Order\Models;
 
 use Modules\PBX\Models\PbxAnveoCallModel;
-use Xcart\App\Orm\AutoMetaModel;
+use Xcart\App\Orm\AutoMetaTrait;
 use Xcart\App\Orm\Fields\CharField;
 use Xcart\App\Orm\Fields\ForeignField;
 use Xcart\App\Orm\Fields\IntField;
+use Xcart\App\Orm\Model;
 
-class OrdersCallsModel extends AutoMetaModel
+class OrdersCallsModel extends Model
 {
+    use AutoMetaTrait;
+
     public static function tableName()
     {
         return 'orders_calls';
