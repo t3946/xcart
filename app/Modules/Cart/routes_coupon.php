210fef13c5897168b75c0faa1817777630efecaf
Maksim K <dog.mixer@gmail.com>
2017-11-23 15:04:17 +0300

    fix

--- app/Modules/Cart/routes_coupon.php
+++ app/Modules/Cart/routes_coupon.php
@@ -1,7 +1,7 @@
 <?php
 return [
     [
-        'route' => '/{:code}',
+        'route' => '/{:code}/',
         'target' => ['\Modules\Cart\Controllers\CouponController', 'actionView'],
         'name' => 'view'
     ],
