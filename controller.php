ca360913d78dfabf719896b4f41fb83f74fb2a33
Maksim K <dog.mixer@gmail.com>
2017-11-23 16:24:07 +0300

    temporary/disabled integration in frontend

--- controller.php
+++ controller.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL ^ E_DEPRECATED);
+//error_reporting(E_ALL ^ E_DEPRECATED);
 date_default_timezone_set('EST');
 
 defined('XCART_APP') ?: define('XCART_APP', 1);
@@ -7,10 +7,11 @@ defined('XCART_EXT_ENV') ?: define('XCART_EXT_ENV', 1);
 
 $XCART_APP_CONFIG = include './app/config/settings_controller.php';
 
-require "./auth.php";
+require_once './include/libs/autoload.php';
+//require_once "./auth.php";
 
 use Xcart\App\Main\Xcart;
 
-//Xcart::init($config);
-//Xcart::app()->run();
-Xcart::app()->handleRequest();
+Xcart::init($XCART_APP_CONFIG);
+Xcart::app()->run();
+//Xcart::app()->handleRequest();
