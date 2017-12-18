e5397c8f5ca17ed94f628a50dcc68af959835319
Roman Novokshonov <kshon@mail.ru>
2017-12-18 14:21:25 +0300

    up

--- cleanup.php
+++ cleanup.php
@@ -9,6 +9,8 @@ $smarty->clear_compiled_tpl();
 \Xcart\App\Main\Xcart::app()->cache->cleanUp(true);
 \Xcart\App\Main\Xcart::app()->template->getRenderer()->clearAllCompiles();
 
-\Modules\Amazon\Helpers\AmazonAWSHelper::invalidateCDN();
+if (!empty($_GET['cdn'])) {
+    \Modules\Amazon\Helpers\AmazonAWSHelper::invalidateCDN();
+}
 ?>
 The compiled templates cache ("templates_c" directory) has been cleaned up.
