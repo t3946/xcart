d7ac1cad6c98fd53b21a0cafa66d49cbfb78b8fe
Maksim K <dog.mixer@gmail.com>
2017-12-18 22:26:53 +0300

    test

--- include/Xcart/ElasticSearch.php
+++ include/Xcart/ElasticSearch.php
@@ -268,6 +268,7 @@ JSON;
     }
     //curl -X GET http://localhost:9200/{INDEX}/{TYPE}/_search?q= ...
     function query($q = array()){
+        return [];
         return $this->call($this->type . '/_search?' . http_build_query($q), array('method' => 'POST', 'content' => $this->queryParams));
     }
 }
