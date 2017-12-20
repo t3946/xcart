fd83341374d42fd5bc829d0160d20b3eea4b94db
Roman Novokshonov <kshon@mail.ru>
2017-12-20 13:46:31 +0300

    up

--- app/Modules/Goods/Stores/SupplierFeedStore.php
+++ app/Modules/Goods/Stores/SupplierFeedStore.php
@@ -123,10 +123,10 @@ class SupplierFeedStore extends BaseStore
     public static function replaceFeedFields($data)
     {
         $data['productcode'] = strtoupper(trim(!isset($data['sku']) ? $data['productcode'] : $data['sku']));
-        $data['r_avail'] = (string) !isset($data['quantity']) ? $data['r_avail'] : $data['quantity'];
+        $data['r_avail'] = !isset($data['quantity']) ? $data['r_avail'] : $data['quantity'];
         $data['eta_date_mm_dd_yyyy'] = !isset($data['eta_date']) ? $data['eta_date_mm_dd_yyyy'] : $data['eta_date'];
         $data['product'] = !isset($data['title']) ? $data['product'] : $data['title'];
-        $data['list_price'] = (string) round(!isset($data['listprice']) ? $data['list_price'] : $data['listprice'], 2, PHP_ROUND_HALF_UP);
+        $data['list_price'] = !isset($data['listprice']) ? $data['list_price'] : $data['listprice'];
 
         $data = array_filter($data, function ($v) {
             return !is_null($v);
