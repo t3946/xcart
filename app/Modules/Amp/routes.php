07d048ac989da97f9e6df0c83065a8c3a929cbec
Maksim K <dog.mixer@gmail.com>
2018-01-09 15:24:22 +0300

    fix: amp

--- app/Modules/Amp/routes.php
+++ app/Modules/Amp/routes.php
@@ -2,6 +2,10 @@
 
 return [
     [
+        'route' => '/product/{i:id}/{*:slug}',
+        'target' => ['\Modules\Amp\Controllers\AmpController', 'index'],
+    ],
+    [
         'route' => '/product/{i:id}/{*:slug}/',
         'target' => ['\Modules\Amp\Controllers\AmpController', 'amp'],
         'name' => 'product'
