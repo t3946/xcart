2c0f54812b46fd0b0491455bbd490317fd5f2856
Maksim K <dog.mixer@gmail.com>
2018-01-10 18:48:43 +0300

    up

--- www/modules/Multiple_Storefronts/init.php
+++ www/modules/Multiple_Storefronts/init.php
@@ -58,13 +58,13 @@ if ($storefronts) {
 
     $sd_selects = [];
 //    $domains = Modules\Sites\Models\SiteModel::objects()->filter(['config__name__isnt' => null])->allSql();
-    $t_domains = Modules\Sites\Models\SiteModel::objects()->all();
-    /** @var Modules\Sites\Models\SiteModel $t_domains */
-    foreach ($t_domains as $domain) {
-        $sd_selects[$t_domains->storefrontid] = $t_domains->__toString();
-    }
-    $smarty->assign('sd_selects', $sd_selects);
-}
+//    $t_domains = Modules\Sites\Models\SiteModel::objects()->all();
+//    /** @var Modules\Sites\Models\SiteModel $t_domains */
+//    foreach ($t_domains as $domain) {
+//        $sd_selects[$t_domains->storefrontid] = $t_domains->__toString();
+//    }
+//    $smarty->assign('sd_selects', $sd_selects);
+//}
 
 if ($search_all_website) {
     return;
