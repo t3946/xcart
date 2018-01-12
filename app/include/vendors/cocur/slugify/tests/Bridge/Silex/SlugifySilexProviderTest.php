30cade58ce5504c9ea33865ff67e006eff8cfa1c
Maksim K <dog.mixer@gmail.com>
2018-01-12 20:00:25 +0300

    up sluggify

--- app/include/vendors/cocur/slugify/tests/Bridge/Silex/SlugifySilexProviderTest.php
+++ app/include/vendors/cocur/slugify/tests/Bridge/Silex/SlugifySilexProviderTest.php
@@ -9,9 +9,10 @@
  * file that was distributed with this source code.
  */
 
-namespace Cocur\Slugify\Bridge\Silex;
+namespace Cocur\Slugify\Tests\Bridge\Silex;
 
 use Cocur\Slugify\Bridge\Silex\SlugifyServiceProvider;
+use Cocur\Slugify\Bridge\Twig\SlugifyExtension;
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 
@@ -26,7 +27,7 @@ use Silex\Provider\TwigServiceProvider;
  * @license    http://www.opensource.org/licenses/MIT The MIT License
  * @group      unit
  */
-class SlugifyServiceProviderTest extends \PHPUnit_Framework_TestCase
+class SlugifySilexProviderTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @test
@@ -40,7 +41,7 @@ class SlugifyServiceProviderTest extends \PHPUnit_Framework_TestCase
         $app->boot();
 
         $this->assertArrayHasKey('slugify', $app);
-        $this->assertArrayHasKey('slugify.regex', $app);
+        $this->assertArrayHasKey('slugify.provider', $app);
         $this->assertArrayHasKey('slugify.options', $app);
         $this->assertInstanceOf('Cocur\Slugify\Slugify', $app['slugify']);
     }
@@ -54,6 +55,6 @@ class SlugifyServiceProviderTest extends \PHPUnit_Framework_TestCase
         $app->register(new TwigServiceProvider());
         $app->register(new SlugifyServiceProvider());
 
-        $this->assertTrue($app['twig']->hasExtension('slugify_extension'));
+        $this->assertTrue($app['twig']->hasExtension(SlugifyExtension::class));
     }
 }
