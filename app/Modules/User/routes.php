a9db4008c65ec30b10c7626dceafb4f6709957fd
Roman Novokshonov <kshon@mail.ru>
2018-01-16 15:15:24 +0300

    up

--- app/Modules/User/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    [
-        'route' => '/remember_admin_user/{*:slug}',
-        'target' => ['\Modules\User\Controllers\UserController', 'remember_admin_user'],
-        'name' => 'remember_admin_user'
-    ],
-];
