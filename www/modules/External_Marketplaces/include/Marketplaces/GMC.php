0383c299426043f450172465f42a3635f0eb7a43
Roman Novokshonov <kshon@mail.ru>
2018-01-23 10:00:39 +0300

    remove logging

--- www/modules/External_Marketplaces/include/Marketplaces/GMC.php
+++ www/modules/External_Marketplaces/include/Marketplaces/GMC.php
@@ -116,9 +116,9 @@ class GMC extends StoreFrontMarketPlace
     {
         $iUpdateProductCount = $iNewIssues = $totalCounter = 0;
         $pageToken = null;
-        $log = new Logger('gmc_info');
+        /*$log = new Logger('gmc_info');
         $logFile = sprintf("../var/log/gmc_products-{$iStoreFrontId}-%s.php", date('ymd'));
-        $log->pushHandler(new StreamHandler($logFile, Logger::INFO));
+        $log->pushHandler(new StreamHandler($logFile, Logger::INFO));*/
         do {
             $oResponse = null;
             if (!empty($pageToken)) {
