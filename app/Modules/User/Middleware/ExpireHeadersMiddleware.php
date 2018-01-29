8cf72158a6e13fd451359da7d1cdbdae9da0cb99
Maksim K <dog.mixer@gmail.com>
2018-01-29 18:46:11 +0300

    up cache headers up

--- app/Modules/User/Middleware/ExpireHeadersMiddleware.php
+++ app/Modules/User/Middleware/ExpireHeadersMiddleware.php
@@ -13,13 +13,10 @@ class ExpireHeadersMiddleware extends Middleware
 
     public function processHttpRequest($request)
     {
-
         if (!headers_sent()) {
-
-            header("Vary: User-Agent");
 //            header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-            $this->noCache();
-            return;
+//            $this->noCache();
+//            return;
 
             if (defined('AREA_TYPE') && AREA_TYPE == 'A') {
                 $this->noCache();
@@ -29,10 +26,11 @@ class ExpireHeadersMiddleware extends Middleware
                     $this->noCache();
                 }
                 else {
-
-                    $request->getIsSecureConnection() ?
-                        header("Cache-Control: private, max-age=3600, must-revalidate") :
-                        header("Cache-Control: public, max-age=3600");
+                    if (!defined('SET_EXPIRE')) {
+                        $request->getIsSecureConnection() ?
+                            header("Cache-Control: private, max-age=3600, must-revalidate") :
+                            header("Cache-Control: public, max-age=3600");
+                    }
 
                     !defined("SET_EXPIRE") ?:
                         header("Expires: " . gmdate("D, d M Y H:i:s", SET_EXPIRE) . " GMT"); // is defined
@@ -46,6 +44,8 @@ class ExpireHeadersMiddleware extends Middleware
         header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
         header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
         header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
-        header("Pragma: no-cache");
+
+//        header("Vary: User-Agent");
+//        header("Pragma: no-cache");
     }
 }
