9d7d9d8dfc475f877a8bd59ec345ed0bcc442faa
Maksim K <dog.mixer@gmail.com>
2018-01-30 19:14:42 +0300

    up: otrmessages update on event

--- app/Modules/Order/Models/OrderEventsModel.php
+++ app/Modules/Order/Models/OrderEventsModel.php
@@ -74,7 +74,9 @@ class OrderEventsModel extends Model
             $model->setAttributes(['order_id' => $order_id, 'message' => $message, 'user_id' => $user_id]);
 
             //Cache UP
-//            $model->order->getOTRSTicketMessages();
+            if (rand(0,10) > 6) {
+                $model->order->getOTRSTicketMessages();
+            }
 
             if ($model->isValid() && $model->save())
             {
