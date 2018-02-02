97f950631119ba24c6ca7156278221dcc085819d
Maksim K <dog.mixer@gmail.com>
2018-02-02 12:55:11 +0300

    fix: filters

--- www/modules/CIDEV_Best_Search_Filter/filter_init.php
+++ www/modules/CIDEV_Best_Search_Filter/filter_init.php
@@ -33,7 +33,7 @@ if (!empty($b_ids)) {
     );
 }
 
-if (!$aFilterValues = \Xcart\App\Main\Xcart::app()->cache->get("oFilter->getMoreBrands:st.{$current_storefront}:cat.{$cat}:f_id.{$f_id}")) {
+if (!$aFilterValues = \Xcart\App\Main\Xcart::app()->cache->get("oFilter->getMoreBrands:st.{$current_storefront}:cat.{$cat}")) {
     $aFilterValues = $oFilter->getMoreBrands();
     \Xcart\App\Main\Xcart::app()->cache->set('$oFilter->getMoreBrands', $aFilterValues);
 }
