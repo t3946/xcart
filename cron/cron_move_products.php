0ac86e0a6ab82682c25ab1e1b03bb744b520f067
Nikolay Meleshev <nikolay@s3stores.com>
2018-02-22 12:28:03 +0300

    up

--- cron/cron_move_products.php
+++ cron/cron_move_products.php
@@ -12,11 +12,28 @@ use Modules\Goods\Models\ProductsSfMovesModel;
 use Modules\Goods\Models\ProductStorefrontModel;
 use Modules\Goods\Models\UpdatedProductModel;
 
-$models = UpdatedProductModel::objects()->filter(['type' => 2])->all();
+$models = UpdatedProductModel::objects()->filter(['type' => 9])->all();
+
+$start_time = new DateTime('now');
+$count_all = count($models);
+$count_processed = 0;
 
 foreach ($models as $model){
+
+    /** @var UpdatedProductModel $model */
     $product_id = $model->resourceid;
     $storefront_id = $model->extra_data_int;
+
+
+    /** @var ProductModel $product_model */
+    $product_model = ProductModel::objects()->get(['productid' => $model->resourceid]);
+
+    if ($product_model->isGroupRoot() || $product_model->isGroupChild()){
+        echo "It's group product";
+        exit;
+    }
+
+/*    $model = ProductsSfMovesModel::objects()->filter(['productid' => $product_id])->order('batch_id')->desc->limit(1);*/
 
     /** @var ProductsSfMovesModel [] $p_moves_models */
     if ($p_moves_models = ProductsSfMovesModel::objects()->filter(['productid' => $model->resourceid, 'resource_type' => "SF"])->all()){
@@ -28,9 +45,10 @@ foreach ($models as $model){
         $p_moves_model = new ProductsSfMovesModel(['batch_id' => $batch_id, 'productid' => $model->resourceid]);
     }
 
-    /** @var ProductModel $product_model */
-    $product_model = ProductModel::objects()->get(['productid' => $model->resourceid]);
-    $product_model->update(['source_sfid' => $model->extra_data_int]);
+
+    $product_model->source_sfid = $model->extra_data_int;
+    $product_model->save();
+
 
     /** @var ProductStorefrontModel [] $products_sf_models */
     $products_sf_models = ProductStorefrontModel::objects()->filter(['productid' => $model->resourceid])->all();
@@ -38,10 +56,17 @@ foreach ($models as $model){
     if (count($products_sf_models) > 1){
         continue;
     }
+    /** @var ProductStorefrontModel $products_sf_models */
+    $products_sf_model = $products_sf_models[0];
+    $old_sfid = $products_sf_model->sfid;
+    $products_sf_model->sfid = $model->extra_data_int;
+    $products_sf_model->save();
 
-    (new ProductsSfMovesModel(['batch_id' => $batch_id, 'productid' => $model->resourceid, 'resource_id' => $products_sf_models[0]->source_sfid, 'resource_type' => "SF"]))->save();
+    $products_sf_model->sfid = $old_sfid;
+    $products_sf_model->delete();
 
-    $products_sf_models[0]->update(['sfid' => $model->extra_data_int]);
+
+    (new ProductsSfMovesModel(['batch_id' => $batch_id, 'productid' => $model->resourceid, 'resource_id' => $products_sf_models[0]->sfid, 'resource_type' => "SF"]))->save();
 
 
     /** @var ProductCategoriesModel $categories_models */
@@ -51,6 +76,23 @@ foreach ($models as $model){
 
     ProductsToMoveHelper::processingFilterAndValuesToNewSf($model->resourceid, $model->extra_data_int, $batch_id);
 
+    $model->delete();
+
+    $count_processed++;
+
 }
 
+$time = (new DateTime('now'))->diff($start_time)->format('%H:%I:%S');
+
+$log_category = "products_sf_moves";
+$log_text = "Время обработки = {$time}\r\n Всего продуктов обработано = {$count_all}\r\n Продуктов перенесено = {$count_processed}";
+func_backprocess_log($log_category, $log_text);
 $breakpoint = 1;
+
+/*
+ * статистика сессии крона - записывается в backprocess logs для process_id = products_sf_moves
+итоги:
+время обработки
+всего продуктов обработано
+продуктов перенесено (включая групповые)
+ */
