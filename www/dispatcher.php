29686baa2f7ebce37669eeace570b62effc7c349
Maksim K <dog.mixer@gmail.com>
2018-02-28 15:52:19 +0300

    up

--- www/dispatcher.php
+++ www/dispatcher.php
@@ -322,6 +322,9 @@ case 'P':
     $QUERY_STRING = 'productid=' . $productid . (!empty($QUERY_STRING) ? '&' . $QUERY_STRING : '');
     $PHP_SELF = dirname($PHP_SELF) . '/product.php';
 
+    Templater::getInstance()->caching = 2;
+    Templater::getInstance()->cache_lifetime = \Modules\Core\Helpers\Cache::CACHE_DAY;
+
     if ( Templater::getInstance()->is_cached("customer/home.tpl", "product_" . $productid)) {
         func_display_cached("customer/home.tpl", "product_" . $productid);
         die();
