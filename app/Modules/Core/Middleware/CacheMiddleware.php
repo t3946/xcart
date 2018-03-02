04f97dfdc5739d7c6777e5884d7a63eee29bc134
Maksim K <dog.mixer@gmail.com>
2018-03-02 20:01:59 +0300

    up

--- app/Modules/Core/Middleware/CacheMiddleware.php
+++ app/Modules/Core/Middleware/CacheMiddleware.php
@@ -140,7 +140,7 @@ class CacheMiddleware extends Middleware
                 $isMobile = true;
             }
 
-            if ( in_array($request->getPath(),['', '/', '/home.php'])) {
+            if ( empty($request->getQueryArray()) && in_array($request->getPath(),['', '/', '/home.php'])) {
                 $key = 'home-'. $request->getHost() . '-' . rand(1, 10);
 
                 if ($isMobile) {
