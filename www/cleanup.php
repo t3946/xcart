65a27606dde742cd78b171c05cd6124cfa2729b1
Maksim K <dog.mixer@gmail.com>
2018-03-05 13:57:06 +0300

    up

--- www/cleanup.php
+++ www/cleanup.php
@@ -8,7 +8,7 @@ if (empty($_GET['only_cdn'])) {
     $smarty->clear_all_cache();
     $smarty->clear_compiled_tpl();
 
-    \Xcart\App\Main\Xcart::app()->cache->cleanUp(true);
+    \Xcart\App\Main\Xcart::app()->cache->cleanUp(true, ['html']);
     \Xcart\App\Main\Xcart::app()->template->getRenderer()->clearAllCompiles();
 }
 
