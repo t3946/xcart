af405cd88a73582b5ffa164aa04b7393fdcebacc
Maksim K <dog.mixer@gmail.com>
2018-03-07 12:54:34 +0300

    cache up

--- app/include/Xcart/App/Traits/CacheFilesTrait.php
+++ app/include/Xcart/App/Traits/CacheFilesTrait.php
@@ -19,15 +19,12 @@ trait CacheFilesTrait
 
     public function set($key, $value, $timeout = null)
     {
-        $timeout = $timeout ?: $this->timeout;
-
         if (is_null($value)) {
             @unlink($this->getFileName($this->buildKey($key)));
             return true;
         }
         else {
-
-            return $this->setValue($this->buildKey($key), $this->serialize($value), $timeout);
+            return $this->setValue($this->buildKey($key), $this->serialize($value), $timeout ?: $this->timeout);
         }
     }
 
