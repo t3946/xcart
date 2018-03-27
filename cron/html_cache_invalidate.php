4a21db69a2698e230e8bb2631c5f1fdac2ac75ad
Maksim K <dog.mixer@gmail.com>
2018-03-27 16:12:41 +0300

    up html invalidate 418 cookie

--- cron/html_cache_invalidate.php
+++ cron/html_cache_invalidate.php
@@ -22,7 +22,7 @@ const DATEFORMAT = '%H:%I:%S';
 const LIMIT = 5000;
 const TIME_PRODUCTS_LIMIT = 113;
 
-
+$cookie = new \GuzzleHttp\Cookie\CookieJar(false, ['418' => "I'm a teapot"]);
 $guzzle = new \GuzzleHttp\Client();
 $date = new DateTime();
 $time = time();
@@ -111,8 +111,8 @@ if ($resources_count = getResourcesCount()) {
                     $ssl = ($site->getConfig()['https_enabled'] == 'Y');
                     $url = ($ssl ? 'https' : 'http') . '://' . $site->domain  . $model->getAbsoluteUrl();
 
-                    $requests[] = $guzzle->createRequest('GET', $url, ['headers' => ['User-Agent' => desktop_user_agent]]);
-                    $requests[] = $guzzle->createRequest('GET', $url, ['headers' => ['User-Agent' => mobile_user_agent]]);
+                    $requests[] = $guzzle->createRequest('GET', $url, ['headers' => ['User-Agent' => desktop_user_agent], 'cookies' => $cookie]);
+                    $requests[] = $guzzle->createRequest('GET', $url, ['headers' => ['User-Agent' => mobile_user_agent ], 'cookies' => $cookie]);
                 }
             }
         }
@@ -155,8 +155,8 @@ if (rand(1, 7) > 5) {
             $ssl = ($site->getConfig()['https_enabled'] == 'Y');
             $url = ($ssl ? 'https' : 'http') . '://' . $site->domain;
 
-            $requests[] = $guzzle->createRequest('GET', $url, ['headers' => ['User-Agent' => desktop_user_agent]]);
-            $requests[] = $guzzle->createRequest('GET', $url, ['headers' => ['User-Agent' => mobile_user_agent]]);
+            $requests[] = $guzzle->createRequest('GET', $url, ['headers' => ['User-Agent' => desktop_user_agent], 'cookies' => $cookie]);
+            $requests[] = $guzzle->createRequest('GET', $url, ['headers' => ['User-Agent' => mobile_user_agent ], 'cookies' => $cookie]);
         }
     }
 
