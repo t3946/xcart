f1d41e40dbfb20d8c08282fa168e95ae10b1de6f
Nikolay Meleshev <nikolay@s3stores.com>
2018-04-16 15:47:27 +0300

    script

--- cron/tmp_script.php
+++ cron/tmp_script.php
@@ -1,7 +1,125 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 4/16/2018
- * Time: 9:48 AM
- */
+
+use Mindy\QueryBuilder\Expression;
+use Mindy\QueryBuilder\Q\QOr;
+use Modules\Goods\Models\ProductModel;
+use Modules\Goods\Models\ProductVideosModel;
+
+define("CIDEV_CRON_START", "CRON");
+
+require __DIR__ . DIRECTORY_SEPARATOR . "../www/top.inc.php";
+require __DIR__ . DIRECTORY_SEPARATOR . "../www/init.php";
+
+function cutTags($fulldescr, $flag = true, $tags = [])
+{
+    $mass = [
+        'div',
+        'span',
+        'p',
+        'br',
+        'ol',
+        'ul',
+        'li',
+        'table',
+        'thead',
+        'tbody',
+        'th',
+        'tr',
+        'td',
+    ];
+
+    $regexps = [
+        '/<script[^>]*?>.*?<\/script>/i',
+        '/<noscript[^>]*?>.*?<\/noscript>/i',
+        '/<style[^>]*?>.*?<\/style>/i',
+        '/<video[^>]*?>.*?<\/video>/i',
+        '/<a[^>]*?>.*?<\/a>/i',
+        '/<iframe[^>]*?>.*?<\/iframe>/i'
+    ];
+    foreach ($regexps as $regexp) {
+        if (preg_match($regexp, $fulldescr)) {
+            $fulldescr = preg_replace($regexp, "", $fulldescr);
+        }
+    }
+
+    $fulldescr = trim($fulldescr);
+
+    if (!$flag) {
+        $mass = [];
+    }
+
+    if (!empty($tags) && is_array($tags)){
+        foreach ($tags as $tag){
+            $regexp = '/<(\D+)\s?[^>]*?>/';
+            if (preg_match($regexp, $tag, $matches)){
+                $mass[] = $matches[1];
+            }
+            else {
+                $mass[] = $tag;
+            }
+        }
+    }
+
+    $tag = "";
+
+    $tags_string = "";
+    foreach ($mass as $tag){
+        $tags_string .= "<$tag>";
+    }
+
+    $fulldescr = strip_tags($fulldescr, $tags_string);
+
+    foreach ($mass as $tag) {
+
+        $regexp = "/(<{$tag})([^>]*)(>)/i";
+
+        if (preg_match($regexp, $fulldescr)) {
+            $fulldescr = preg_replace($regexp, "$1$3", $fulldescr);
+        }
+
+    }
+
+    return $fulldescr;
+}
+
+$regexp = '/<iframe[^>]*?src=(""|"|\'|\\"|\\\')(.*?)("""|\'|\\"|\\\')[^>]*?>/i';
+$regexp_2 = '/<video[^>]*?>\s+?<source[^>]*?src=(""|"|\'|\\"|\\\')(.*?)(""|"|\'|\\"|\\\')[^>]*?>/i';
+
+$product_count_all = 0;
+$product_count = 0;
+
+$qor_1 = ['fulldescr__contains' => 'iframe'];
+$qor_2 = ['fulldescr__contains' => '<video'];
+    $product_models = ProductModel::objects()->filter([
+                                                        new Qor([
+                                                            $qor_1, $qor_2
+                                                                ])
+                                                      ])->exclude(['provider' => 'feed'])->all();
+
+    foreach ($product_models as $product_model){
+        $video = $videos = [];
+        $video_url = '';
+        if ( (preg_match($regexp, $product_model->fulldescr, $matches)) || (preg_match($regexp_2, $product_model->fulldescr, $matches_2))){
+            if (empty($matches[2])){
+                $breakpoint = 1;
+            }
+
+            $video_url = $matches[2];
+
+            (new ProductVideosModel([
+                'product_id' => $product_model->productid,
+                'video' => $video_url,
+                'name' => $product_model->product
+                                    ]))->save();
+
+            $tmp_fulldescr = '';
+            $tmp_fulldescr = cutTags($product_model->fulldescr);
+            $product_model->fulldescr = $tmp_fulldescr;
+            $product_model->update(['fulldescr']);
+            $product_count++;
+        }
+        $product_count_all++;
+    }
+
+
+echo "All {$product_count_all}, is clear ==> {$product_count}";
