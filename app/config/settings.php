5feea916109f2c2f6a655d724a299200c511968e
Maksim K <dog.mixer@gmail.com>
2018-04-16 13:56:40 +0300

    config up

--- app/config/settings.php
+++ app/config/settings.php
@@ -13,8 +13,8 @@ return array_replace_recursive([
        'www'    => realpath(implode(DS, [__DIR__, '..', '..', 'www'])),
    ],
    'globals' => [
-//       'blowfish_key' => '8d5db63ada15e11643a0b1c3477c2c5c',
-//       'blowfish' => new \ctBlowfish(),
+       'blowfish_key' => '8d5db63ada15e11643a0b1c3477c2c5c',
+       'blowfish' => new \ctBlowfish(),
        'sql_tbl' => include __DIR__ . DS . "xcart_tables.php",
    ],
    'modules' => include __DIR__ . DS . 'modules.php',
