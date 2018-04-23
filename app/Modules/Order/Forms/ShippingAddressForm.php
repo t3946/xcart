baefd21df726d53d729243cf37150adb5d9d7285
Nikolay Meleshev <nikolay@s3stores.com>
2018-04-23 12:13:23 +0300

    form

--- app/Modules/Order/Forms/ShippingAddressForm.php
+++ app/Modules/Order/Forms/ShippingAddressForm.php
@@ -1,11 +1,76 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 4/20/2018
- * Time: 3:53 PM
- */
 
-class ShippingAddressForm
+use Xcart\App\Form\BaseForm;
+use Xcart\App\Form\Fields\CharField;
+use Xcart\App\Form\Fields\DropDownField;
+use Xcart\App\Form\Fields\EmailField;
+use Xcart\App\Form\Fields\NumberField;
+
+class ShippingAddressForm extends BaseForm
 {
+    public function getFields()
+    {
+        return [
+            's_firstname' => [
+                'class' => CharField::className(),
+                'label' => "Full Name",
+
+            ],
+
+            's_company' => [
+                'class' => CharField::className(),
+                'label' => "Companu (optional)",
+            ],
+
+            's_address' => [
+                'class' => CharField::className(),
+                'label' => 'Address',
+            ],
+
+            's_address_2' => [
+                'class' => CharField::className(),
+                'label' => 'Address',
+            ],
+
+            's_country' => [
+                'class' => DropDownField::className(),
+                'label' => 'Country',
+            ],
+
+            's_zipcode' => [
+                'class' => NumberField::className(),
+                'label' => 'Zip/Postal Code',
+            ],
+
+            's_statename' => [
+                'class' => CharField::className(),
+                'label' => 'State/Province',
+            ],
+
+            's_city' => [
+                'class' => CharField::className(),
+                'label' => 'City'
+            ],
+
+            'firstname' => [
+                'class' => CharField::className(),
+                'label' => 'fullname'
+            ],
+
+            'phone' => [
+                'class' => CharField::className(),
+                'label' => 'Phone'
+            ],
+
+            'phone_ext' => [
+                'class' => CharField::className(),
+                'label' => 'ext'
+            ],
+
+            'email' => [
+                'class' => EmailField::className(),
+                'label' => 'Email'
+            ],
+        ];
+    }
 }
