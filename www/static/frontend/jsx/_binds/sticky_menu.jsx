0d7cc53146b4fa0b82db1a307af5a8a06bccc824
anna <annav@s3stores.com>
2018-05-10 18:57:09 +0300

    about us

---
+++ www/static/frontend/jsx/_binds/sticky_menu.jsx
@@ -0,0 +1,58 @@
+import isMedia from "../utils/isMedia";
+
+(()=>{
+
+    // После загрузки css
+    $(document).on('app.start', function () {
+
+        var lastKnownScrollPosition = 0;
+        var ticking = false;
+
+        if (isMedia('large')) {
+
+            let stickyContainer = $('.sticky-container');
+            let sticky = stickyContainer.find('.sticky');
+
+            let processScroll = _.throttle(function(){
+
+                lastKnownScrollPosition = window.scrollY;
+                if (!ticking) {
+                    window.requestAnimationFrame(function() {
+                        checkMenuPosition(lastKnownScrollPosition);
+                        ticking = false;
+                    });
+                    ticking = true;
+                }
+            }, 50);
+
+            let initStickyMenu = function () {
+
+                let heightOfStickyBlock = sticky.height();
+                if(heightOfStickyBlock <= 0){
+                    return;
+                }
+                //console.log(heightOfStickyBlock);
+                stickyContainer.css({
+                    'display': 'block',
+                    'height': heightOfStickyBlock + 'px'
+                });
+
+                window.addEventListener('scroll', processScroll, {'passive' : true});
+            };
+
+            let initStickyMenuOnResize = _.throttle(initStickyMenu, 50);
+
+            function checkMenuPosition(lastKnownScrollPosition) {
+
+                if (lastKnownScrollPosition >= stickyContainer.offset().top) {
+                    sticky.addClass('menu-fixed');
+                } else {
+                    sticky.removeClass('menu-fixed');
+                }
+            }
+
+            initStickyMenu();
+            $(window).resize(initStickyMenuOnResize);
+        }
+    });
+})();
