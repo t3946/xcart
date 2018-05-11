f57deac94feddaeade4b1ad7ba8bf63fbc6bef93
Maksim K <dog.mixer@gmail.com>
2018-05-11 17:14:05 +0300

    up

--- app/Modules/Cart/Components/XCart.php
+++ app/Modules/Cart/Components/XCart.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Cart\Components;
 
+use Modules\Distributor\Models\DistributorModel;
 use Modules\Order\Helpers\OrderHelper;
 
 class XCart extends Cart
@@ -17,6 +18,10 @@ class XCart extends Cart
         return $this->getStorage()->getCartNumber();
     }
 
+    /**
+     * @param string $property
+     * @return CartItem[]
+     */
     public function getItemsGroupedBy($property = 'manufacturerid'): array
     {
         $groups = [];
@@ -47,5 +52,45 @@ class XCart extends Cart
         parent::onChange();
 
         OrderHelper::OrderStepsReset($this->getCartNumber());
+    }
+
+    public function isValid():bool
+    {
+        if ($this->getIsEmpty()) {
+            return false;
+        }
+
+        $groups = $this->getItemsGroupedBy();
+
+        if ($mids = array_keys($groups)) {
+            /** @var DistributorModel[] $distrs */
+            $distrs = DistributorModel::objects()->all(['pk__in' => $mids]);
+            $dist_min = [];
+            $dist_valid = [];
+
+            foreach ( $distrs as $model) {
+                if ($model->d_minimum_order_amount && $model->d_minimum_order_amount_in_us) {
+                    $dist_min[$model->pk] += $model->d_minimum_order_amount_in_us;
+                    $dist_valid[$model->pk] = false;
+                }
+            }
+
+            foreach ($groups as $mid => $item) {
+                if ($dist_min[$mid]) {
+                    $dist_min[$mid] -= $item->getPrice();
+                }
+
+                if ($dist_min[$mid] <= 0 ) {
+                    unset($dist_valid[$mid]);
+                }
+            }
+
+            if ($dist_valid) {
+                return false;
+            }
+        }
+
+
+        return true;
     }
 }
