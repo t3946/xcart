5770bc61f95956fb4a53a3d4fb53d79e1cf8496a
Nikolay Meleshev <nikolay@s3stores.com>
2018-05-18 15:31:19 +0300

    Retrieve Order up

---
+++ app/Modules/Order/Controllers/RetrieveOrderController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 5/18/2018
+ * Time: 2:46 PM
+ */
+
+namespace Modules\Order\Controllers;
+
+class RetrieveOrderController
+{
+}
---
+++ app/Modules/Order/routes_retrieve.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 5/18/2018
+ * Time: 2:48 PM
+ */
