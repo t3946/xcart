384d756d0d25cd19a5cb95a880f280cb1b11103b
Nikolay Meleshev <nikolay@s3stores.com>
2018-05-22 16:05:58 +0300

    flash up

--- www/static/frontend/dist/js/main.js
+++ www/static/frontend/dist/js/main.js
@@ -32022,7 +32022,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* WEBPACK VAR INJECTION */(function(jQuery, global, module, $) {var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;
+/* WEBPACK VAR INJECTION */(function(jQuery, global, module) {var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
@@ -42433,47 +42433,7 @@ jQuery(window).on('load', function () {
   }
 
 });
-(function () {
-  var flashOutTime = 7000;
-
-  var $flashList = $('.flash-messages-block .flash-list');
-
-  $(document).on('click', '.close-flash', function (e) {
-    e.preventDefault();
-    $(this).closest('.flash-message').fadeOut(400, function () {
-      $(this).remove();
-    });
-    return false;
-  });
-
-  window.addFlashMessage = function (message, type, time) {
-    type = type ? type : 'success';
-    var outTime = time && time > flashOutTime ? time : flashOutTime;
-
-    var $item = $('<div class="flash-message"></div>').addClass(type);
-    var $closer = $('<a class="close-flash right"><i class="icon-delete_in_filter"></i></a>');
-    var $text = $('<span/>').addClass('message').text(message);
-
-    $item.append([$closer, $text]);
-    $flashList.append($item);
-
-    setTimeout(function () {
-      if ($item && $item.length > 0) {
-        $item.fadeOut(400, function () {
-          $(this).remove();
-        });
-      }
-    }, outTime);
-  };
-
-  if (window['flashStack'] && window['flashStack'].length) {
-    for (var i in window['flashStack']) {
-      var f = window['flashStack'][i];
-      addFlashMessage(f.message, f.type);
-    }
-  }
-})();
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0), __webpack_require__(1), __webpack_require__(5)(module), __webpack_require__(0)))
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0), __webpack_require__(1), __webpack_require__(5)(module)))
 
 /***/ }),
 /* 46 */
