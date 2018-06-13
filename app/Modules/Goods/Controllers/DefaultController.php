2030032637a9247ef4a88e31ddec98ebe432b4ed
Nikolay Meleshev <nikolay@s3stores.com>
2018-06-13 13:59:50 +0300

    up

--- app/Modules/Goods/Controllers/DefaultController.php
+++ app/Modules/Goods/Controllers/DefaultController.php
@@ -59,8 +59,9 @@ class DefaultController extends FrontendController
                 if ($mv_category = $mv_category->getObjects()->ancestors(true)->filter(['avail' => 'Y', 'product_count__gt' => 0])->limit(1)->get()) {
                     Xcart::app()->request->redirect($mv_category->getAbsoluteUrl());
                 }
-                Xcart::app()->request->redirect('/');
             }
+
+            Xcart::app()->request->redirect('/');
         }
 
 
