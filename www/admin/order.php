57fb66c4b946388301e80fb7f6d3e3536faea2b1
Roman Novokshonov <kshon@mail.ru>
2018-07-13 17:07:41 +0300

    tatyana refund

--- www/admin/order.php
+++ www/admin/order.php
@@ -1535,7 +1535,7 @@ if ($mode == 'ref_notify')
             }
         }
 
-        if ($ref_notify_button_clicked == "Update_C2B_status_and_Send_refund_notification" && in_array($login, ['sergey2', 'igor', 'roman_n', 'dmitry_s'])) {
+        if ($ref_notify_button_clicked == "Update_C2B_status_and_Send_refund_notification" && in_array($login, ['sergey2', 'tatyanap', 'roman_n', 'dmitry_s'])) {
             if ($orderModel = OrderModel::objects()->get(['orderid' => $orderid])) {
                 $error_message = $ref_sum = null;
 
