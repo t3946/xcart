62b633b79dab6be38ce7c116bd636485f5048da1
anna <annav@s3stores.com>
2018-07-31 18:26:48 +0300

    form: duplicated field and validate after autocomplete

--- www/static/frontend/jsx/components/FormValidation.jsx
+++ www/static/frontend/jsx/components/FormValidation.jsx
@@ -48,7 +48,13 @@ class FormValidation {
             && typeof event.explicitOriginalTarget !== 'undefined'
             && typeof event.explicitOriginalTarget.classList !== 'undefined'
             && event.explicitOriginalTarget.classList.contains('clear-input')){
-            return;
+
+            let clearContainer = event.explicitOriginalTarget.closest('.input-container');
+            let eventContainer = event.target.closest('.input-container');
+
+            if(clearContainer === eventContainer){
+                return;
+            }
         }
 
         this.checkForm(event.target);
