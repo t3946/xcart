b33230dcc433593668316df5a75a30f367cbcb48
Roman Novokshonov <kshon@mail.ru>
2019-07-24 12:14:41 +0300

    log Shipping

--- app/include/Xcart/Shipping/UPS.php
+++ app/include/Xcart/Shipping/UPS.php
@@ -13,10 +13,8 @@ use Ups\Entity\ShipFrom;
 use Ups\Entity\Shipment;
 use Ups\Entity\UnitOfMeasurement;
 use Ups\Rate;
-use Xcart\ApproximationShippingRates;
-use Xcart\Logs;
+use Xcart\App\Main\Xcart;
 use Xcart\ShippingRate;
-use Xcart\SQLBuilder;
 
 class UPS extends ShippingProcessor
 {
@@ -72,7 +70,7 @@ class UPS extends ShippingProcessor
             $UPS_password = 'f3ddbec6bf';
             $UPS_accesskey = '8C381B1AAE49E83E';
 
-            $rate = new Rate($UPS_accesskey, $UPS_username, $UPS_password);
+            $rate = new Rate($UPS_accesskey, $UPS_username, $UPS_password, false, Xcart::app()->logger->getLogger('UPS'));
 
             try {
                 $oCustomer = $this->getCustomer();
@@ -133,7 +131,7 @@ class UPS extends ShippingProcessor
             } catch (Exception $e) {
                 $message = __CLASS__ . ': ' . $e->getMessage();
                 $to = " From: {$this->getManufacturer()->m_zipcode} To: {$oCustomer->s_zipcode}";
-                Logs::_log(Logs::LOG_RESOURCE_SHIPPING_QUOTES, time(), Logs::LOG_TYPE_SYSTEM, $message. $to);
+                Xcart::app()->logger->error($message. $to, [], 'shipping');
             }
         }
 
