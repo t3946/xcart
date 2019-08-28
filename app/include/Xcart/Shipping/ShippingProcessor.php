0f111764ef1fe41a25fe0fba99bc731361761ecd
Roman Novokshonov <kshon@mail.ru>
2019-08-28 11:31:49 +0300

    up

--- app/include/Xcart/Shipping/ShippingProcessor.php
+++ app/include/Xcart/Shipping/ShippingProcessor.php
@@ -265,7 +265,7 @@ abstract class ShippingProcessor
 
     protected function saveShippingQuotesCached(): void
     {
-        if (!$this->useCache || 1==1) {
+        if (!$this->useCache) {
             return;
         }
 
