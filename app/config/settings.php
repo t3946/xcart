70070ff13c8362044ec95f7b15a6501cb090b988
Roman Novokshonov <kshon@mail.ru>
2019-11-29 09:25:57 +0300

    use charset utf8mb4

--- app/config/settings.php
+++ app/config/settings.php
@@ -35,7 +35,7 @@ return array_replace_recursive([
                    'host' => '127.0.0.1',
                    'user' => 'xcart_k',
                    'password' => 'i250923lst',
-                   'charset'  => 'utf8',
+                   'charset'  => 'utf8mb4',
                    'mapping' => [
                        'enum' => 'string'
                    ],
