1858f8a3f39ad45bc87955528acc1e1994538f15
Roman Novokshonov <kshon@mail.ru>
2019-12-10 11:46:44 +0300

    cache proxies list

--- app/Modules/Core/Proxy/Parser.php
+++ app/Modules/Core/Proxy/Parser.php
@@ -5,6 +5,7 @@ namespace Modules\Core\Proxy;
 
 
 use Modules\Core\Helpers\GuzzleDownloader;
+use Xcart\App\Main\Xcart;
 
 class Parser {
 
@@ -23,25 +24,36 @@ class Parser {
     }
 
     private static function getProxychekerNetProxies($day_sub = 0) {
+        if ($validProxies = Xcart::app()->cache->get('proxies')) {
+            $total = count($validProxies);
+            echo "{$total} proxies found \n";
+            return $validProxies;
+        }
+
         $validProxies = [];
-        $url = str_replace('{{date}}', self::getDate($day_sub), self::CHECKERPROXY_NET_API_URL);
-        $downloader = new GuzzleDownloader(['timeout' => 60]);
-        $downloader->get($url);
-        if ($response = $downloader->getInternalResponse()) {
-            if ($json = json_decode($response->getContent(), true)) {
-                foreach ($json as $items) {
-                    if ((int)$items['type'] === 2 && Helper::validateProxyIpPort($items['addr'])) {
-                        $validProxies[] = $items['addr'];
+        $total = 0;
+        while(!$total) {
+            $url = str_replace('{{date}}', self::getDate($day_sub), self::CHECKERPROXY_NET_API_URL);
+            echo "Get proxies list {$url} \n";
+            $downloader = new GuzzleDownloader(['timeout' => 60]);
+            $downloader->get($url);
+            if ($response = $downloader->getInternalResponse()) {
+                if ($json = json_decode($response->getContent(), true)) {
+                    foreach ($json as $items) {
+                        if ((int)$items['type'] === 2 && Helper::validateProxyIpPort($items['addr'])) {
+                            $validProxies[] = $items['addr'];
+                        }
+                    }
+                } else {
+                    if (!$day_sub) {
+                        return self::getProxychekerNetProxies(1);
                     }
                 }
-            } else {
-                if (!$day_sub) {
-                    return self::getProxychekerNetProxies(1);
-                }
             }
+            $total = count($validProxies);
         }
-        $total = count($validProxies);
         echo "{$total} proxies found \n";
+        $validProxies = Xcart::app()->cache->set('proxies', $validProxies, 6*60);
         return $validProxies;
     }
 
