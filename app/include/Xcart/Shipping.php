c358bfd9d8840c94be0cc178978e8c8c4cf90a42
Roman Novokshonov <kshon@mail.ru>
2019-12-11 09:44:30 +0300

    fix: Shipping rates

--- app/include/Xcart/Shipping.php
+++ app/include/Xcart/Shipping.php
@@ -122,7 +122,7 @@ FROM xcart_zone_element AS ZE
 INNER JOIN xcart_zone_element AS ZES USING (zoneid, field_type)
 INNER JOIN xcart_shipping_rates SR ON SR.manufacturerid = {$oManufacturer->manufacturerid} AND ZE.zoneid = SR.zoneid
 WHERE (ZE.field_type = 'S' AND ZE.field ='{$sCA_ST}') OR (ZE.field_type = 'C' AND ZE.field = '{$cs_country}') 
-GROUP BY ZE.zoneid 
+GROUP BY ZE.zoneid, ZE.field_type 
 UNION
 SELECT zoneid, 999999999, 999999999
 FROM xcart_shipping_rates
