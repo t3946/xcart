593a73dff43cb4966fd88656bec6b5805a131748
Roman Novokshonov <kshon@mail.ru>
2020-01-17 10:47:20 +0300

    up

--- app/Modules/Amazon/Commands/InventoryCommand.php
+++ app/Modules/Amazon/Commands/InventoryCommand.php
@@ -34,12 +34,8 @@ class InventoryCommand extends Command
 
                 $prevent_selling = $product->amazon_fields->limit(1)->get()->prevent_selling_on_amazon;
 
-                if ($brand = $product->brand) {
-                    foreach ($brand->markets_disabled as $dm) {
-                        if ($dm->marketplace_id == 3) {
-                            $prevent_selling = 'MFN';
-                        }
-                    }
+                if (!$product->isMarketPlaceEnabled(3)) {
+                    $prevent_selling = 'MFN';
                 }
 
                 if ($product->manufacturerid == 12 || ($product->isAmazonFBAEnabled() && ((int)$product->amazon_fba_avail > 0 || $product->getAmazonFBAStockReservedTransfers() > 0) &&
