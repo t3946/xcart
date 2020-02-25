feac90c834e680ee3d41b9e0562e183dfedf8eb9
Roman Novokshonov <kshon@mail.ru>
2020-02-25 11:14:57 +0300

    fix selected Dx search

--- app/Modules/Dashboard/Controllers/SearchController.php
+++ app/Modules/Dashboard/Controllers/SearchController.php
@@ -4,7 +4,6 @@ namespace Modules\Dashboard\Controllers;
 
 use Modules\Dashboard\Helpers\SearchHelper;
 use Modules\Dashboard\Stores\OrderSearchStore;
-use Modules\Order\Models\OrderStatusModel;
 use Xcart\App\Controller\PrototypeAdminController;
 use Xcart\App\Main\Xcart;
 
@@ -12,14 +11,16 @@ class SearchController extends PrototypeAdminController
 {
     public $defaultAction = 'index';
 
-    public function index()
+    public function index(): void
     {
-        $session       = Xcart::app()->request->session;
+        $form_data = [];
         $form_collapse = false;
 
-        if (!empty($_REQUEST['search'])) {
+        $request = Xcart::app()->request->request->all();
+
+        if (!empty($request['search'])) {
             $form_collapse = true;
-            $form_data = OrderSearchStore::getClearedData($_REQUEST['search']);
+            $form_data = OrderSearchStore::getClearedData($request['search']);
         }
 
         if (!is_array($form_data)) {
@@ -31,34 +32,30 @@ class SearchController extends PrototypeAdminController
         }
 
 
-        if (!empty($_REQUEST['fast_search'])) {
+        if (!empty($request['fast_search']) && !empty($form_data['order']['id']['from'])) {
 
-            if ( !empty($form_data['order']['id']['from']) ) {
+            $oid = $form_data['order']['id']['from'];
 
-                $oid = $form_data['order']['id']['from'];
-
-                if (strpos($oid, '-') !== false) {
-                    if (preg_match('/^([a-zA-Z]{2,4}-).++/i', $oid)) {
-                        $oid = substr($oid, 3);
-                        $form_data['order']['id']['from'] = $oid;
-                    }
-                    else {
-                        $form_data['order']['id']['from'] = null;
-                        $form_data['order']['amazon_order'] = $oid;
-                    }
+            if (strpos($oid, '-') !== false) {
+                if (preg_match('/^([a-zA-Z]{2,4}-).++/i', $oid)) {
+                    $oid = substr($oid, 3);
+                    /** @var array $form_data */
+                    $form_data['order']['id']['from'] = $oid;
+                } else {
+                    /** @var array $form_data */
+                    $form_data['order']['id']['from'] = null;
+                    /** @var array $form_data */
+                    $form_data['order']['amazon_order'] = $oid;
                 }
             }
         }
 
         $orderStore = new OrderSearchStore($form_data);
 
-        if (!empty($_REQUEST['fast_search'])) {
-
+        if (!empty($request['fast_search'])) {
             $qs = $orderStore->getQuerySet();
-
-            if ($qs->count() == 1) {
-                $model = $qs->limit(1)->get();
-                $this->redirect( $model->getAdminUrl() );
+            if ($qs->count() === 1 && $model = $qs->limit(1)->get()) {
+                $this->redirect($model->getAdminUrl());
             }
         }
 
@@ -79,13 +76,14 @@ class SearchController extends PrototypeAdminController
         }
 
         echo $this->renderInternal('dashboard/search.tpl', array_merge(
-            SearchHelper::getFormAndListData(),
-            [
-                'pager'         => $pager,
-                'models'        => $models,
-                'form_data'     => SearchHelper::prepareFormDataForTemplate($form_data),
-                'form_collapse' => $form_collapse,
-            ])
+                SearchHelper::getFormAndListData(),
+                [
+                    'pager' => $pager,
+                    'models' => $models,
+                    'form_data' => SearchHelper::prepareFormDataForTemplate($form_data),
+                    'form_data_raw' => $form_data,
+                    'form_collapse' => $form_collapse,
+                ])
         );
     }
 
