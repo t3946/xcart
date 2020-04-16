e7994c69e0208a09729809902a3de421a63c3951
Roman Novokshonov <kshon@mail.ru>
2020-04-16 09:50:27 +0300

    up

--- app/Modules/Order/Controllers/Api/AfterShipController.php
+++ app/Modules/Order/Controllers/Api/AfterShipController.php
@@ -16,22 +16,34 @@ class AfterShipController extends Controller
     {
         $json = file_get_contents('php://input');
         Xcart::app()->logger->debug($json, [], 'afterShip');
-        if (($params = json_decode($json, true)) &&
+        if (true || (($params = json_decode($json, true)) &&
             isset($params['msg']) &&
             $params['msg']['order_id'] &&
             $params['msg']['tag'] === 'Delivered' &&
-            $order = OrderModel::objects()->get(['orderid' => $params['msg']['order_id']]))
+            $order = OrderModel::objects()->get(['orderid' => $params['msg']['order_id']])))
         {
-            $order->groups->update([
-                'dc_status' => OrderStatusModel::ORDER_DC_STATUS_DELIVERED,
-            ]);
-            $order->dc_status = OrderStatusModel::ORDER_DC_STATUS_DELIVERED;
-            $order->save();
-            (new OrderLogModel([
-                'orderid' => $order->orderid,
-                'type' => OrderLogModel::LOG_TYPE_XCART,
-                'log' => 'Order has been delivered',
-            ]))->save();
+            $params['msg']['tracking_number'] = '1Z7749810352229132';
+            $params['msg']['order_id'] = '115042';
+            $order = OrderModel::objects()->get(['orderid' => $params['msg']['order_id']]);
+
+            if ($group = $order->groups->get(['trackings__tracknum' => $params['msg']['tracking_number']])) {
+
+                $current_dc_status_value = $group->dc_status_model->name;
+                $group->dc_status = OrderStatusModel::ORDER_DC_STATUS_DELIVERED;
+                $group->save();
+                $new_value = $group->dc_status_model->name;
+                $log = "<b>{$group->manufacturer->code}:</b> dc_status: {$current_dc_status_value} -> {$new_value}\n";
+                (new OrderLogModel([
+                    'orderid' => $order->orderid,
+                    'type' => OrderLogModel::LOG_TYPE_XCART,
+                    'log' => $log,
+                ]))->save();
+
+            }
+            if ($order->groups->exclude(['dc_status' => OrderStatusModel::ORDER_DC_STATUS_DELIVERED])->count() === 0) {
+                $order->dc_status = OrderStatusModel::ORDER_DC_STATUS_DELIVERED;
+                $order->save();
+            }
         }
     }
 }
