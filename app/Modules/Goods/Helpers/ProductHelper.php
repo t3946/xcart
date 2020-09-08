f2ae536697a5c6ef5912c68ddf4b91d9e42336bc
Roman Novokshonov <kshon@mail.ru>
2020-09-08 12:37:59 +0300

    fix Schema.org::Product

--- app/Modules/Goods/Helpers/ProductHelper.php
+++ app/Modules/Goods/Helpers/ProductHelper.php
@@ -338,8 +338,8 @@ class ProductHelper
                     'PriceSpecification' => [
                         '@type' => 'PriceSpecification',
                         'priceCurrency' => 'USD',
-                        'price' => $model->getFrontendPrice(),
-                        'minPrice' => $model->getFrontendPrice(),
+                        'price' => $model->getFrontendPrice($model->min_amount ?? 1),
+                        'minPrice' => $model->getFrontendPrice($model->min_amount ?? 1),
                         'maxPrice' => $model->getFrontendPrice(2),
                     ],
                     'url' => $model->getAbsoluteUrl(true),
