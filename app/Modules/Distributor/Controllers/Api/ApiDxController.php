0e6eb8e2ec9984003905eb3ff5fb980c4267cdbe
Roman Novokshonov <kshon@mail.ru>
2020-10-23 10:12:11 +0300

    up

--- app/Modules/Distributor/Controllers/Api/ApiDxController.php
+++ app/Modules/Distributor/Controllers/Api/ApiDxController.php
@@ -111,7 +111,7 @@ class ApiDxController extends Controller
         $runForces = array_filter($feeds, static fn($f) => $f->run_force === true);
 
         $schedule = SchedulerHelper::algorithm(self::TIME_FRAME_SEC, $times);
-        $schedule = array_map(static fn($sh) => $sh / 60, $schedule);
+        $schedule = array_map(static fn($sh) => (int)($sh / 60), $schedule);
 
         [$h, $m] = explode(':', self::FEEDS_START_TIME);
 
@@ -119,7 +119,7 @@ class ApiDxController extends Controller
         $start = (int)$now->format('H') < (int)$h ? new DateTime('yesterday') : new DateTime();
         $start->setTime($h, $m);
         $offset = $now->getTimestamp() - $start->getTimestamp();
-        $offset /= 60;
+        $offset = (int)($offset / 60);
         echo $offset;
         var_dump($schedule);
         if ($offset >= 0) {
