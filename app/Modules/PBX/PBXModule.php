b35e34417da7d4f72d115fddf5325ffd1e242a28
Roman Novokshonov <kshon@mail.ru>
2020-11-17 14:32:40 +0300

    pbx init

--- app/Modules/PBX/PBXModule.php
+++ app/Modules/PBX/PBXModule.php
@@ -2,11 +2,14 @@
 
 namespace Modules\PBX;
 
+use Modules\Admin\Traits\AdminTrait;
 use Xcart\App\Main\Xcart;
 use Xcart\App\Module\Module;
 
 class PBXModule extends Module
 {
+    //use AdminTrait;
+
     public static function getAdminMenu()
     {
         if (Xcart::app()->user->hasRoles(['vrs','vrv'])) {
@@ -22,6 +25,4 @@ class PBXModule extends Module
             ]
         ];
     }
-
-
 }
