f7b8047320ec63714159df63c7db31cc11e5aecc
Roman Novokshonov <kshon@mail.ru>
2020-11-23 16:10:06 +0300

    clean up code

--- app/Modules/Goods/Commands/CleanImagesCommand.php
+++ app/Modules/Goods/Commands/CleanImagesCommand.php
@@ -4,6 +4,7 @@
 namespace Modules\Goods\Commands;
 
 
+use Dariuszp\CliProgressBar;
 use Modules\Goods\Models\ImageDModel;
 use Xcart\App\Commands\Command;
 use Xcart\App\Main\Xcart;
@@ -15,21 +16,23 @@ class CleanImagesCommand extends Command
     {
         ini_set('memory_limit', '4G');
         if ($storage = Xcart::app()->storage->getFilesystem('www')) {
-            foreach ($storage->listContents('images/D') as $file) {
+            $total = $storage->listContents('images/D');
+            $bar = new CliProgressBar(count($total));
+            foreach ($total as $file) {
                 if ($images = ImageDModel::objects()->all(['image_path' => './' . $file['path']])) {
                     $md5 = md5($storage->read($file['path']));
                     array_walk($images, static function ($image) use ($file, $md5) {
                         if (($old = $image->md5) !== $md5) {
                             $image->update(['md5' => $md5]);
-                            echo "Update | Product_id: {$image['id']} {$file['path']} Old md5 {$old}; New md5 {$md5}" . PHP_EOL;
                         }
                     });
 
                 } else {
                     $storage->delete($file['path']);
-                    echo "Delete | ${file['path']}" . PHP_EOL;
                 }
+                $bar->progress();
             }
+            $bar->end();
         }
     }
 }
