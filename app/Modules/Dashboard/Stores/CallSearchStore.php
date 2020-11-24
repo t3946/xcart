08aa461b6a1d88074ecd4a45995d07ae63fe93cf
Roman Novokshonov <kshon@mail.ru>
2020-11-24 16:31:33 +0300

    up

--- app/Modules/Dashboard/Stores/CallSearchStore.php
+++ app/Modules/Dashboard/Stores/CallSearchStore.php
@@ -38,7 +38,7 @@ class CallSearchStore extends BaseStore
             }
         }
         $this->where = $condition ?? [];
-        $qs->filter($this->where)->having($this->having)->order('-id');
+        $qs->filter($this->where)->having($this->having);
         $this->qs = $qs;
     }
 
