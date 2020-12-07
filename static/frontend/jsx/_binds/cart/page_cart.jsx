daf229840cb9f8446518fe421e381a5587057bdf
Alexandr <alexander@s3stores.com>
2020-12-07 17:08:30 +0300

    remove unused files

--- static/frontend/jsx/_binds/cart/page_cart.jsx
+++
@@ -1,8 +0,0 @@
-import storeCart from 'stores/StoreCart';
-(()=>{
-    let cart_container = $('.cart-page');
-    if (cart_container.length) {
-        storeCart.dispatch({type:'FETCH'});
-
-    }
-})();
--- static/frontend/jsx/utils/trigger.jsx
+++
@@ -1,5 +0,0 @@
-import $ from 'jquery';
-
-export default (type, data, target = document) => {
-    $(target).trigger(type, data);
-};
