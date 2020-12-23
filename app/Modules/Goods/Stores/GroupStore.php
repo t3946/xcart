d7ffc0f498c1ae3b310a8c93e20ea1848987ef8d
Roman Novokshonov <kshon@mail.ru>
2020-12-23 12:06:35 +0300

    up

--- app/Modules/Goods/Stores/GroupStore.php
+++ app/Modules/Goods/Stores/GroupStore.php
@@ -67,7 +67,7 @@ class GroupStore extends BaseStore
             $this->qs->filter([
                 'forsale' => 'Y',
                 'sites__through__sfid' => $this->data['sfid'],
-                'distributor__active' => 'Y',
+                'distributor__avail' => 'Y',
                 'group_root__isnull' => true
             ]);
 
