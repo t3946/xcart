087dbfe6b3a9b9615d67d8c23901441873c2e6eb
Alexandr <werhcuh@gmail.com>
2021-01-26 16:02:15 +0300

    intermediate

--- app/Modules/Sites/Admin/SitesAdmin.php
+++ app/Modules/Sites/Admin/SitesAdmin.php
@@ -1,21 +1,52 @@
 <?php
 
-
 namespace Modules\Sites\Admin;
 
-
 use Modules\Admin\Contrib\Admin;
+use Modules\Admin\Traits\AdminTrait;
+use Modules\Forms\Forms\TemplateForm;
+use Modules\Forms\Models\TemplateModel;
+use Modules\Sites\Forms\SiteForm;
+use Modules\Sites\Models\SiteModel;
 
 class SitesAdmin extends Admin
 {
-    public static $public = false;
+    use AdminTrait;
+
+//    public static $public = false;
+
+
+    public function getListColumns()
+    {
+        return [
+            'code',
+            'domain',
+            'prefix'
+        ];
+    }
+
     public function getForm()
     {
+        return new SiteForm();
+    }
 
+    public function getModel()
+    {
+        return new SiteModel();
     }
 
     public static function getName()
     {
-        return 'Storefronts';
+        return 'Sites here';
     }
+
+//    public function isAjaxUpdate(): bool
+//    {
+//        return true;
+//    }
+//
+//    public function isAjaxCreate(): bool
+//    {
+//        return true;
+//    }
 }
