549cad73d392d93b1e689b4c89d87fd0e45c638b
Roman Novokshonov <kshon@mail.ru>
2021-01-26 10:18:20 +0300

    Payment Methods Model

---
+++ app/Modules/Sites/Models/SitePaymentMethodModel.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Modules\Sites\Models;
+
+
+use Xcart\App\Orm\Fields\AutoField;
+use Xcart\App\Orm\Fields\BooleanField;
+use Xcart\App\Orm\Fields\CharField;
+use Xcart\App\Orm\Fields\ImageField;
+use Xcart\App\Orm\Fields\IntField;
+use Xcart\App\Orm\Model;
+
+class SitePaymentMethodModel extends Model
+{
+    public static function tableName(): string
+    {
+        return 'sites_payment_methods';
+    }
+
+    public static function getFields(): array
+    {
+        return [
+            'payment_method_id' => AutoField::class,
+            'name' => [
+                'class' => CharField::class
+            ],
+            'logo' => [
+                'class' => ImageField::class,
+                'adapterName' => 'www',
+                'uploadTo' => 'images/payment_methods/',
+                'required' => true
+            ],
+            'is_active' => [
+                'class' => BooleanField::class,
+                'default' => true,
+            ],
+            'position' => [
+                'class' => IntField::class,
+                'default' => 0
+            ]
+        ];
+    }
+}
