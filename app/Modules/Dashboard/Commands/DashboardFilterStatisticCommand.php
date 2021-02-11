d3e8a526010e28eaa14bddf09908098424f25473
Roman Novokshonov <kshon@mail.ru>
2021-02-11 10:16:04 +0300

    fix create dashboard statistic

--- app/Modules/Dashboard/Commands/DashboardFilterStatisticCommand.php
+++ app/Modules/Dashboard/Commands/DashboardFilterStatisticCommand.php
@@ -16,11 +16,11 @@ class DashboardFilterStatisticCommand extends Command
         $models = DashboardFilter::objects()->filter(['enabled' => true])->cache(60)->all();
         foreach ($models as $model) {
             $storage = $model->getSearchStorage();
-            DashboardFilterStatisticModel::objects()->getOrCreate([
-                'filter' => $model,
+            (new DashboardFilterStatisticModel[
+                'filter_id' => $model->id,
                 'hour' => (int)date('H'),
                 'count' => $storage->getCashedCount()
-            ]);
+            ])->save();
         }
     }
 }
