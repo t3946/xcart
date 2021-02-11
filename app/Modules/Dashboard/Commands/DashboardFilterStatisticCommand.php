a47b7100e42444c7d549d92b5fec4f402717e438
Roman Novokshonov <kshon@mail.ru>
2021-02-11 11:48:24 +0300

    up

--- app/Modules/Dashboard/Commands/DashboardFilterStatisticCommand.php
+++ app/Modules/Dashboard/Commands/DashboardFilterStatisticCommand.php
@@ -16,11 +16,11 @@ class DashboardFilterStatisticCommand extends Command
         $models = DashboardFilter::objects()->filter(['enabled' => true])->cache(60)->all();
         foreach ($models as $model) {
             $storage = $model->getSearchStorage();
-            (new DashboardFilterStatisticModel[
+            (new DashboardFilterStatisticModel([
                 'filter_id' => $model->id,
                 'hour' => (int)date('H'),
                 'count' => $storage->getCashedCount()
-            ])->save();
+            ]))->save();
         }
     }
 }
