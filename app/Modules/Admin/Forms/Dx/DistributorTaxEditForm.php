b0cdea962cdd57c5a953ff98c0ad0eafd6d97069
Roman Novokshonov <kshon@mail.ru>
2021-03-10 12:56:57 +0300

    fix dx taxes

--- app/Modules/Admin/Forms/Dx/DistributorTaxEditForm.php
+++ app/Modules/Admin/Forms/Dx/DistributorTaxEditForm.php
@@ -5,6 +5,7 @@ namespace Modules\Admin\Forms\Dx;
 
 
 use Modules\Distributor\Models\DistributorTaxModel;
+use Modules\Sites\Models\TaxModel;
 use Xcart\App\Form\Fields\HiddenField;
 use Xcart\App\Form\Fields\Select2Field;
 use Xcart\App\Form\ModelForm;
@@ -18,14 +19,23 @@ class DistributorTaxEditForm extends ModelForm
 
     public function getFields()
     {
+        /** @var DistributorTaxModel $dx_taxes */
+        $dx_taxes = $this->getInstance()->getObjects();
         return [
             'tax' => [
                 'class' => Select2Field::class,
-                'html' => ['style' => 'width: 400px;']
+                'html' => ['style' => 'width: 400px;'],
+                'choices' => function () use ($dx_taxes) {
+                    foreach (TaxModel::objects()->exclude(['pk__in' => $dx_taxes->valuesList(['tax_id'], true)]) as $tax) {
+                        $result[$tax->taxid] = (string)$tax;
+                    }
+                    return $result ?? [];
+                }
             ],
             'distributor' => [
                 'class' => HiddenField::class
             ]
         ];
     }
+
 }
