2eaee31377221523f5bd44158083ebc73eee3a6d
Roman Novokshonov <kshon@mail.ru>
2021-03-29 12:07:08 +0300

    fix reconciliation end date

--- www/admin/reconciliation.php
+++ www/admin/reconciliation.php
@@ -310,6 +310,13 @@ if ($REQUEST_METHOD == "POST") {
             $search_data["reconciliation_tab_" . $t]["date_csv"]["end_date"] = $posted_data["date_csv"]["end_date"];
             $search_data["reconciliation_tab_" . $t]["date_csv"]["end_date_str"] = $posted_data["date_csv"]["end_date_str"];
         }
+    } else {
+        foreach ($all_tabs as $t) {
+            unset(
+                $search_data["reconciliation_tab_" . $t]["date_csv"]["end_date"],
+                $search_data["reconciliation_tab_" . $t]["date_csv"]["end_date_str"]
+            );
+        }
     }
 
     if (!empty($date_Start)) {
@@ -804,7 +811,6 @@ SQL
         }
 
     } else {
-
         $_filter = [
             'date_csv__gte' => $search_data["reconciliation_tab_" . $tab]["date_csv"]["start_date"],
             'date_csv__lte' => ($search_data["reconciliation_tab_" . $tab]["date_csv"]["end_date"]) ?: time(),
