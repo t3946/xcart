7ba4edaffcd9a8e85b348a3d4995f02b519853b5
Roman Novokshonov <kshon@mail.ru>
2021-04-09 11:07:23 +0300

    fix dx taxes

--- app/Modules/Admin/Forms/Dx/DistributorTaxEditForm.php
+++ app/Modules/Admin/Forms/Dx/DistributorTaxEditForm.php
@@ -6,6 +6,7 @@ namespace Modules\Admin\Forms\Dx;
 
 use Modules\Distributor\Models\DistributorTaxModel;
 use Modules\Sites\Models\TaxModel;
+use Xcart\App\Form\Fields\DropDownField;
 use Xcart\App\Form\Fields\HiddenField;
 use Xcart\App\Form\Fields\Select2Field;
 use Xcart\App\Form\ModelForm;
@@ -20,13 +21,18 @@ class DistributorTaxEditForm extends ModelForm
     public function getFields()
     {
         /** @var DistributorTaxModel $dx_taxes */
-        $dx_taxes = $this->getInstance()->getObjects();
+        $dx_taxes = $this->getInstance();
+        $tax_excluded = $this->getModel()->getObjects()->filter(['distributor_id' => $dx_taxes->distributor_id])->valuesList(['tax_id'], true);
         return [
             'tax' => [
-                'class' => Select2Field::class,
+                'class' => DropDownField::class,
                 'html' => ['style' => 'width: 400px;'],
-                'choices' => function () use ($dx_taxes) {
-                    foreach (TaxModel::objects()->exclude(['pk__in' => $dx_taxes->valuesList(['tax_id'], true)]) as $tax) {
+                'choices' => function () use ($tax_excluded) {
+                    $taxes = TaxModel::objects();
+                    if ($tax_excluded) {
+                        $taxes->exclude(['pk__in' => $tax_excluded]);
+                    }
+                    foreach ($taxes as $tax) {
                         $result[$tax->taxid] = (string)$tax;
                     }
                     return $result ?? [];
