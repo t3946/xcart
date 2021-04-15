bed97a482ad3de479d4ae4a7215e3366cd380c79
Alexandr <werhcuh@gmail.com>
2021-04-15 10:27:49 +0300

    remove unused

--- www/static/frontend/jsx/js/Components/checkout/DistributorCart.js
+++ www/static/frontend/jsx/js/Components/checkout/DistributorCart.js
@@ -60,42 +60,6 @@ export const DistributorCart = ( function() {
     };
 
     /**
-     * recalculate carts totals
-     */
-    function recalc() {
-        const page_cart = document.querySelector( '.cart-page, .checkout-page' );
-
-        let products = page_cart.querySelectorAll( '[data-product]' );
-        if ( products ) {
-            let subtotals = Object.create( null ), fullquantity = 0;
-            subtotals.wh = Object.create( null );
-            subtotals.cart = 0;
-
-            for ( let i = 0; products.length > i; ++i ) {
-                let product = products[ i ];
-                let quantity = parseInt( product.dataset.quantity );
-                let subtotal = product.dataset.price * quantity;
-
-                fullquantity += quantity;
-
-                subtotals.cart += subtotal;
-                subtotals.wh[ product.dataset.wh ] = subtotal + ( subtotals.wh[ product.dataset.wh ] || 0 );
-            }
-
-            let whs = page_cart.querySelectorAll( '.warehouse_subtotal' );
-
-            for ( let i = 0; whs.length > i; ++i ) {
-                let wh = whs[ i ];
-                wh.querySelector( '.subtotal' ).innerHTML = toLocaleCurrency( subtotals.wh[ wh.dataset.wh ] );
-            }
-
-            page_cart.querySelector( '.cart_subtotal' ).innerHTML = toLocaleCurrency( subtotals.cart );
-            page_cart.dataset.total = subtotals.cart;
-            page_cart.dataset.quantity = fullquantity;
-        }
-    }
-
-    /**
      * remove item from cart handler
      */
     $( 'a.cart-remove-item-button' ).click( function( e ) {
