a08b5640a23fba1930f8a05adc84c023859b2195
Alexandr <alexander@s3stores.com>
2021-04-22 17:52:42 +0300

    update total in checkout when change delivery

--- www/static/frontend/jsx/components/Checkout/Checkout.jsx
+++ www/static/frontend/jsx/components/Checkout/Checkout.jsx
@@ -6,6 +6,50 @@ export default class Checkout extends Component {
         super();
         this.PayByCardStripe = createRef();
         this.checkoutSubmit.bind( this );
+        this.state = {
+            // need for update total in checkout only after last request
+            checkoutUpdateQueries: 0,
+        };
+
+        $( document ).on( 'updateRequestSend.checkout', () => {
+            this.setState( {
+                checkoutUpdateQueries: this.state.checkoutUpdateQueries + 1,
+            } );
+
+            $('.order-total_preloader').fadeIn();
+        } );
+
+        $( document ).on( 'updateRequestSuccess.checkout', ( e, total ) => {
+            if ( this.state.checkoutUpdateQueries === 1 ) {
+                this.updateTotal( total );
+            }
+        } );
+
+        $( document ).on( 'updateRequestComplete.checkout', () => {
+            const checkoutUpdateQueries = this.state.checkoutUpdateQueries - 1;
+
+            this.setState( { checkoutUpdateQueries, } );
+
+            //hide preloader if this is the last query
+            if ( checkoutUpdateQueries === 0 ) {
+                $( '.order-total_preloader' ).fadeOut();
+            }
+        } );
+    }
+
+    formatNumber( number ) {
+        return Intl
+            .NumberFormat( 'en-US', { style: 'currency', currency: 'USD' } )
+            .format( number )
+            .substr( 1 );
+    }
+
+    updateTotal( total ) {
+        $( '.order-total .total .price' ).text( this.formatNumber( total[ 'total' ] ) );
+        $( '.shipping-total .price' ).text( this.formatNumber( total[ 'total_shipping_cost' ] ) );
+        $( '.total-sales-tax .price' ).text( this.formatNumber( total[ 'total_sales_tax' ] ) );
+        $( '.total-vat-tax .price' ).text( this.formatNumber( total[ 'total_vat_tax' ] ) );
+        $( '.grand-total .price' ).text( this.formatNumber( total[ 'grand_total' ] ) );
     }
 
     componentDidMount() {
