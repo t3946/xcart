bfca332d7e09cd1b8fa14e5c5ce1ae554246a174
Alexandr <alexander@s3stores.com>
2021-05-06 13:40:22 +0300

    fix no active class

--- www/static/frontend/jsx/components/product/card/QuantityGroup.jsx
+++ www/static/frontend/jsx/components/product/card/QuantityGroup.jsx
@@ -1,51 +1,60 @@
 import classnames from "classnames";
+import _ from "lodash";
 
 export default class QuantityGroup extends Component {
   constructor(props) {
     super(props);
+
+    const min = parseInt(props.product.min_amount);
+
+    this.state = {
+      min,
+      quantity: min,
+      max: parseInt(props.product.avail),
+      step: props.product.mult_order_quantity === "Y" ? min : 1,
+    };
   }
 
   render(props) {
-    const product = props.product;
-    const min = product.min_amount;
-    const quantity = min;
-    const step = product.mult_order_quantity === "Y" ? min : 1;
-
-    const classes = props.classes || {};
-    const groupClass = classnames("quantity-group", classes.group);
-    const btnClass = ["quantity-group-btn", classes.button];
-    const decClass = classnames([
-      btnClass,
-      "quantity-group-btn_dec",
-      { "quantity-group-btn_active": quantity > min },
-    ]);
-    const incClass = classnames([
-      btnClass,
-      "quantity-group-btn_inc",
-      { "quantity-group-btn_active": quantity < product.avail },
-    ]);
+    const classes = _.merge(props.classes, {
+      group: ["quantity-group"],
+      dec: [
+        "quantity-group-btn",
+        "quantity-group-btn_dec",
+        { "quantity-group-btn_active": this.state.quantity > this.state.min },
+      ],
+      inc: [
+        "quantity-group-btn",
+        "quantity-group-btn_inc",
+        { "quantity-group-btn_active": this.state.quantity < this.state.max },
+      ],
+    });
 
     return (
-      <div className={groupClass}>
-        <span className={decClass}>
+      <div className={classnames(classes.group)}>
+        {/*dec button*/}
+        <span className={classnames(classes.dec)}>
           <svg className="icon quantity-group-icon">
             <use xlinkHref="/static/frontend/images/icons/sprite.svg#switcher-minus" />
           </svg>
         </span>
+
         <input
           className="quantity-group-input"
           type="number"
           name="quantity"
-          min={min}
-          max={product.avail}
-          data-min={min}
-          step={step}
-          value={quantity}
-          id={"quantity-" + product.productid}
+          min={this.state.min}
+          max={this.state.max}
+          data-min={this.state.min}
+          step={this.state.step}
+          value={this.state.quantity}
+          id={"quantity-" + props.product.productid}
           autoComplete="off"
           inputMode="numeric"
         />
-        <span className={incClass}>
+
+        {/*inc button*/}
+        <span className={classnames(classes.inc)}>
           <svg className="icon quantity-group-icon">
             <use xlinkHref="/static/frontend/images/icons/sprite.svg#switcher-plus" />
           </svg>
