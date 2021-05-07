df97e6f58529d9ae92ac0cf52196d0ed922eb905
Alexandr <alexander@s3stores.com>
2021-05-07 13:23:52 +0300

    fix subtotal for distributor cart on checkout

--- www/static/frontend/jsx/js/Components/checkout/Checkout.js
+++ www/static/frontend/jsx/js/Components/checkout/Checkout.js
@@ -36,7 +36,32 @@ export default ( function () {
                 Constructor.prototype.update( data );
             }
         } );
+
+        /**
+         * recount subtotal for cart distributor with shipping cost
+         * SERVER PROBLEM:
+         * Server always return subtotal without shipping cost thus
+         * when page loaded with shipping method need add shipping cost to subtotal
+         */
+        $('.warehouse_products').each((i, elem) => {
+            const $distributorCart = $(elem);
+            const $shipping = $distributorCart.find('.shipping-methods-group input:checked');
+
+            //if no shipping methods then subtotal is correct
+            if ($shipping.length === 0){
+                return;
+            }
+
+            const $subtotal = $distributorCart.find('.subtotal');
+            const clearSubtotal = $subtotal.text();
+            const shippingCost = $shipping.data('shipping-cost');
+            const subtotal = parseFloat(clearSubtotal) + parseFloat(shippingCost);
+
+            $subtotal.text(this.formatNumber(subtotal));
+        });
     }
+
+
 
     Constructor.prototype.formatNumber = function( number ) {
         return Intl
@@ -58,6 +83,15 @@ export default ( function () {
             success: function ( res ) {
                 $( document ).trigger( 'updateRequestSuccess.checkout', res );
 
+                if ( res.templates.payment_methods && PaymentMethods ) {
+                    PaymentMethods.updateTemplate( res.templates.payment_methods );
+                }
+
+                if ( res.templates.shipping_methods ) {
+                    ShippingMethods.updateTemplate( res.templates.shipping_methods );
+                    $( document ).trigger( 'update.total.checkout', res );
+                }
+
                 for ( let manufacturer_id in res.distributor_carts ) {
                     const manufacturer = res.distributor_carts[ manufacturer_id ];
                     const whTotal = $( `.warehouse_subtotal[data-wh=${ manufacturer_id }]` );
@@ -66,9 +100,9 @@ export default ( function () {
 
                     if ( manufacturer[ 'sales_tax' ] ) {
                         $salesTax
-                            .show()
-                            .find( '.subtotal' )
-                            .text( self.formatNumber( manufacturer[ 'sales_tax' ] ) );
+                          .show()
+                          .find( '.subtotal' )
+                          .text( self.formatNumber( manufacturer[ 'sales_tax' ] ) );
                     } else {
                         $salesTax.hide();
                     }
@@ -77,25 +111,18 @@ export default ( function () {
 
                     if ( manufacturer[ 'vat_tax' ] ) {
                         $vaxTax
-                            .show()
-                            .find( '.subtotal' )
-                            .text( self.formatNumber( parseFloat( manufacturer[ 'vat_tax' ] ) ) );
+                          .show()
+                          .find( '.subtotal' )
+                          .text( self.formatNumber( parseFloat( manufacturer[ 'vat_tax' ] ) ) );
                     } else {
                         $vaxTax.hide();
                     }
 
+                    const deliveryCost = whTotal.closest('.warehouse_products').find('.shipping-methods-group input:checked').data('shipping-cost') || 0;
+
                     whTotal
-                        .find( '.format_price .subtotal' )
-                        .text( self.formatNumber( parseFloat( manufacturer[ 'subtotal' ] ) ) );
-                }
-
-                if ( res.templates.payment_methods && PaymentMethods ) {
-                    PaymentMethods.updateTemplate( res.templates.payment_methods );
-                }
-
-                if ( res.templates.shipping_methods ) {
-                    ShippingMethods.updateTemplate( res.templates.shipping_methods );
-                    $( document ).trigger( 'update.total.checkout', res );
+                      .find( '.format_price .subtotal' )
+                      .text( self.formatNumber( parseFloat( manufacturer[ 'subtotal' ] ) + deliveryCost ) );
                 }
 
                 if ( typeof callback === "function" ) {
