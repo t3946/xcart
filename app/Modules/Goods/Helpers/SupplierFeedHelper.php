3de69057d8744a1406a4421f35f8a881c55bbddf
Roman Novokshonov <kshon@mail.ru>
2021-05-11 17:16:29 +0300

    up

--- app/Modules/Goods/Helpers/SupplierFeedHelper.php
+++ app/Modules/Goods/Helpers/SupplierFeedHelper.php
@@ -401,7 +401,7 @@ class SupplierFeedHelper
                             $fv_name_arr = [$fv_name_arr];
                         }
                         foreach ($fv_name_arr as $fv_name) {
-                            if (($fv_name = trim($fv_name)) && strlen($f_name) <= 512) {
+                            if (($fv_name = trim($fv_name)) && strlen($fv_name) <= 768) {
                                 [$filterValueModel] = FilterValueModel::objects()->getOrCreate(['f_id' => $filterModel->f_id, 'fv_name' => $fv_name]);
                                 FilterProductModel::objects()->getOrCreate(['fv_id' => $filterValueModel->fv_id, 'productid' => $model->productid, 'is_feed' => 1]);
                                 $fv_ids[] = $filterValueModel->fv_id;
