e44aa598b53fb96123b282509b55d55ea5b8bf45
Alexandr <alexander@s3stores.com>
2021-05-13 15:57:49 +0300

    format

--- www/static/frontend/jsx/js/Components/checkout/ShippingMethods.js
+++ www/static/frontend/jsx/js/Components/checkout/ShippingMethods.js
@@ -1,45 +1,47 @@
-export const ShippingMethods = ( function () {
-    let $shippingMethodsGroups = $( '.shipping-methods-group' );
+export const ShippingMethods = (function () {
+  let $shippingMethodsGroups = $(".shipping-methods-group");
 
-    function updateClasses() {
-        $shippingMethodsGroups.each( function ( i, elem ) {
-            $( elem )
-                .find( '.shipping-method-row' )
-                .removeClass( 'shipping-method-row_selected' )
-                .find( 'input:checked' )
-                .parents( '.shipping-method-row' )
-                .addClass( 'shipping-method-row_selected' );
-        } );
-    }
+  function updateClasses() {
+    $shippingMethodsGroups.each(function (i, elem) {
+      $(elem)
+        .find(".shipping-method-row")
+        .removeClass("shipping-method-row_selected")
+        .find("input:checked")
+        .parents(".shipping-method-row")
+        .addClass("shipping-method-row_selected");
+    });
+  }
 
-    const constructor = function () {
-        initHandlers();
-        updateClasses();
-    }
+  const constructor = function () {
+    initHandlers();
+    updateClasses();
+  };
 
-    function initHandlers() {
-        $shippingMethodsGroups.on( 'change', 'input', function () {
-            updateClasses( $( this ).parents( '.shipping-methods-group' ) );
-        } );
-    }
+  function initHandlers() {
+    $shippingMethodsGroups.on("change", "input", function () {
+      updateClasses($(this).parents(".shipping-methods-group"));
+    });
+  }
 
-    constructor.prototype.updateTemplate = function ( template ) {
-        const $newShippingMethodsGroups = $( template ).filter( '.shipping-methods-group' );
+  constructor.prototype.updateTemplate = function (template) {
+    const $newShippingMethodsGroups = $(template).filter(
+      ".shipping-methods-group"
+    );
 
-        $newShippingMethodsGroups.each( function ( i, e ) {
-            $shippingMethodsGroups
-                .eq( i )
-                .html( e.outerHTML )
-                .on( 'change', 'input', function () {
-                    updateClasses( $( this ).parents( '.shipping-methods-group' ) );
-                } );
-        } );
+    $newShippingMethodsGroups.each(function (i, e) {
+      $shippingMethodsGroups
+        .eq(i)
+        .html(e.outerHTML)
+        .on("change", "input", function () {
+          updateClasses($(this).parents(".shipping-methods-group"));
+        });
+    });
 
-        $shippingMethodsGroups = $( '.shipping-methods-group' ); // = $newShippingMethodsGroups;
+    $shippingMethodsGroups = $(".shipping-methods-group");
 
-        initHandlers();
-        updateClasses();
-    }
+    initHandlers();
+    updateClasses();
+  };
 
-    return new constructor();
-} )();
+  return new constructor();
+})();
