2ab36f5a52e9073347e14fc40f9abfa33c89cb65
Alexandr <alexander@s3stores.com>
2021-05-18 10:17:42 +0300

    fix price wrapping styles

--- www/static/frontend/jsx/components/product/card/components/Price.jsx
+++ www/static/frontend/jsx/components/product/card/components/Price.jsx
@@ -1,12 +1,15 @@
-import { Fragment } from 'preact';
+import classNames from "classnames";
+import { Fragment } from "preact";
 
-export default function Price( { currency, price } ) {
-    return (
-        <Fragment>
-            { currency.symbol_prefix }
-            { !currency.after && currency.currency }
-            <span className="price"> { price }</span>
-            { currency.after && currency.currency }
-        </Fragment>
-    );
+export default function Price(props) {
+  const { currency, price, classes } = props;
+
+  return (
+    <Fragment>
+      {currency.symbol_prefix}
+      {!currency.after && currency.currency}
+      <span className={classNames(["price-number", classes])}> {price}</span>
+      {currency.after && currency.currency}
+    </Fragment>
+  );
 }
