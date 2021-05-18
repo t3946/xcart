c87bc849b82daeffa6f3aecf7a7c3e199f2dbe4f
Alexandr <alexander@s3stores.com>
2021-05-18 12:20:46 +0300

    ignore composer packages

--- app/include/vendors/caponica/amazon-mws-complete/src/CaponicaAmazonMwsComplete/Domain/Report/InvalidReportHeaderException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace CaponicaAmazonMwsComplete\Domain\Report;
-
-/**
- * Exception thrown when a report header does not match the expected value
- */
-class InvalidReportHeaderException extends \Exception
-{
-}
