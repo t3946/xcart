3a19fcb7edfad79a8e5512a4af3afa0a37b324a4
Roman Novokshonov <kshon@mail.ru>
2021-05-18 09:49:11 +0300

    optimize google shopping feed

--- app/Modules/Goods/Commands/GoogleShoppingProductCommand.php
+++ app/Modules/Goods/Commands/GoogleShoppingProductCommand.php
@@ -147,10 +147,19 @@ class GoogleShoppingProductCommand extends Command
                                 ->exclude(['base_state_zipcode' => ''])
                                 ->order(['state'])
                                 ->all()) {
+                                $average_shipping = null;
                                 /** @var StateModel $state */
                                 foreach ($states as $state) {
                                     $states[$state->stateid] = $state;
-                                    $rates[$state->stateid] = ShippingHelper::getStateShipping($product->productid, $product->min_amount ?? 1, $state);
+                                    if ($average_shipping === null && !in_array($state->code, ['AK', 'HI'], true)) {
+                                        $average_shipping = ShippingHelper::getStateShipping($product->productid, $product->min_amount ?? 1, $state);
+                                    }
+                                    if (in_array($state->code, ['AK', 'HI'], true)) {
+                                        $state_shipping = ShippingHelper::getStateShipping($product->productid, $product->min_amount ?? 1, $state);
+                                    } else {
+                                        $state_shipping = $average_shipping;
+                                    }
+                                    $rates[$state->stateid] = $state_shipping;
                                     $rate = reset($rates[$state->stateid]);
                                     if ($rate && $sModel = $rate->shipping) {
                                         $shipping = new Google_Service_ShoppingContent_ProductShipping();
