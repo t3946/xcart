7baba82fdd28ac0f8cb482ef9f4092e4106dcac3
Alexandr <alexander@s3stores.com>
2021-05-26 17:04:53 +0300

    restore google autocomplete on checkout

--- www/static/frontend/jsx/js/Components/checkout/ShippingForm.js
+++ www/static/frontend/jsx/js/Components/checkout/ShippingForm.js
@@ -1,3 +1,4 @@
+import { ShippingGoogleAutoComplete } from "@/js/Classes/ShippingGoogleAutoComplete";
 import { SwitcherButton } from "@/js/Classes/SwitcherButton";
 import "node_modules/imask";
 
@@ -32,6 +33,25 @@ export const ShippingForm = ( function () {
         switcher.isOn = true;
         this.$otherFields.stop( true, false ).slideDown();
     }
+
+    // autocomplete for main address field
+    const componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'short_name',
+        administrative_area_level_1: 'long_name',
+        country: 'long_name',
+        postal_code: 'short_name',
+    };
+
+    const shipping_fields = {
+        locality: '#CheckoutForm_s_city',
+        administrative_area_level_1: '#CheckoutForm_s_state',
+        country: '#CheckoutForm_s_country',
+        postal_code: '#CheckoutForm_s_zipcode',
+    };
+
+    new ShippingGoogleAutoComplete( '#CheckoutForm_s_full_address', componentForm, shipping_fields );
 
     return new constructor();
 } )();
