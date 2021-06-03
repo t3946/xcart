86d9ee6ffc471a97eae7134eeec7de3d345971bf
alexander <alexander@s3stores.com>
2021-06-03 17:16:49 +0300

    turn on development mode

--- www/static/config/webpack.backend.js
+++ www/static/config/webpack.backend.js
@@ -3,6 +3,7 @@ const _ = require("lodash");
 const path = require("path");
 
 const config = _.merge(baseConfig, {
+  mode: "development",
   entry: "./backend/jsx/main.jsx",
   output: {
     path: path.resolve("./backend/dist/js"),
