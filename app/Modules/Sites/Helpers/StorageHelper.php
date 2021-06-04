b91ae0c200aa877d4de4e3b4cde9f65685b3eda2
alexander <alexander@s3stores.com>
2021-06-04 10:59:22 +0300

    hierarchy of context && throw exception when bad parameters

--- app/Modules/Sites/Helpers/StorageHelper.php
+++ app/Modules/Sites/Helpers/StorageHelper.php
@@ -4,6 +4,12 @@
 namespace Modules\Sites\Helpers;
 
 
+use Exception;
+
+
+/**
+ * Помогает передвать данные с сервера на клиент при рендеринге
+*/
 class StorageHelper
 {
     private static array $store = [];
@@ -11,19 +17,44 @@ class StorageHelper
     /**
      * сохранить данные в хранилище
      * @param object|array|string|int|float|bool|null $data сохраняемые данные
-     * @param string $key ключ по которому будут сохранены данные
+     * @param string|null $key ключ по которому будут сохранены данные
+     * @param string|null $context может быть записан как имена разделённые слэшем name1/name2
+     * для создания иерархии контекстов
+     * @throws Exception
      */
-    public static function push($data, string $key, string $context = ''): void
+    public static function push($data, ?string $key, ?string $context = ''): void
     {
         if ($context) {
-            if (!isset(self::$store[$context])) {
-                self::$store[$context] = [];
+            $namespaces = explode('/', $context);
+            $target = &self::$store;
+
+            foreach ($namespaces as $namespace) {
+                if (!isset($target[$namespace])) {
+                    $target[$namespace] = [];
+                }
+
+                $target = &$target[$namespace];
             }
 
-            self::$store[$context][$key] = $data;
+            if ($key) {
+                $target[$key] = $data;
+            } else {
+                $target = $data;
+            }
         } else {
+            if (!$key) {
+                /**
+                 * Невозможно определить к чему отнести отправленные данные, если у них нет ни ключа ни контекста
+                */
+                throw new Exception("\$key and \$context can't be null in one time");
+            }
+
             self::$store[$key] = $data;
         }
+    }
+
+    public static function getStorage(): array {
+        return self::$store;
     }
 
     public static function print(): string
