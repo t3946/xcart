ca3597478b64e0419e96dae61696de0d52be76a9
Roman Novokshonov <kshon@mail.ru>
2021-07-20 13:45:08 +0300

    disable flat shipping

--- app/include/Xcart/Shipping/UPSFlat.php
+++ app/include/Xcart/Shipping/UPSFlat.php
@@ -2,13 +2,11 @@
 
 namespace Xcart\Shipping;
 
-use Xcart\App\Main\Xcart;
 
 class UPSFlat extends UPS
 {
     public function isProcessorApplicable()
     {
-        $config = $this->getManufacturer()->site->getConfig();
-        return $config['flat_shipping_enabled'] !== 'N';
+        return false;
     }
 }
