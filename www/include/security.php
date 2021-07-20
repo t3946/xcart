95842bde8c51371681775bdae9fe95682039faa6
Roman Novokshonov <kshon@mail.ru>
2021-07-20 16:43:43 +0300

    fix security namespaces

--- www/include/security.php
+++ www/include/security.php
@@ -1,18 +1,16 @@
 <?php
 
-use Xcart\App\Main\Xcart;
-
 if ( !defined('XCART_SESSION_START') ) { header("Location: ../"); die("Access denied"); }
 
-$user = Xcart::app()->auth->getUser();
+$app = Xcart\App\Main\Xcart::app();
+$user = $app->auth->getUser();
 
 if (!$user || $user->getIsGuest()) {
-    Xcart::app()->request->redirect('admin:login');
+    $app->request->redirect('admin:login');
 }
 
-/** @var \Modules\User\Models\RoleModel $role */
-if (($uModel = Xcart::app()->user) && $role = $uModel->role) {
-    if (!$role->canRequest(Xcart::app()->request)) {
-        Xcart::app()->request->redirect('/admin/error_message.php?access_denied&id=25');
+if (($uModel = $app->user) && $role = $uModel->role) {
+    if (!$role->canRequest($app->request)) {
+        $app->request->redirect('/admin/error_message.php?access_denied&id=25');
     }
 }
