14331ce6a6f77963dc70c4f66996928d2cf623ee
alexander <alexander@s3stores.com>
2021-07-22 09:30:01 +0300

    update form input

--- www/static/frontend/jsx/modules/account/components/shared/FormInput.tsx
+++ www/static/frontend/jsx/modules/account/components/shared/FormInput.tsx
@@ -1,16 +1,43 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import classnames from "classnames";
 
-export const FormInput = ({ label }) => {
+export const FormInput = (props: any) => {
+  const { label, placeholder, classes, name, id, type, caption, handleChange } =
+    props;
+
+  function labelTemplate() {
+    if (label) {
+      return (
+        <label
+          htmlFor={id}
+          className={classnames("form-input-label", classes.label)}
+        >
+          {label}
+        </label>
+      );
+    }
+  }
+
   return (
     <Grid
-      className="form-input-container"
+      className={classnames("form-input-container", classes.group)}
       container
       justify="space-between"
       alignItems="center"
     >
-      <div className="form-input-label">{label}</div>
-      <input placeholder="Hello world" className="form-input" />
+      {labelTemplate()}
+
+      <input
+        placeholder={placeholder}
+        className="form-input"
+        name={name}
+        id={id}
+        type={type ? type : "text"}
+        onChange={handleChange}
+      />
+
+      {caption && <div className="form-input-caption">{caption}</div>}
     </Grid>
   );
 };
