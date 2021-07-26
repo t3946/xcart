efe803921fcaf3b3aa6a4dee0821c8840fceb3fa
alexander <alexander@s3stores.com>
2021-07-26 16:02:05 +0300

    add login icon in hat

--- www/static/frontend/jsx/modules/account/components/hat/HatNavigation.tsx
+++ www/static/frontend/jsx/modules/account/components/hat/HatNavigation.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import classNames from "classnames";
 
 const HatNavigation = (props) => {
+  function loginButton() {
+    const classes = ["navigation-login-button common-icon"];
+
+    if (appData.user) {
+      classes.push("navigation-login-button__logged");
+    } else {
+      classes.push("navigation-login-button__not-logged");
+    }
+
+    return <i className={classNames(classes)} />;
+  }
+
   return (
     <div id="top-header-content">
       <div id="top-header-menu">
@@ -19,7 +32,7 @@ const HatNavigation = (props) => {
                     href="#"
                     data-toggle="offCanvasLeft"
                     className="mobile_menu middle-inline-block hamburger"
-                  ></a>
+                  />
                 </div>
 
                 <div className="col-3 col-md-2">
@@ -48,25 +61,31 @@ const HatNavigation = (props) => {
                   </div>
                 </div>
 
-                <div className="col-6 col-md-4 hide-for-large small-offset-0 medium-offset-0 text-align--right mobile-header">
-                  <a
-                    href="tel:18009292431"
-                    className="mobile__call-btn middle-inline-block right-icon"
-                  ></a>
+                <div className="col-6 col-md-4 hide-for-large small-offset-0 medium-offset-0 mobile-header d-flex align-items-center justify-content-end">
+                  <div className="hat-icons-container d-flex justify-content-between w-100">
+                    <a
+                      href="tel:18009292431"
+                      className="mobile__call-btn middle-inline-block right-icon"
+                    ></a>
 
-                  <a
-                    className="mobile__search-btn middle-inline-block right-icon"
-                    data-swich="search_container"
-                  ></a>
+                    <a
+                      className="mobile__search-btn middle-inline-block right-icon"
+                      data-swich="search_container"
+                    />
 
-                  <a
-                    href={appData.routes["cart:list"]}
-                    className="mobile__cart middle-inline-block right-icon"
-                  >
-                    <span className="count">
-                      <span className="mc_count">{appData.Cart.quantity}</span>
-                    </span>
-                  </a>
+                    {loginButton()}
+
+                    <a
+                      href={appData.routes["cart:list"]}
+                      className="mobile__cart middle-inline-block right-icon"
+                    >
+                      <span className="count">
+                        <span className="mc_count">
+                          {appData.Cart.quantity}
+                        </span>
+                      </span>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
