c0b91f3ad7f732b7b5730b0559a4b51b3a06dfe5
alexander <alexander@s3stores.com>
2021-07-28 16:02:13 +0300

    close mobile menu after click on a link

--- www/static/frontend/jsx/modules/account/components/sidebar-menu/SideBarMenuAccordIonItem.tsx
+++ www/static/frontend/jsx/modules/account/components/sidebar-menu/SideBarMenuAccordIonItem.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { SidebarItemDto } from "@modules/account/ts/types/sidebar-item.type";
 import { NavLink } from "react-router-dom";
+import { setMobileMenuVisible } from "../../../../redux/actions/account-actions/MobileMenuActions";
+import { useDispatch } from "react-redux";
 
 export const SideBarMenuAccordIonItem: React.FC<SidebarItemDto> = ({
   to,
   label,
 }) => {
+  const dispatch = useDispatch();
   return (
     <NavLink
       to={to}
       className="sidebar-menu-container sidebar-menu-accordion-item"
       activeClassName="accordion-active"
       exact={true}
+      onClick={() => dispatch(setMobileMenuVisible(false))}
     >
       {label}
     </NavLink>
--- www/static/frontend/jsx/modules/account/components/sidebar-menu/SideBarMenuItem.tsx
+++ www/static/frontend/jsx/modules/account/components/sidebar-menu/SideBarMenuItem.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import classNames from "classnames";
+import { setMobileMenuVisible } from "../../../../redux/actions/account-actions/MobileMenuActions";
+import { useDispatch } from "react-redux";
 
 interface sideBarMenuItemPropsDto {
   to: string;
   label: string;
   className?: any;
+  onClick?: any;
 }
 
 export const SideBarMenuItem: React.FC<sideBarMenuItemPropsDto> = ({
@@ -13,12 +16,15 @@ export const SideBarMenuItem: React.FC<sideBarMenuItemPropsDto> = ({
   label,
   className,
 }) => {
+  const dispatch = useDispatch();
+
   return (
     <NavLink
       to={to}
       exact={true}
       activeClassName="active-route"
       className={classNames("sidebar-menu-container", className)}
+      onClick={() => dispatch(setMobileMenuVisible(false))}
     >
       <span>{label}</span>
     </NavLink>
