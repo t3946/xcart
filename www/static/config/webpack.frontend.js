183c55fc57b4c40cefc06e51223f8b7f67438c93
alexander <alexander@s3stores.com>
2021-08-04 10:15:49 +0300

    fix client aliases

--- www/static/config/webpack.frontend.js
+++ www/static/config/webpack.frontend.js
@@ -8,7 +8,7 @@ config = {
   output: {
     path: path.resolve("./" + paths.dst.jsx),
     filename: "[name].[hash].js",
-    clean: true
+    clean: true,
   },
   target: "web",
   resolve: {
@@ -20,6 +20,8 @@ config = {
       // Not necessary unless you consume a module using `createClass`
       "create-react-class": "preact/compat/lib/create-react-class",
       "@": path.resolve("./frontend/jsx"),
+      "@client/modules": path.resolve("./frontend/jsx/modules"),
+      "@client": path.resolve("./frontend"),
     },
 
     modules: [
