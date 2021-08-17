e2f72fa4b6fc87f01043b01b7d3de7dca6e873c6
alexander <alexander@s3stores.com>
2021-08-17 12:42:41 +0300

    login and securities settings list

--- www/static/frontend/jsx/modules/account/components/login-and-security/LoginAndSecurity.tsx
+++ www/static/frontend/jsx/modules/account/components/login-and-security/LoginAndSecurity.tsx
@@ -1,24 +1,99 @@
 import React, { useContext } from "react";
-import { Redirect } from "react-router-dom";
+import { NavLink, Redirect } from "react-router-dom";
 import { route } from "@client/jsx/utils/AppData";
 import { useSelector } from "react-redux";
 import { StoreDto } from "@s3stores-mail/ts/types";
 import { SnackbarContext } from "@client/modules/account/contexts/snackbar/Snackbar.context";
+import classnames from "classnames";
 
 const LoginAndSecurity = (): any => {
   const user = useSelector((e: StoreDto) => e.user);
   const { showSnackbar } = useContext(SnackbarContext);
+  const listItems = [
+    {
+      title: "full name",
+      caption: user.name,
+    },
+    {
+      title: "email",
+      caption: user.email,
+    },
+    {
+      title: "mobile phone number",
+      caption: user.phone,
+    },
+    {
+      title: "password",
+      caption: "********",
+    },
+    {
+      title: "two-step verification (2SV) settings",
+      caption: "Manage your Two-Step Verification (2SV) Authenticators",
+      classes: {
+        caption: "settings-item-caption__small",
+      },
+    },
+    {
+      title: "secure your account",
+      caption:
+        "If you think your S3 Stores account has been compromised, follow these steps to make your account more secure",
+      classes: {
+        caption: "settings-item-caption__small",
+      },
+    },
+  ];
 
-  showSnackbar({
-    header: "Success",
-    message: "You have successfully modified your account!",
-    theme: "wrong",
-  });
+  function settingsItemsTemplate() {
+    const items = [];
+
+    for (const listItem of listItems) {
+      items.push(
+        <li className="login-and-security-settings-item">
+          <div className="d-flex align-items-center justify-content-between">
+            <div>
+              <b className="settings-item-title">{listItem.title}:</b>
+              <br />
+              <span
+                className={classnames(
+                  "settings-item-caption",
+                  listItem.classes?.caption
+                )}
+              >
+                {listItem.caption}
+              </span>
+            </div>
+
+            <button className={"form-button form-button__outline w-auto"}>
+              edit
+            </button>
+          </div>
+        </li>
+      );
+    }
+
+    return items;
+  }
 
   return (
     <>
-      <div>LoginAndSecurity</div>
+      <div className="page-label">Login & security</div>
       {!user && <Redirect to={route("account:login")} />}
+
+      <div className="content-panel login-and-security-settings-panel p-0">
+        <ul className={"list-unstyled m-0"}>{settingsItemsTemplate()}</ul>
+      </div>
+
+      <NavLink
+        to={route("account:dashboard")}
+        exact={true}
+        className="common-link login-and-security_submit-button d-inline-block mt-4"
+      >
+        <button
+          className={"admin-form-control form-button w-md-auto d-inline-block"}
+        >
+          done
+        </button>
+      </NavLink>
     </>
   );
 };
