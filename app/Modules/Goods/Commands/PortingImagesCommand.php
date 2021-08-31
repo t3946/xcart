b503fe6543103fc99ec708e58651f8fd5bb6583c
Roman Novokshonov <kshon@mail.ru>
2021-08-31 16:22:09 +0300

    porting all not downloaded images

--- app/Modules/Goods/Commands/PortingImagesCommand.php
+++ app/Modules/Goods/Commands/PortingImagesCommand.php
@@ -14,14 +14,11 @@ class PortingImagesCommand extends Command
     public function handle($arguments = [])
     {
         $sql = <<<SQL
-        SELECT i1.*
-FROM xcart_images_D AS i1
-LEFT JOIN xcart_product_images i2 ON i1.md5 = i2.hash 
-INNER JOIN xcart_products p ON i1.id = p.productid AND p.forsale = 'Y'
-WHERE i2.image_id IS NULL
-AND i1.avail = 'Y'
-AND image_path NOT REGEXP '/D/[0-9]{1,3}_'
-ORDER BY id desc, orderby
+        SELECT p.productid FROM xcart_products p
+LEFT JOIN xcart_products_images i ON i.product_id = p.productid
+WHERE forsale ='Y'
+AND i.image_id IS NULL
+AND EXISTS (SELECT 1 FROM xcart_images_D d WHERE d.id = p.productid)
 SQL;
 
         $stmt = Connection::getInstance()->executeQuery($sql);
@@ -30,23 +27,24 @@ SQL;
         foreach ($images_data as $data) {
             /** @var ProductModel $product */
 
-            $product = ProductModel::objects()->get(['productid' => $data['id']]);
-            $image = ImageDModel::objects()->get(['imageid' => $data['imageid']]);
-
+            $product = ProductModel::objects()->get(['productid' => $data['productid']]);
             $site = $product->sites->limit(1)->get();
-            $pref = $site->Enable_CDN ? 'cdn.': 'www.';
-            $domain = $site->getBaseDomain();
-            $image_url = 'https://' .$pref . $domain . $image->getURL();
 
-            $action = [
-                'product_id' => $product->pk,
-                'dx_code' => $product->distributor->code,
-                'image_position' => 0,
-                'image_link' => $image_url,
-                'action' => 'create'
-            ];
-            Xcart::app()->queue->send('images_action', json_encode($action, JSON_THROW_ON_ERROR));
-            print_r($action);
+            foreach (ImageDModel::objects()->filter(['id' => $data['productid']])->all() as $image) {
+                $pref = $site->Enable_CDN ? 'cdn.': 'www.';
+                $domain = $site->getBaseDomain();
+                $image_url = 'https://' .$pref . $domain . $image->getURL();
+
+                $action = [
+                    'product_id' => $product->pk,
+                    'dx_code' => $product->distributor->code,
+                    'image_position' => $image->orderby,
+                    'image_link' => $image_url,
+                    'action' => 'create'
+                ];
+                Xcart::app()->queue->send('images_action', json_encode($action, JSON_THROW_ON_ERROR));
+                print_r($action);
+            }
         }
     }
 }
