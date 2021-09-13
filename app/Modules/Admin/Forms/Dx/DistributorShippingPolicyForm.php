47c40e44e7d8de8be5e4cf02e53460e85116cebf
Roman Novokshonov <kshon@mail.ru>
2021-09-13 12:26:22 +0300

    show dropship fee in dx currency

--- app/Modules/Admin/Forms/Dx/DistributorShippingPolicyForm.php
+++ app/Modules/Admin/Forms/Dx/DistributorShippingPolicyForm.php
@@ -208,7 +208,7 @@ class DistributorShippingPolicyForm extends DistributorForm
             'd_drop_ship_fee_type' => [
                 'class' => DropDownField::class,
                 'choices' => [
-                    'value' => 'US$',
+                    'value' => $dx->symbol_prefix.$dx->symbol,
                     'percent' => '% of subtotal in Cost to us pricing',
                 ]
             ],
