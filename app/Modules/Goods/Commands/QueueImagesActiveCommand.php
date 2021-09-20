c0202273ecdc5429712f183a9c171ef3a60abfd3
Roman Novokshonov <kshon@mail.ru>
2021-09-20 17:20:33 +0300

    fix image links save

--- app/Modules/Goods/Commands/QueueImagesActiveCommand.php
+++ app/Modules/Goods/Commands/QueueImagesActiveCommand.php
@@ -26,13 +26,16 @@ class QueueImagesActiveCommand extends Command
         /** @var ProductImageModel $model */
 
         if ($message->body && $data = json_decode($message->body, true, 512, JSON_THROW_ON_ERROR)) {
-            [$image_by_link] = ProductImageLinkModel::objects()->getOrNew(
-                ['hash' => $data['image_link_hash']]
+            /** @var ProductImageModel $image */
+            [$image] = ProductImageModel::objects()->getOrCreate(
+                ['hash' => $data['image_hash']]
             );
 
-            /** @var ProductImageModel $image */
-            [$image] = ProductImageModel::objects()->getOrNew(
-                ['hash' => $data['image_hash']]
+            [$link] = ProductImageLinkModel::objects()->getOrNew(
+                [
+                    'hash' => $data['image_link_hash'],
+                    'image_id' => $image->pk,
+                ]
             );
 
             $image->setAttributes([
@@ -52,20 +55,20 @@ class QueueImagesActiveCommand extends Command
 
             $image->save();
 
-            $image_by_link->setAttributes([
+            $link->setAttributes([
                 'image_id' => $image->pk,
                 'url' => $data['image_link']
             ]);
 
-            if ($image_by_link->getChangedAttributes()) {
+            if ($link->getChangedAttributes()) {
                 echo "Image link has changed\n";
                 echo "==============Queue data=============\n";
                 print_r($data);
                 echo "==============Changed image data=============\n";
-                print_r($image_by_link->getChangedAttributes());
+                print_r($link->getChangedAttributes());
             }
 
-            $image_by_link->save();
+            $link->save();
 
 
             if (isset($data['product_id']) && $product = ProductModel::objects()->get(
