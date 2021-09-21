3af133cab4f7faeb90e6f2507ad09d8a4df42d2f
alexander <alexander@s3stores.com>
2021-09-21 13:16:24 +0300

    remove unused

--- app/Modules/Main/Controllers/DefaultController.php
+++ app/Modules/Main/Controllers/DefaultController.php
@@ -14,16 +14,6 @@ class DefaultController extends FrontendController
 
     public function index()
     {
-//        $this->redirect('demo:index');
-
-        //@TODO: To future
-//        /** @var SitesModule $module */
-//        $module = Xcart::app()->getModule('Sites');
-//
-//        $module = $module->getSite()->getDefaultModule();
-//        $controller = new \Modules\Demo\Controllers\DefaultController($this->getRequest());
-//        $controller->run(null, func_get_args());
-
         $site = Xcart::app()->getModule('Sites')->getSite();
 
         $category_new = CategoryModel::objects()->filter(['category' => 'New Products', 'storefrontid' => $site->pk, 'level' => 1])->limit(1)->get();
