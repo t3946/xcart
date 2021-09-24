b05ebc03b3cda62b651d37f211dc60d502a4deec
Roman Novokshonov <kshon@mail.ru>
2021-09-24 16:33:39 +0300

    revert serialize

--- app/include/Xcart/App/Orm/Base.php
+++ app/include/Xcart/App/Orm/Base.php
@@ -19,7 +19,7 @@ use Serializable;
  * @package Xcart\App\Orm
  * @method static Manager objects($instance = null)
  */
-abstract class Base implements ModelInterface, ArrayAccess
+abstract class Base implements ModelInterface, ArrayAccess, Serializable
 {
     use ClassNames;
     /**
@@ -838,18 +838,35 @@ abstract class Base implements ModelInterface, ArrayAccess
         $this->related = [];
     }
 
-    public function __serialize() : array
+    /**
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
     {
-        return ['attributes' => $this->getAttributes(), 'attributesNotField' => $this->attributesNotField];
+        return serialize(['attributes' => $this->getAttributes(), 'attributesNotField' => $this->attributesNotField]);
     }
 
-    public function __unserialize(array $serialized) : void
+    /**
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @throws \Exception
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
     {
-        if ($serialized) {
+        $us = unserialize($serialized);
+        if ($us) {
             $this->attributes = new AttributeCollection;
-            $this->setAttributes($serialized['attributes']);
+            $this->setAttributes($us['attributes']);
 
-            $this->attributesNotField = $serialized['attributesNotField'];
+            $this->attributesNotField = $us['attributesNotField'];
         }
     }
 
