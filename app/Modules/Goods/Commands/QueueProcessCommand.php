5091a35458a60dcd8d642d61c1ff9c490b185bff
Roman Novokshonov <kshon@mail.ru>
2021-10-13 16:28:41 +0300

    don't add products if Inventory feed

--- app/Modules/Goods/Commands/QueueProcessCommand.php
+++ app/Modules/Goods/Commands/QueueProcessCommand.php
@@ -41,7 +41,12 @@ class QueueProcessCommand extends Command
         /** @var ProductModel $group_product */
         /** @var SiteModel $site */
 
-        if ($message->body && $data = json_decode($message->body, true, 512, JSON_THROW_ON_ERROR)) {
+        $body = <<<TAG
+{"productcode":"","ASIN":null,"product":"Wall Mounted Baby Changing Station, Horizontal Fold Down Diaper Changer Table With Safety Straps For Commercial Bathrooms, Ada And Ansi Compliant","descr":"","fulldescr":"Restaurants, businesses, and other locations with public or private restrooms will appreciate the durability, safety, and style infused into this ADA-, ANSI-, and ASTM-compliant baby changing station. The ECR4Kids Wall-Mounted Baby Changing Station is a horizontal fold-down diaper changing table with built-in safety straps, designed for use with infants and toddlers in restrooms at businesses, restaurants, shops, and institutional facilities. It has a smooth and comfortable concave changing surface featuring an adjustable nylon child safety strap with slide release to keep kids safely in place.","brand_name":"Ecr4kids","brand_normalized":false,"forsale":"Y","eta_date_mm_dd_yyyy":null,"upc":"","supplier_categories":["Adult","Infant & toddler","New items","Toddler"],"supplier_internal_id":"https:\/\/www.ecr4kids.com\/products\/ecr4kids-wall-mounted-baby-changing-station-with-500-disposable-liners-horizontal-fold-down-diaper-changer-table-with-safety-straps-for-commercial-bathrooms-ada-and-ansi-compliant-white-granite","hash_product":"826d1e1c701662c70a9779606320979e","images":[],"alt_names":[],"dim_x":null,"dim_y":null,"dim_z":null,"shipping_weight":null,"shipping_dim_x":null,"shipping_dim_y":null,"shipping_dim_z":null,"weight":null,"min_amount":1,"mult_order_quantity":"N","is_group":true,"child_products":[{"productcode":"FIP-ELR-18009","ASIN":null,"product":"White Granite","descr":"","fulldescr":"Restaurants, businesses, and other locations with public or private restrooms will appreciate the durability, safety, and style infused into this ADA-, ANSI-, and ASTM-compliant baby changing station. The ECR4Kids Wall-Mounted Baby Changing Station is a horizontal fold-down diaper changing table with built-in safety straps, designed for use with infants and toddlers in restrooms at businesses, restaurants, shops, and institutional facilities. It has a smooth and comfortable concave changing surface featuring an adjustable nylon child safety strap with slide release to keep kids safely in place.","brand_name":"Ecr4kids","brand_normalized":false,"forsale":"Y","eta_date_mm_dd_yyyy":null,"upc":"7639605474720","supplier_categories":["Adult","Infant & toddler","New items","Toddler"],"supplier_internal_id":"https:\/\/www.ecr4kids.com\/products\/ecr4kids-wall-mounted-baby-changing-station-with-500-disposable-liners-horizontal-fold-down-diaper-changer-table-with-safety-straps-for-commercial-bathrooms-ada-and-ansi-compliant-white-granite","hash_product":"3c8e2b736771ed647dbd570862bd2cad","images":["https:\/\/cdn.shopify.com\/s\/files\/1\/0410\/4205\/1239\/products\/ii4qlkffimuxucsddvgc.jpg?v=1633589772"],"alt_names":[],"dim_x":null,"dim_y":null,"dim_z":null,"shipping_weight":null,"shipping_dim_x":null,"shipping_dim_y":null,"shipping_dim_z":null,"weight":95.25,"min_amount":1,"mult_order_quantity":"N","is_group":false,"child_products":[],"group_mask":"Wall Mounted Baby Changing Station, Horizontal Fold Down Diaper Changer Table With Safety Straps For Commercial Bathrooms, Ada And Ansi Compliant","r_avail":0,"mpn":"ELR-18009","lead_time_message":null,"attributes":null,"product_files":[],"options":[],"videos":[]},{"productcode":"FIP-ELR-17520-GS","ASIN":null,"product":"Grey Speckled","descr":"","fulldescr":"Restaurants, businesses, and other locations with public or private restrooms will appreciate the durability, safety, and style infused into this ADA-, ANSI-, and ASTM-compliant baby changing station. The ECR4Kids Wall-Mounted Baby Changing Station is a horizontal fold-down diaper changing table with built-in safety straps, designed for use with infants and toddlers in restrooms at businesses, restaurants, shops, and institutional facilities. It has a smooth and comfortable concave changing surface featuring an adjustable nylon child safety strap with slide release to keep kids safely in place.","brand_name":"Ecr4kids","brand_normalized":false,"forsale":"Y","eta_date_mm_dd_yyyy":null,"upc":"7639607752642","supplier_categories":["Adult","Infant & toddler","New items","Toddler"],"supplier_internal_id":"https:\/\/www.ecr4kids.com\/products\/ecr4kids-wall-mounted-baby-changing-station-with-500-disposable-liners-horizontal-fold-down-diaper-changer-table-with-safety-straps-for-commercial-bathrooms-ada-and-ansi-compliant-white-granite","hash_product":"9f42c903304b787e1e2c47b68cb69520","images":["https:\/\/cdn.shopify.com\/s\/files\/1\/0410\/4205\/1239\/products\/zu82d2e5d5zi3koux35e.jpg?v=1633589772"],"alt_names":[],"dim_x":null,"dim_y":null,"dim_z":null,"shipping_weight":null,"shipping_dim_x":null,"shipping_dim_y":null,"shipping_dim_z":null,"weight":90.72,"min_amount":1,"mult_order_quantity":"N","is_group":false,"child_products":[],"group_mask":"Wall Mounted Baby Changing Station, Horizontal Fold Down Diaper Changer Table With Safety Straps For Commercial Bathrooms, Ada And Ansi Compliant","r_avail":0,"mpn":"ELR-17520-GS","lead_time_message":null,"attributes":null,"product_files":[],"options":[],"videos":[]}],"group_mask":null,"r_avail":0,"mpn":"","lead_time_message":null,"attributes":null,"product_files":[],"options":[],"videos":[],"manufacturerid":640,"storefront":10}
+TAG;
+
+
+        if ($message->body && $data = json_decode($body, true, 512, JSON_THROW_ON_ERROR)) {
             try {
                 $feed =  null;
                 $data = array_filter($data, static fn($v) => $v !== null);
@@ -58,7 +63,6 @@ class QueueProcessCommand extends Command
 
                 if ($data['source'] !== 'manual' && (!$feed || !$feed->enabled)) {
                     echo "Feed is not active\n";
-                    print_r($data);
                     return;
                 }
 
@@ -69,10 +73,17 @@ class QueueProcessCommand extends Command
                         return;
                     }
                     [$product, $is_new] = ProductModel::objects()->getOrNew(['productcode' => $product_code]);
+
                     if ($is_new && !$site) {
                         echo "Empty site for product {$data['productcode']}\n";
                         return;
                     }
+
+                    if ($is_new && $feed && $feed->feed_type === 'I') {
+                        echo "Dont add product for inventory feed: {$data['productcode']}\n";
+                        return;
+                    }
+
                     if ($data['source'] === 'manual' || $product->hash_product !== $data['hash_product']) {
                         $product->setAttributes($data);
                         if ($data['eta_date_mm_dd_yyyy']) {
@@ -114,6 +125,12 @@ class QueueProcessCommand extends Command
                     $group_code = self::getGroupProductCode($data);
                     /** @var ProductModel $group_product */
                     [$group_product, $is_new] = ProductModel::objects()->getOrNew(['productcode' => $group_code]);
+
+                    if ($is_new && $feed && $feed->feed_type === 'I') {
+                        echo "Dont add group product for inventory feed: {$group_code}\n";
+                        return;
+                    }
+
                     if ($group_product->hash_product !== $data['hash_product']) {
                         $group_product->setAttributes($data);
                         $group_product->group_root = $group_product->productid;
@@ -137,6 +154,12 @@ class QueueProcessCommand extends Command
                             continue;
                         }
                         [$product, $is_new] = ProductModel::objects()->getOrNew(['productcode' => $child_code]);
+
+                        if ($is_new && $feed && $feed->feed_type === 'I') {
+                            echo "Dont add child product for inventory feed: {$child_code}\n";
+                            return;
+                        }
+
                         if ($product->hash_product !== $child['hash_product'] || $product->group_root != $group_product->productid) {
                             $child += ['manufacturerid' => $group_product['manufacturerid']];
                             $product->setAttributes($child);
