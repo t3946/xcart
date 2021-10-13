47228dc8cbcb5355d3cf22c8f65d6a205e8f5597
Roman Novokshonov <kshon@mail.ru>
2021-10-13 11:44:32 +0300

    fix image update thumbs

--- app/Modules/Goods/Admin/ProductImagesAdmin.php
+++ app/Modules/Goods/Admin/ProductImagesAdmin.php
@@ -96,11 +96,12 @@ class ProductImagesAdmin extends ListViewAdmin
                 }
             }
             if ($this->ownerModel->save()) {
+                $link = $imageModel ? $imageModel->path->getValue() : $model->path->getValue();
                 $action = [
                     'product_id' => $product_model->pk ?? $this->ownerPk,
                     'dx_code' => $product_model->distributor->code,
                     'image_position' => $this->ownerModel->order_by,
-                    'image_link' => $imageModel->path->getValue() ?? $model->path->getValue(),
+                    'image_link' => $link,
                     'action' => 'create'
                 ];
                 Xcart::app()->queue->send('images_action', json_encode($action, JSON_THROW_ON_ERROR));
