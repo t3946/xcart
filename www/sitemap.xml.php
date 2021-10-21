c2ef7fa9965d4fe9c3aa398b5fac3587a1dc8058
Roman Novokshonov <kshon@mail.ru>
2021-10-21 16:03:22 +0300

    add sitemap and robots.txt rewrite rules, send gz sitemap headers

--- www/sitemap.xml.php
+++ www/sitemap.xml.php
@@ -1,12 +1,11 @@
 <?php
 $default_host = 'www.artistsupplysource.com';
-if (isset($_SERVER['HTTP_HOST'])) {
-    $host = $_SERVER['HTTP_HOST'];
-} else {
-    $host = $default_host;
-}
-$filename = $host . '-sitemap.xml';
+$host = $_SERVER['HTTP_HOST'] ?? $default_host;
+
+$filename = $host . '-sitemap.xml.gz';
 if (file_exists($filename)) {
-    header("Content-type: text/xml");
+    header("Content-type: application/x-gzip");
+    header("Content-Encoding: gzip");
+    header('Content-Disposition: attachment; filename="'.$filename.'"');
     echo file_get_contents($filename);
 }
