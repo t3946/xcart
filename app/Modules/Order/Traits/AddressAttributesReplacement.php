de409ee0a370f0bdeab385b5db6fe6a899c698c6
asbut <logantuscano@yandex.ru>
2021-11-09 11:46:22 +0300

    fix order form(country)

--- app/Modules/Order/Traits/AddressAttributesReplacement.php
+++ app/Modules/Order/Traits/AddressAttributesReplacement.php
@@ -3,6 +3,7 @@
 namespace Modules\Order\Traits;
 
 
+use Modules\Core\Models\CountryLangsModel;
 use Modules\Core\Models\CountryModel;
 use Modules\Core\Models\StateModel;
 
@@ -69,15 +70,15 @@ trait AddressAttributesReplacement
 
         foreach ($replacements as $replacement) {
             if ($data[$replacement . 'country']) {
-                $filter = ['name' => $data[$replacement . 'country']];
+                $filter = ['value' => $data[$replacement . 'country']];
 
                 if (array_key_exists(strtoupper($data[$replacement . 'country']), CountryModel::$codes)) {
-                    $filter = ['code' => CountryModel::$codes[strtoupper($data[$replacement . 'country'])]];
+                    $filter = ['country_code' => CountryModel::$codes[strtoupper($data[$replacement . 'country'])]];
                 }
 
-                /** @var CountryModel $cModel */
-                if ($cModel = CountryModel::objects()->get($filter)) {
-                    $data[$replacement . 'country'] = $cModel->code;
+                /** @var CountryLangsModel $cModel */
+                if ($cModel = CountryLangsModel::objects()->get($filter)) {
+                    $data[$replacement . 'country'] = $cModel->country_code;
                 }
             }
 
