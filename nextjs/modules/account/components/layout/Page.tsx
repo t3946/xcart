bcd8888405bcf42a00cf7ed1dd3799ca792f713a
alexander <alexander@s3stores.com>
2021-12-08 15:03:46 +0300

    remove unused

--- nextjs/modules/account/components/layout/Page.tsx
+++ nextjs/modules/account/components/layout/Page.tsx
@@ -1,31 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
-import useSelectorAccount from "@modules/account/hooks/useSelectorAccount";
-import { getTerritory } from "@redux/actions/account-actions/MainActions";
 import HatNavigation from "@modules/account/components/hat/HatNavigation";
 import HatSearchLine from "@modules/account/components/hat/HatSearchLine";
 import MenuMobile from "@modules/account/components/hat/MenuMobile";
-import { getAddresses } from "@redux/actions/account-actions/AddressActions";
 import { setBreadcrumbsAddresses } from "@redux/actions/account-actions/BreadcrumbsActions";
 import { staticRoutes } from "@modules/account/ts/consts/breadcrumbs";
 import Snackbar from "@modules/account/components/snackbar/Snackbar";
 import SideBarMenu from "@modules/account/components/sidebar-menu/SideBarMenu";
-import PageContainerHoc from "@modules/account/hoc/PageContainerHoc";
 import cn from "classnames";
 import BreadCrumbs from "@modules/account/components/bread-crumbs/BreadCrumbs";
 
 const Page: React.FC = (props): any => {
   const dispatch = useDispatch();
-  // const user = useSelectorAccount((e) => e.user);
-
-  // useEffect(() => {
-  //   dispatch(getTerritory());
-  //
-  //   if (user) {
-  //     dispatch(getAddresses(user.user_id));
-  //   }
-  // }, []);
-  //
   dispatch(setBreadcrumbsAddresses(staticRoutes));
 
   const classes = {
@@ -35,7 +21,6 @@ const Page: React.FC = (props): any => {
 
   return (
     <>
-      {/*<ShadowPanel />*/}
       <Snackbar>
         <HatNavigation />
         <HatSearchLine isStatic={true} />
