36a447cd9da5b04a982c91701f4adc57241729c1
YuryevArt <jurjev.artem@gmail.com>
2021-12-14 17:51:33 +0300

    module styles

--- nextjs/modules/account/components/orders/Navigation/Navigation.tsx
+++ nextjs/modules/account/components/orders/Navigation/Navigation.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Item from "@modules/account/components/orders/Navigation/Item";
+// import useBreakpoint from "@modules/account/hooks/useBreakpoint";
 import StoreInterface from "@modules/account/ts/types/store.type";
 import NavigationMobile from "@modules/account/components/orders/Navigation/NavigationMobile";
+import { route } from "@utils/AppData";
 import useSelectorAccount from "@modules/account/hooks/useSelectorAccount";
+import Styles from "@modules/account/components/orders/Navigation/Navigation.module.scss"
 
 const Navigation: React.FC = () => {
-  useSelectorAccount((e) => e.main.breakpoint);
+  // useSelector((store: AccountStore) => store.main.breakpoint);
   const user = useSelectorAccount((e: StoreInterface) => e.user);
 
   const menu = [
@@ -15,8 +18,7 @@ const Navigation: React.FC = () => {
       badge: user?.decisions_required_count || 0,
       classes: {
         button: [
-          "orders-navigation-button_theme_red_active",
-          "orders-navigation-button_active",
+          Styles.ordersNavigationButton_active,
         ],
       },
     },
@@ -31,22 +33,25 @@ const Navigation: React.FC = () => {
     { text: "Order log", path: "/" },
   ];
 
-  const items: any[] = [];
+  const items = [];
 
   menu.forEach((value, index) => {
     items.push(<Item {...value} key={index} />);
   });
 
-  return (
-    <>
+  // const breakpoint = useBreakpoint();
+
+  //todo: fix breakpoint
+  /*return breakpoint({
+    md: <div className={"orders-navigation"}>{items}</div>,
+    xs: (
       <NavigationMobile
         menu={menu}
-        className={"account-orders__mobile-navigation d-lg-none"}
+        className={"account-orders__mobile-navigation"}
       />
-
-      <div className={"orders-navigation d-none d-lg-flex"}>{items}</div>
-    </>
-  );
+    ),
+  });*/
+  return <div className={"orders-navigation"}>{items}</div>;
 };
 
 export default Navigation;
