21f53e06c2762c57870039585ce197f6483d4c8b
alexander <alexander@s3stores.com>
2021-12-16 16:39:56 +0300

    customize feedback

---
+++ nextjs/modules/ui/forms/Feedback.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+import { FeedbackProps } from "react-bootstrap/Feedback";
+import Styles from "@modules/ui/forms/Feedback.module.scss";
+import cn from "classnames";
+
+const Feedback: React.FC<FeedbackProps> = (props: FeedbackProps) => {
+  const newProps: FeedbackProps = { ...props };
+
+  newProps.className = cn([
+    {
+      [Styles.feedback_invalid]: props.type === "invalid",
+      [Styles.feedback_valid]: props.type === "valid",
+    },
+    props.className,
+  ]);
+
+  return <Form.Control.Feedback {...newProps} />;
+};
+
+export default Feedback;
