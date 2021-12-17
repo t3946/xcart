f6aa5ddc03fb0a8712021f3343ccfdb5ed235410
asbut <logantuscano@yandex.ru>
2021-12-17 16:44:36 +0300

    send metrics only active dx

--- app/Modules/Metrics/Commands/MetricsProductCommand.php
+++ app/Modules/Metrics/Commands/MetricsProductCommand.php
@@ -17,10 +17,10 @@ class MetricsProductCommand extends Command
     {
         $str_result = '';
         /** @var DistributorModel $distributor_model */
-        foreach (DistributorModel::objects()->all() as $distributor_model) {
+        foreach (DistributorModel::objects()->filter(['avail' => 'Y'])->all() as $distributor_model) {
             $name = preg_replace('/[^a-zA-Z0-9\s]/iu', '', (string)$distributor_model);
             $name = "[$distributor_model->code] $name";
-            $count_without_picture = $distributor_model->products->filter(['detail_images__image_id__isnull' => true])->group(['productid'])->count();
+            $count_without_picture = $distributor_model->products->filter(['detail_images__image_id__isnull' => true, 'forsale' => 'Y'])->group(['productid'])->count();
 
             $str_result .= MetricsDataHelper::convertToMetricsWithParams('products_active', $distributor_model->active_products, [
                 'dx_code' => $name
@@ -35,7 +35,7 @@ class MetricsProductCommand extends Command
         foreach (SiteModel::getAllEnabled() as $site_model) {
             $products_count = $site_model->products->filter(['is_group_root' => false, 'forsale' => 'Y'])->count();
             $products_ads_count = $site_model->products->filter(['google_ads__shopping_status' => GoogleProductsModel::SHOPPING_STATUS_APPROVED])->count();
-            $count_without_picture = $site_model->products->filter(['detail_images__image_id__isnull' => true])->group(['productid'])->count() ?? 0;
+            $count_without_picture = $site_model->products->filter(['detail_images__image_id__isnull' => true, 'forsale' => 'Y'])->group(['productid'])->count();
 
             $str_result .= MetricsDataHelper::convertToMetricsWithParams('products_site', $products_count, [
                 'site' => (string)$site_model
