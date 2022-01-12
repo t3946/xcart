391c408181465c0fc2592d9db38c70ec1fa7680c
YuryevArt <jurjev.artem@gmail.com>
2022-01-12 14:00:08 +0300

    fix styles and tablet loginButton when is not logined

--- nextjs/modules/account/components/hat/LoginButton/LoginButtonTablet.tsx
+++ nextjs/modules/account/components/hat/LoginButton/LoginButtonTablet.tsx
@@ -10,10 +10,35 @@ import TransitionFade from "@modules/account/components/shared/TransitionFade";
 import useSelectorAccount from "@modules/account/hooks/useSelectorAccount";
 import UserIcon from "@modules/account/components/hat/LoginButton/UserIcon";
 
+import Styles from "@modules/account/components/hat/LoginButton/LoginButtonTablet.module.scss";
+
 const LoginButtonTablet: React.FC<any> = () => {
   const dispatch = useDispatch();
   const user = useSelectorAccount((e) => e.user);
   const text = user ? user.name : "log in";
+
+  if (!user) {
+    const path = "/account/login";
+
+    return (
+      <a
+        href={path}
+        className={classNames(
+          "navigation-login-button",
+          "d-none",
+          "d-md-flex",
+          "navigation-login-button__tablet",
+          "align-items-center",
+          "text-decoration-none",
+          "justify-content-evenly",
+          Styles.button
+        )}
+      >
+        <UserIcon />
+        <span className="hat-login-button-username">{text}</span>
+      </a>
+    );
+  }
 
   const CustomToggle = React.forwardRef((props, ref) => {
     const { onClick } = props;
