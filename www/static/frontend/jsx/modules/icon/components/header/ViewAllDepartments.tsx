47d82d7a09d9318652123db13bd7d13943f1034d
YuryevArt <jurjev.artem@gmail.com>
2022-01-12 14:23:10 +0300

    move icons to old project

---
+++ www/static/frontend/jsx/modules/icon/components/header/ViewAllDepartments.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import classnames from "classnames";
+const ViewAllDepartments = (props: Record<any, any>): any => {
+  return (
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 28.431 23.981"
+      className={classnames(props.className)}
+    >
+      <g>
+        <path
+          fill="currentColor"
+          d="M16.413,0l12.019,12.019L16.413,23.981v-7.569
+		C3.543,15.307,0,21.855,0,21.855S0,9.921,16.413,7.654V0z"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default ViewAllDepartments;
