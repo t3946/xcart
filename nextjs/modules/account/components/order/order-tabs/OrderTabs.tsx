838bc1eebedd01c3746535824f3be13401af2a0c
lightziro <logantuscano@yandex.ru>
2022-01-17 12:14:05 +0300

    order tabs, problem statuses model

---
+++ nextjs/modules/account/components/order/order-tabs/OrderTabs.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Tab, Tabs } from "react-bootstrap";
+import { ProblemWithOrder } from "@modules/account/components/orders/ProblemWithOrder";
+import { ReturnOrReplaceItems } from "@modules/account/components/orders/ReturnOrReplaceItems";
+import { CancelItems } from "@modules/account/components/orders/CancelItems";
+
+export const OrderTabs: React.FC = () => {
+  return (
+    <Tabs
+      defaultActiveKey="home"
+      id="uncontrolled-tab-example"
+      className={`mb-3 account-tabs`}
+    >
+      <Tab
+        tabClassName="account-tab"
+        eventKey="home"
+        title="Problem with order"
+      >
+        <div className="account-tab-content">
+          <ProblemWithOrder />
+        </div>
+      </Tab>
+      <Tab
+        tabClassName="account-tab"
+        eventKey="profile"
+        title="Return or replace items"
+      >
+        <div className="account-tab-content">
+          <ReturnOrReplaceItems />
+        </div>
+      </Tab>
+      <Tab tabClassName="account-tab" eventKey="contact" title="Cancel items">
+        <div className="account-tab-content">
+          <CancelItems />
+        </div>
+      </Tab>
+    </Tabs>
+  );
+};
