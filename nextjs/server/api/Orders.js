4b55ef287e2bd6cdcfda9c70cfa9b439ed7e95f5
alexander <alexander@s3stores.com>
2022-01-24 10:48:45 +0300

    remove unused

--- nextjs/server/api/Orders.js
+++ nextjs/server/api/Orders.js
@@ -3,13 +3,11 @@ const isAuthMiddleware = require("../middleware/isAuth");
 const axios = require("axios");
 
 app.get("/get/:type/:date", isAuthMiddleware, async (req, res) => {
-  console.log("HI!");
   const { type, date } = req.params;
   const userId = req.user.userId;
   const orders = await axios
     .get(process.env.BASE_URL_NGINX + `/api/account/orders/get/${userId}/${type}/${date}`)
     .then((response) => response.data);
-  console.log(orders);
   res.json(orders);
 });
 
