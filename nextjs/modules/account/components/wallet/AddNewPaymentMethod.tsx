582150cec4ba828ad8bcb729b20609c5122008a2
alexander <alexander@s3stores.com>
2022-01-28 09:36:24 +0300

    fix button styles

--- nextjs/modules/account/components/wallet/AddNewPaymentMethod.tsx
+++ nextjs/modules/account/components/wallet/AddNewPaymentMethod.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import { CardDialog } from "@modules/account/components/wallet/CardDialog";
 import { useDialog } from "@modules/account/hooks/useDialog";
 import { BillingAddressFormEnum } from "../../ts/consts/billing-address-form-types";
-import { useHistory } from "react-router";
 import { useRouter } from "next/router";
 import useBreakpoint from "@modules/account/hooks/useBreakpoint";
+import Button from "@modules/ui/forms/Button";
 
 export const AddNewPaymentMethod: React.FC = () => {
   const router = useRouter();
-
   const addDialog = useDialog();
-
   const breakpoint = useBreakpoint();
-
   const addCard = () => {
     breakpoint({
       sm: () => router.push("/account/payments/wallet/add"),
@@ -22,12 +19,9 @@ export const AddNewPaymentMethod: React.FC = () => {
 
   return (
     <div className="add-new-payment-method-container">
-      <button
-        onClick={addCard}
-        className="form-button account-submit-btn edit-card-btn add-new-payment"
-      >
+      <Button onClick={addCard} className="w-auto">
         Add a credit or debit card
-      </button>
+      </Button>
       <div>S3 Stores Inc accepts major credit and debit cards</div>
       <CardDialog
         contentType={BillingAddressFormEnum.ADD_CARD}
