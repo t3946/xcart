dc71cbbc01cf3f182e2922140b13cfb18022e6bb
alexander <alexander@s3stores.com>
2022-02-04 11:17:45 +0300

    move from dashboard if unauthorised

--- nextjs/pages/dashboard.tsx
+++ nextjs/pages/dashboard.tsx
@@ -2,26 +2,38 @@ import * as React from "react";
 import PageTwoColumns from "@modules/account/components/layout/PageTwoColumns";
 import DashboardPage from "@modules/account/components/dashboard/Dashboard";
 import { getInstance } from "@services/axios/Instance";
+import { useRouter } from "next/router";
+import useSelectorAccount from "@modules/account/hooks/useSelectorAccount";
 
 export async function getServerSideProps(ctx: Record<any, any>) {
   const instance = getInstance(ctx.req);
-  let groups: { solved: []; notSolved: [] };
+  let groups: { solved: []; notSolved: [] } = null;
 
   await instance
     .get("/api-client/orders/get-order-groups")
     .then((res) => {
       groups = res.data;
     })
-    .catch();
+    .catch((err) => {
+      console.log(err);
+    });
+
   return {
     props: { groups },
   };
 }
 
-function Dashboard(props) {
+function Dashboard(props: any) {
+  const router = useRouter();
+  const user = useSelectorAccount((e) => e.user);
+
+  React.useEffect(() => {
+    router.push(user ? "/dashboard" : "/login");
+  });
+
   return (
     <PageTwoColumns>
-      <DashboardPage {...props} />
+      {props.groups && <DashboardPage {...props} />}
     </PageTwoColumns>
   );
 }
