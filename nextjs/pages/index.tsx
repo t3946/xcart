5bf924383fb041be788931dfc869975b15a8427f
alexander <alexander@s3stores.com>
2022-02-04 10:58:22 +0300

    redirect to log in from main page

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 import { useRouter } from "next/router";
 import PageTwoColumns from "../modules/account/components/layout/PageTwoColumns";
+import useSelectorAccount from "@modules/account/hooks/useSelectorAccount";
 
 function Home() {
   const router = useRouter();
+  const user = useSelectorAccount((e) => e.user);
 
   React.useEffect(() => {
-    router.push("/dashboard");
+    router.push(user ? "/dashboard" : "/login");
   });
 
-  return <PageTwoColumns>Welcome to your account!</PageTwoColumns>;
+  const greetings = user ? "Welcome to your account!" : "Please sign in!";
+
+  return <PageTwoColumns>{greetings}</PageTwoColumns>;
 }
 
 export default Home;
