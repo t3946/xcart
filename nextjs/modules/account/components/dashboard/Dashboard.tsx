629d4a2c44428eb8914cc9cdcd74caf336f48d95
alexander <alexander@s3stores.com>
2022-02-07 09:37:08 +0300

    fix key problem

--- nextjs/modules/account/components/dashboard/Dashboard.tsx
+++ nextjs/modules/account/components/dashboard/Dashboard.tsx
@@ -18,8 +18,8 @@ const Dashboard = (props) => {
     <div className="pb-3">
       <div className={cn("d-flex", "flex-dir-column", Styles.pageColumn)}>
         <AccountInfo />
-        {props.groups?.map((order) => (
-          <OrderTracking orderInfo={order} />
+        {props.groups?.map((order, key) => (
+          <OrderTracking orderInfo={order} key={`tracking-${key}`} />
         ))}
 
         <AccountNavigation />
