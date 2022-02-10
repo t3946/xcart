2016fc051ab9509c42713afcabd98360abc26624
YuryevArt <jurjev.artem@gmail.com>
2022-02-10 12:20:05 +0300

    user avatar in login button for sm resolution

--- www/static/frontend/jsx/modules/account/components/hat/LoginButton/LoginButtonMobile.tsx
+++ www/static/frontend/jsx/modules/account/components/hat/LoginButton/LoginButtonMobile.tsx
@@ -5,15 +5,14 @@ import { StoreDto } from "@s3stores-mail/ts/types";
 import { setMobileMenuIsVisible } from "@client/jsx/redux/actions/account-actions/MenuActions";
 import HideAllMenu from "@client/jsx/modules/account/utils/hide-all-menu";
 import { setVisibleShadowPanelAction } from "@client/jsx/redux/actions/account-actions/ShadowPanelActions";
+import UserIcon from "@client/modules/account/components/hat/LoginButton/UserIcon";
 
 const MobileTemplate: React.FC<any> = () => {
   const user = useSelector((e: StoreDto) => e.user);
   const classes = ["navigation-login-button d-flex align-items-center"];
   const dispatch = useDispatch();
 
-  if (user) {
-    classes.push("navigation-login-button__logged");
-  } else {
+  if (!user) {
     classes.push("navigation-login-button__not-logged");
   }
 
@@ -29,7 +28,11 @@ const MobileTemplate: React.FC<any> = () => {
       onClick={openMenu}
       className="d-md-none hat-navigation-item d-flex align-items-center"
     >
-      <i className={classNames(classes)} />
+      {user ? (
+        <UserIcon className={classes} />
+      ) : (
+        <i className={classNames(classes)} />
+      )}
     </div>
   );
 };
