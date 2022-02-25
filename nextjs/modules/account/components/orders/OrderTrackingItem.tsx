9851f01285470eacf12453c853d2ef724f917dc0
alexander <alexander@s3stores.com>
2022-02-25 13:35:36 +0300

    remove unused

--- nextjs/modules/account/components/orders/OrderTrackingItem.tsx
+++ nextjs/modules/account/components/orders/OrderTrackingItem.tsx
@@ -13,12 +13,6 @@ export const OrderTrackingItem: React.FC<OrderTrackingItemProps> = ({
       {!!orderGroupInfo.trackings.length && (
         <div className="order-tracking-line-text">
           <div>
-            {/* <div className="order-tracking-text">Estimated delivery date:</div>
-            <div className="order-tracking-weight-text">
-              Monday, May 3rd, 2021
-            </div> */}
-          </div>
-          <div>
             {orderGroupInfo.trackings.map((track, i) => (
               <div
                 key={`${track.id}_${i}`}
