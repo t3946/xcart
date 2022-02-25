4a6688103cea3ff3c617f0fd8d9f6e9cd99631d3
YuryevArt <jurjev.artem@gmail.com>
2022-02-25 15:11:27 +0300

    fix styles

--- nextjs/modules/account/components/shared/FormCheckBox.tsx
+++ nextjs/modules/account/components/shared/FormCheckBox.tsx
@@ -20,7 +20,7 @@ export const FormCheckBox = ({
         checked={value}
         disabled={disabled}
       />
-      <label className={"checkbox-label"} htmlFor={id}>
+      <label className={"checkbox-label"}>
         <div className={"label-text text-dark"}>{label}</div>
       </label>
     </div>
