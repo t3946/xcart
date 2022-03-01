972fc67af9005731a5b7bce4b4438f00bc20263a
alexander <alexander@s3stores.com>
2022-03-01 12:31:54 +0300

    fix condition

--- nextjs/modules/account/components/wallet-transactions/TransactionsList.tsx
+++ nextjs/modules/account/components/wallet-transactions/TransactionsList.tsx
@@ -88,7 +88,7 @@ export const TransactionsList: React.FC<IProps> = (props) => {
 
     return (
       <div>
-        {groups.completed && (
+        {groups.completed.length && (
           <>
             <div className={"transactions-completed-header d-none d-md-block"}>
               Completed
@@ -97,7 +97,7 @@ export const TransactionsList: React.FC<IProps> = (props) => {
           </>
         )}
 
-        {groups.pending && (
+        {groups.pending.length && (
           <>
             <div className={"transactions-completed-header d-none d-md-block"}>
               Pending
@@ -106,7 +106,7 @@ export const TransactionsList: React.FC<IProps> = (props) => {
           </>
         )}
 
-        {groups.cancelled && (
+        {groups.cancelled.length && (
           <>
             <div className={"transactions-completed-header d-none d-md-block"}>
               Cancelled
