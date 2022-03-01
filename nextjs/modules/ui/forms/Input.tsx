ba4d3fbde3524049e61ae40bde2d0f84bd019915
alexander <alexander@s3stores.com>
2022-03-01 15:39:38 +0300

    fix textarea styles

--- nextjs/modules/ui/forms/Input.tsx
+++ nextjs/modules/ui/forms/Input.tsx
@@ -1,5 +1,4 @@
 import { Form, FormControlProps } from "react-bootstrap";
-
 import React, { ChangeEvent } from "react";
 import Styles from "@modules/ui/forms/Input.module.scss";
 import cn from "classnames";
@@ -10,7 +9,7 @@ interface IProps extends FormControlProps {
   value?: any;
   disabled?: boolean;
   onChange?: (e: ChangeEvent) => void;
-  onBlur?: (e: BlurEvent) => void;
+  onBlur?: (e: any) => void;
   className?: any;
   isValid?: boolean;
   isInvalid?: boolean;
@@ -29,6 +28,11 @@ const Input = React.forwardRef<HTMLInputElement | null, IProps>(
         [Styles.input_invalid]: props.isInvalid,
       },
     ];
+
+    if (props.as === "textarea") {
+      classes.push(Styles.input_textarea);
+    }
+
     const { maxLength = Number.MAX_VALUE, onChange, type = "text" } = props;
     const mergeProps = {
       ...props,
@@ -39,6 +43,7 @@ const Input = React.forwardRef<HTMLInputElement | null, IProps>(
       className: cn(classes),
       ref: ref,
     };
+
     return <Form.Control {...mergeProps} />;
   }
 );
