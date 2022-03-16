a2fdc479c3af5be0551ab80f06d069906c6474f6
alexander <alexander@s3stores.com>
2022-03-16 17:40:49 +0300

    update dependencies

--- www/static/gulp/tasks/client/bem.js
+++ www/static/gulp/tasks/client/bem.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const sass = require('gulp-sass')(require('sass'));
 const frontend = require("../../../config/gulp.frontend");
 const inlineImage = require("gulp-inline-image");
 const { default: GulpAssets } = require("../../../GulpAssets");
