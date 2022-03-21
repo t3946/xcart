ad3445d8ab79dd9d437f576d334a918025ee0019
alexander <alexander@s3stores.com>
2022-03-21 09:48:00 +0300

    disable eslint in build

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -48,5 +48,10 @@ module.exports = withImages(
       // !! WARN !!
       ignoreBuildErrors: true,
     },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
   })
 );
