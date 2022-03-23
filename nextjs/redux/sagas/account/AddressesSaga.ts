d44646b344e98bc073723925d46d03c591508d9a
alexander <alexander@s3stores.com>
2022-03-23 17:31:18 +0300

    replace php addresses api on node js api

--- nextjs/redux/sagas/account/AddressesSaga.ts
+++ nextjs/redux/sagas/account/AddressesSaga.ts
@@ -1,11 +1,7 @@
 import { put, takeLatest } from "redux-saga/effects";
 import { SagaIterator } from "redux-saga";
-import { ApiService } from "@modules/shared/services/api.service";
 import { AnyAction } from "redux";
-import Store from "@redux/stores/Store";
 import axios from "axios";
-
-const api = new ApiService();
 
 function* getAddresses(): Generator {
   let addresses = null;
@@ -21,114 +17,57 @@ function* getAddresses(): Generator {
 }
 
 function* changeDefaultAddress(action: AnyAction): Generator {
-  const result: any = yield api
-    .post<any>(
-      `/api/account/addresses/change-default-address`,
-      JSON.stringify({
-        user: action.userId,
-        addressId: action.id,
-      })
-    )
-    .then((response) => response)
-    .catch((error) => console.log(error));
+  yield axios.post("/api-client/user/address/set-default", {
+    addressId: action.id,
+  });
 
-  try {
-    yield put({
-      type: "SET_ADDRESSES",
-      addresses: result,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  yield getAddresses();
 
   action.callback();
 }
 
 function* removeAddress(action: AnyAction): Generator {
-  const result: any = yield api
-    .post<any>(
-      `/api/account/addresses/remove-address`,
-      JSON.stringify({
-        addressId: action.id,
-        user: Store.getState().user.id,
-      })
-    )
-    .then((response) => response)
-    .catch((error) => console.log(error));
+  yield axios.post("/api-client/user/address/remove", {
+    addressId: action.id,
+  });
 
-  try {
-    yield put({
-      type: "SET_ADDRESSES",
-      addresses: result,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  yield getAddresses();
 
   action.callback();
 }
 
 function* addAddress(action: AnyAction): Generator {
-  const result: any = yield api.post<any>(
-    `/api/account/addresses/add-address`,
-    JSON.stringify({
-      user: action.userId,
+  let newAddress = null;
+
+  yield axios
+    .post("/api-client/user/address/create", {
       address: action.address,
     })
-  );
-
-  const oldList = Store.getState().addresses.addressesList;
-
-  const newBilling = result.find(
-    (newAddress) =>
-      !oldList.find(
-        (oldAddress) => oldAddress.address_id === newAddress.address_id
-      )
-  );
-
-  try {
-    yield put({
-      type: "SET_ADDRESSES",
-      addresses: result,
+    .then((res) => {
+      newAddress = res.data.address;
     });
-  } catch (error) {
-    console.log(error);
-    return;
-  }
+
+  yield getAddresses();
 
   if (action.onPendingEnd) {
-    yield action.onPendingEnd(newBilling);
+    yield action.onPendingEnd(newAddress);
   }
 }
 
 function* editAddress(action: AnyAction): Generator {
-  const result: any = yield api
-    .post<any>(
-      `/api/account/addresses/edit-address`,
-      JSON.stringify({
-        address: action.address,
-        user: Store.getState().user.id,
-      })
-    )
-    .then((response) => response)
-    .catch((error) => console.log(error));
+  yield axios.post("/api-client/user/address/edit", {
+    address: action.address,
+  });
 
-  try {
-    yield put({
-      type: "SET_ADDRESSES",
-      addresses: result,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  yield getAddresses();
 
   yield action.onPendingEnd();
 }
 
 export function* addressesActionWatcher(): SagaIterator {
   yield takeLatest("GET_ADDRESSES", getAddresses);
-  yield takeLatest("CHANGE_DEFAULT_ADDRESS", changeDefaultAddress);
-  yield takeLatest("REMOVE_ADDRESS", removeAddress);
   yield takeLatest("ADD_ADDRESS", addAddress);
   yield takeLatest("EDIT_ADDRESS", editAddress);
+  yield takeLatest("REMOVE_ADDRESS", removeAddress);
+  yield takeLatest("CHANGE_DEFAULT_ADDRESS", changeDefaultAddress);
 }
--- nextjs/server/api/user/Addresses.js
+++ nextjs/server/api/user/Addresses.js
@@ -3,6 +3,29 @@ const isAuthMiddleware = require("../../middleware/isAuth");
 const PrismaClient = require("@prisma/client").PrismaClient;
 const prisma = new PrismaClient();
 
+async function setAddressAsDefault(userId, addressId) {
+  //reset old default
+  await prisma.account_addresses.updateMany({
+    where: {
+      user_id: userId,
+    },
+    data: {
+      is_default: 0,
+    },
+  });
+
+  //set new default
+  await prisma.account_addresses.updateMany({
+    where: {
+      user_id: userId,
+      address_id: addressId,
+    },
+    data: {
+      is_default: 1,
+    },
+  });
+}
+
 api.get("/get", isAuthMiddleware, async (req, res) => {
   const addresses = await prisma.account_addresses.findMany({
     where: {
@@ -24,9 +47,75 @@ api.get("/get", isAuthMiddleware, async (req, res) => {
         },
       },
     },
+    orderBy: {
+      is_default: "desc",
+    },
   });
 
   res.json({ addresses });
+});
+
+api.post("/set-default", isAuthMiddleware, async (req, res) => {
+  const address = await prisma.account_addresses.findFirst({
+    where: {
+      user_id: req.user.userId,
+      address_id: req.body.addressId,
+    },
+  });
+
+  if (!address) {
+    res.sendStatus(400);
+    return;
+  }
+
+  await setAddressAsDefault(req.user.userId, req.body.addressId);
+
+  res.sendStatus(200);
+});
+
+api.post("/remove", isAuthMiddleware, async (req, res) => {
+  await prisma.account_addresses.deleteMany({
+    where: {
+      user_id: req.user.userId,
+      address_id: req.body.addressId,
+    },
+  });
+
+  res.sendStatus(200);
+});
+
+api.post("/create", isAuthMiddleware, async (req, res) => {
+  const address = await prisma.account_addresses.create({
+    data: {
+      ...req.body.address,
+      user_id: req.user.userId,
+    },
+  });
+
+  if (address.is_default) {
+    await setAddressAsDefault(req.user.user_id, address.address_id);
+  }
+
+  res.json({ address });
+});
+
+api.post("/edit", isAuthMiddleware, async (req, res) => {
+  const address = await prisma.account_addresses.updateMany({
+    data: {
+      ...req.body.address,
+      user_id: req.user.userId,
+    },
+    where: {
+      address_id: req.body.address.address_id,
+      user_id: req.user.userId,
+    },
+  });
+
+  if (address.is_default) {
+    await setAddressAsDefault(req.user.user_id, address.address_id);
+  }
+
+  res.sendStatus(200);
 });
 
 module.exports = api;
