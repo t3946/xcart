d925fc744f15dc7eaec18d327dc550b83eef6dba
alexander <alexander@s3stores.com>
2022-03-23 09:46:42 +0300

    get smtp host and port from .env

--- nextjs/server/services/mail.js
+++ nextjs/server/services/mail.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
   secure: true, // upgrade later with STARTTLS
   auth: {
     user: process.env.SMTP_USER,
