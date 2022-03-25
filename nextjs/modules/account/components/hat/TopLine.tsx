5b6ce8277210705a3b818816d82fa2b467004fbd
alexander <alexander@s3stores.com>
2022-03-25 11:11:47 +0300

    get top line information from state

--- nextjs/modules/account/components/hat/TopLine.tsx
+++ nextjs/modules/account/components/hat/TopLine.tsx
@@ -1,28 +1,24 @@
 import React from "react";
 import CallAfterHours from "@modules/account/components/hat/CallAfterHours";
 import CallInHours from "@modules/account/components/hat/CallInHours";
-
-const CallHours: React.FC = () => {
-  // if (appData.site.workingDayTimeNow) {
-  if (false) {
-    return <CallAfterHours />;
-  } else {
-    return <CallInHours />;
-  }
-};
+import useSelectorAccount from "@modules/account/hooks/useSelectorAccount";
 
 const TopLine: React.FC = () => {
+  const config = useSelectorAccount((e) => e.config);
   return (
     <div className="top-header show-for-large">
       <div className="container">
         <div className="row">
           <div className="col-sm-4 d-flex">
-            {/*<span className="top-line-site-name">{appData.site.shortName}</span>*/}
-            <span className="top-line-site-name">artist</span>
+            <span className="top-line-site-name">{config.site.shortName}</span>
           </div>
           <div className="col-sm-8">
             <div className="align-items-start call_lang d-flex justify-content-end">
-              <CallHours />
+              {config.site.workingDayTimeNow ? (
+                <CallAfterHours />
+              ) : (
+                <CallInHours />
+              )}
               <a href="#" className="top-line-lang-icon top-line_lang-icon"></a>
             </div>
           </div>
