ad70cea8b095ee7c18e806bfa35fd3548ceac786
alexander <alexander@s3stores.com>
2022-03-28 12:50:23 +0300

    fix logout in old site

--- www/static/frontend/jsx/modules/account/components/sidebar-menu/LogoutButton.tsx
+++ www/static/frontend/jsx/modules/account/components/sidebar-menu/LogoutButton.tsx
@@ -1,35 +1,23 @@
 import React from "react";
-import { logoutAction } from "@client/jsx/redux/actions/account-actions/AutorizationActions";
-import { userClearAction } from "@client/jsx/redux/actions/account-actions/UserActions";
-import { useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import {useDispatch} from "react-redux";
 import HideAllMenu from "@client/modules/account/utils/hide-all-menu";
 import cn from "classnames";
-
 import Styles from "@client/jsx/modules/account/components/sidebar-menu/LogoutButton.module.scss";
+import axios from "axios";
 
 interface IProps {
   onClick?: () => void;
 }
 
 const LogoutButton: React.FC<IProps> = function (props: IProps) {
-  const history = useHistory();
   const dispatch = useDispatch();
 
   function logout() {
     HideAllMenu(dispatch);
 
-    dispatch(
-      logoutAction({
-        callback() {
-          dispatch(userClearAction());
-
-          if (history) {
-            history.push("/account/login");
-          }
-        },
-      })
-    );
+    axios.get("/api-client/user/logout").then(() => {
+      document.location.reload();
+    });
 
     props.onClick && props.onClick();
   }
