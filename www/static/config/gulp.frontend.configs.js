92272cc7de7a89531f4999f2d024e25f6f9ac0e2
Roman Novokshonov <kshon@mail.ru>
2022-03-28 17:51:09 +0300

    update gulp-imagemin

--- www/static/config/gulp.frontend.configs.js
+++ www/static/config/gulp.frontend.configs.js
@@ -21,23 +21,7 @@ module.exports = {
             colorTypeReduction: true,
             paletteReduction: true,
             buffer: true
-        }),
-        imagemin.svgo({plugins: [{
-            removeViewBox: false,
-            removeComments: true,
-            removeMetadata: true,
-            removeUselessDefs: true,
-            removeDimensions: true,
-            removeEditorsNSData: true,
-            removeEmptyAttrs: true,
-            removeHiddenElems: true,
-            removeEmptyContainers: true,
-            cleanupEnableBackground: true,
-            cleanupIDs: true,
-            minifyStyles: true,
-            collapseGroups: true,
-            convertPathData: true
-        }]})
+        })
     ],
     cssnano: {
         preset: ['default'],
