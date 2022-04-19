3b7f780bb6dc6aff313a8b1a9f4dbabec99805d3
alexander <alexander@s3stores.com>
2022-04-19 16:25:56 +0300

    get storefront in node api

--- nextjs/ssr-server.js
+++ nextjs/ssr-server.js
@@ -13,6 +13,8 @@ const OrdersApi = require("./server/api/Orders");
 const ProductApi = require("./server/api/Product");
 const ReviewsApi = require("./server/api/Reviews");
 const cookieParser = require("cookie-parser");
+const PrismaClient = require("@prisma/client").PrismaClient;
+const prisma = new PrismaClient();
 
 function listen() {
   const port = process.env.PORT;
@@ -23,6 +25,19 @@ function listen() {
   });
 }
 
+async function getStorefrontMiddleware(req, res, done) {
+  const storefrontDomain =
+    "www." + req.get("host").split(".").slice(-2).join(".");
+
+  req.storefront = await prisma.xcart_storefronts.findMany({
+    where: {
+      domain: storefrontDomain,
+    },
+  });
+
+  done();
+}
+
 mainApp.use(bodyParser.json());
 mainApp.use(bodyParser.urlencoded({ extended: true }));
 mainApp.use(cookieParser());
@@ -31,7 +46,7 @@ apiApp.use("/user", UserApi);
 apiApp.use("/orders", OrdersApi);
 apiApp.use("/product", ProductApi);
 apiApp.use("/review", ReviewsApi);
-mainApp.use("/api-client", apiApp);
+mainApp.use("/api-client", getStorefrontMiddleware, apiApp);
 
 if (process.env.RUN_BACKEND_ONLY === "true") {
   listen();
