9a5a2c3e22c474413423add9cefb3f031e4db326
alexander <alexander@s3stores.com>
2022-04-19 15:56:42 +0300

    remove unused

--- nextjs/server/api/Product.js
+++ nextjs/server/api/Product.js
@@ -2,9 +2,6 @@ const axios = require("axios");
 const app = require("express")();
 const AxiosInstance = axios.create();
 const getBaseUrl = require("../utils/getBaseUrl");
-const PrismaClient = require("@prisma/client").PrismaClient;
-const prisma = new PrismaClient();
-const { normalize } = require("../utils/product");
 
 app.post("/get", async function (req, res) {
   await AxiosInstance.post(
@@ -13,24 +10,6 @@ app.post("/get", async function (req, res) {
     res.json(apiRes.data);
     res.send();
   });
-});
-
-app.post("/get-node", async function (req, res) {
-  const product = await prisma.xcart_products.findFirst({
-    where: {
-      productid: req.body.productid,
-    },
-    select: {
-      productid: true,
-      new_map_price: true,
-      pricings: true,
-      is_group_root: true,
-    },
-  });
-
-  await normalize(product);
-
-  res.json({ product });
 });
 
 module.exports = app;
