4ec69ead8b484ffa2a1b8d008c20b82c8f3ed728
alexander <alexander@s3stores.com>
2022-04-27 11:56:46 +0300

    fix styles

--- nextjs/modules/account/components/dashboard/SliderProducts/CardSkeleton.tsx
+++ nextjs/modules/account/components/dashboard/SliderProducts/CardSkeleton.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Sceleton } from "@modules/shared/components/sceleton/Sceleton";
-import Styles from "@modules/account/components/dashboard/SliderProducts/CardSkeleton.module.scss";
 import cn from "classnames";
 
 const CardSkeleton: React.FC = () => {
   return (
-    <div className={cn(Styles.skeletonSliderCardWrapper)}>
+    <div className={cn("px-3", "px-md-0")}>
       <Sceleton margin={"0 0 10px 0"} height={172} maxWidth={"100%"} />
       <Sceleton margin={"0 0 5px 0"} height={40} maxWidth={"100%"} />
       <Sceleton margin={"0 0 5px 0"} height={15} maxWidth={"100%"} />
