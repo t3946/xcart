f56e556eac393135033cbf86d9f3ca4952c2636d
alexander <alexander@s3stores.com>
2022-04-29 13:42:16 +0300

    super button

--- www/static/frontend/jsx/components/footer-payment-methods/MultiplePaymentItem.tsx
+++ www/static/frontend/jsx/components/footer-payment-methods/MultiplePaymentItem.tsx
@@ -12,7 +12,7 @@ interface IProps {
 }
 
 const MultiplePaymentItem: React.FC<IProps> = (props) => {
-  const { paymentMethod, paymentChildren } = props;
+  const {paymentMethod, paymentChildren} = props;
   const [referenceElement, setReferenceElement] = useState(null);
   const [popperElement, setPopperElement] = useState(null);
   const [arrowElement, setArrowElement] = useState(null);
@@ -25,7 +25,7 @@ const MultiplePaymentItem: React.FC<IProps> = (props) => {
       },
       {
         name: 'offset',
-        options: { offset: [0, 20] }
+        options: {offset: [0, 20]}
       }
     ],
   });
@@ -42,7 +42,7 @@ const MultiplePaymentItem: React.FC<IProps> = (props) => {
     for (const payment of paymentChildren) {
       paymentItems.push(
         <li className={cn("d-flex", Styles.paymentItem)}>
-          <img src={payment.logo} alt={payment.name} />
+          <img src={payment.logo} alt={payment.name}/>
           <span className={"flex-grow-1 mx-2"}>{payment.name}</span>
           <span className={Styles.shortName}>{payment.short_name}</span>
         </li>
@@ -54,7 +54,7 @@ const MultiplePaymentItem: React.FC<IProps> = (props) => {
         <div className={cn(Styles.tooltipContent)}>
           <ul className={cn("list-unstyled m-0", Styles.list)}>{paymentItems}</ul>
         </div>
-        <div ref={setArrowElement} style={styles.arrow} className={cn(Styles.popperArrow)} />
+        <div ref={setArrowElement} style={styles.arrow} className={cn(Styles.popperArrow)}/>
       </div>
     );
   }
@@ -67,27 +67,113 @@ const MultiplePaymentItem: React.FC<IProps> = (props) => {
     };
   }, []);
 
-  function open(e) {
+  const [eventsHistory, setEventsHistory] = useState([]);
+  const [i, setI] = useState(1);
+
+  function toggle(e) {
+    setI(i + 1);
     e.stopPropagation();
-    setIsVisible(true);
+
+    switch (eventsHistory[0]) {
+      case "onClick":
+        if (eventsHistory[1] === "onMouseOver" && eventsHistory[2] === "onTouchStart") {
+          break;
+        }
+
+        if (eventsHistory[1] === "onMouseOver" && eventsHistory[2] === "onMouseOut" && eventsHistory[3] === "onTouchStart") {
+          break;
+        }
+
+        if (eventsHistory[1] === "onTouchStart") {
+          break;
+        }
+
+        setIsVisible(!isVisible);
+        break;
+
+      case "onTouchStart":
+        setIsVisible(!isVisible);
+        break;
+
+      case "onMouseOver":
+        if (eventsHistory[1] === "onTouchStart") {
+          break;
+        }
+
+        if (eventsHistory[1] === "onMouseOut" && eventsHistory[2] === "onTouchStart") {
+          break;
+        }
+
+        setIsVisible(true);
+        break;
+
+      case "onMouseOut":
+        if (eventsHistory[1] === "onTouchStart") {
+          break;
+        }
+
+        setIsVisible(false);
+        break;
+    }
   }
 
-  function close(e) {
-    e.stopPropagation();
-    setIsVisible(false);
+  const refPaymentItem = React.useRef(null);
+
+  function checkOnParent(targetElement, findElement) {
+    let parent = targetElement;
+
+    while (parent) {
+      if (parent === findElement) {
+        return true;
+      }
+
+      parent = parent.parentNode;
+    }
+
+    return false;
   }
 
   return (
     <>
-      <div ref={setReferenceElement}
-           onClick={open}
-           onMouseOver={open}
-           onMouseOut={close}
-           className={"d-inline-block"}
-           onTouchStart={open}
-           onTouchMove={open}
+      <div
+        ref={setReferenceElement}
+         onClick={(e) => {
+           e.stopPropagation();
+           eventsHistory.unshift("onClick");
+           setEventsHistory(eventsHistory.slice(0, 10));
+           toggle(e);
+         }}
+         onMouseOver={(e) => {
+           if (checkOnParent(e.relatedTarget, refPaymentItem.current.base)) {
+             return;
+           }
+
+           eventsHistory.unshift("onMouseOver");
+           setEventsHistory(eventsHistory.slice(0, 10));
+           toggle(e);
+         }}
+         onMouseOut={(e) => {
+           if (checkOnParent(e.relatedTarget, refPaymentItem.current.base)) {
+             return;
+           }
+
+           eventsHistory.unshift("onMouseOut");
+           setEventsHistory(eventsHistory.slice(0, 10));
+           toggle(e);
+         }}
+         className={"d-inline-block"}
+         onTouchStart={(e) => {
+           eventsHistory.unshift("onTouchStart");
+           setEventsHistory(eventsHistory.slice(0, 10));
+           toggle(e);
+         }}
+         onTouchMove={(e) => {
+           eventsHistory.unshift("onTouchMove");
+           setEventsHistory(eventsHistory.slice(0, 10));
+           toggle(e);
+         }}
       >
-        <PaymentItem paymentMethod={paymentMethod} isMultiple={true} isOpen={isVisible} />
+        <PaymentItem paymentMethod={paymentMethod} isMultiple={true} isOpen={isVisible} ref={refPaymentItem}/>
       </div>
 
       {tooltipTemplate()}
--- www/static/frontend/jsx/components/footer-payment-methods/PaymentItem.tsx
+++ www/static/frontend/jsx/components/footer-payment-methods/PaymentItem.tsx
@@ -6,14 +6,15 @@ import Styles from "@client/jsx/components/footer-payment-methods/PaymentItem.mo
 interface IProps {
   paymentMethod: Record<any, any>;
   isMultiple?: boolean;
-  isOpen?: boolean
+  isOpen?: boolean;
+  ref?: any;
 }
 
 export const PaymentItem: React.FC<IProps> = function (props) {
-  const {paymentMethod, isMultiple = false, isOpen = false} = props;
+  const {paymentMethod, isMultiple = false, isOpen = false, ref} = props;
 
   return (
-    <div className={cn("position-relative", {[Styles.item_multiple]: isMultiple})}>
+    <div className={cn("position-relative", {[Styles.item_multiple]: isMultiple})} ref={ref}>
       <img width="54" height="36" className="lazy-img footer-payment-method-image"
            data-src={"/" + paymentMethod.logo} alt={paymentMethod.name}
       />
